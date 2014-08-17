c300884a325a66af3290c6630e0708b09dd6c5b0
Kai Huebl <kai@huebl-sgh.de>
2014-08-17 09:33:09 +0200

    add unittest for class CloseSessionRequest

---
+++ tst/OpcUaStackCore/ServiceSet/CloseSession_t.cpp
@@ -0,0 +1,236 @@
+#include "unittest.h"
+#include "boost/asio.hpp"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaIdentifier.h"
+#include "OpcUaStackCore/SecureChannel/MessageHeader.h"
+#include "OpcUaStackCore/SecureChannel/SequenceHeader.h"
+#include "OpcUaStackCore/ServiceSet/CloseSessionRequest.h"
+#include "OpcUaStackCore/ServiceSet/CloseSessionResponse.h"
+#include "OpcUaStackCore/Base/Utility.h"
+
+#include <streambuf>
+#include <iostream>
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(CloseSession_)
+
+BOOST_AUTO_TEST_CASE(CloseSession_)
+{
+	std::cout << "CloseSession_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(CloseSession_Request)
+{
+	uint32_t pos;
+	MessageHeader::SPtr messageHeaderSPtr;
+	boost::posix_time::ptime ptime = boost::posix_time::from_iso_string("16010101T120000.000000000");
+	OpcUaGuid::SPtr opcUaGuidSPtr;
+	CloseSessionRequest::SPtr closeSessionRequestSPtr;
+	SequenceHeader::SPtr sequenceHeaderSPtr;
+	OpcUaNodeId typeId;
+
+	// stream
+	boost::asio::streambuf sb1;
+	std::iostream ios1(&sb1);
+	boost::asio::streambuf sb2;
+	std::iostream ios2(&sb2);
+	boost::asio::streambuf sb;
+	std::iostream ios(&sb);
+
+	// encode channel id
+	OpcUaUInt32 channelId;
+	channelId = 153451225;
+	OpcUaNumber::opcUaBinaryEncode(ios1, channelId);
+
+	// encode token id
+	OpcUaInt32 tokenId;
+	tokenId = 1;
+	OpcUaNumber::opcUaBinaryEncode(ios1, tokenId);
+
+	// encode sequence header
+	sequenceHeaderSPtr = SequenceHeader::construct();
+	sequenceHeaderSPtr->sequenceNumber(140);
+	sequenceHeaderSPtr->requestId(90);
+	sequenceHeaderSPtr->opcUaBinaryEncode(ios1);
+
+	// encode message type id
+	typeId.nodeId(OpcUaId_CloseSessionRequest_Encoding_DefaultBinary);
+	typeId.opcUaBinaryEncode(ios1);
+
+	// encode CloseSessionRequest
+	opcUaGuidSPtr = OpcUaGuid::construct();
+	*opcUaGuidSPtr = "12345678-9ABC-DEF0-1234-56789ABCDEF0";
+	closeSessionRequestSPtr = CloseSessionRequest::construct();
+
+	closeSessionRequestSPtr->requestHeader()->sessionAuthenticationToken().namespaceIndex(1);
+	closeSessionRequestSPtr->requestHeader()->sessionAuthenticationToken().nodeId(opcUaGuidSPtr);
+	closeSessionRequestSPtr->requestHeader()->time(ptime);
+	closeSessionRequestSPtr->requestHeader()->requestHandle(2);
+	closeSessionRequestSPtr->requestHeader()->returnDisagnostics(0);
+	closeSessionRequestSPtr->requestHeader()->timeoutHint(10000);
+
+	closeSessionRequestSPtr->deleteSubscriptions(true);
+
+	closeSessionRequestSPtr->opcUaBinaryEncode(ios1);
+
+	// encode MessageHeader
+	messageHeaderSPtr = MessageHeader::construct();
+	messageHeaderSPtr->messageType(MessageType_Message);
+	messageHeaderSPtr->messageSize(OpcUaStackCore::count(sb1)+8);
+	messageHeaderSPtr->opcUaBinaryEncode(ios2);
+
+	// stream
+	ios << ios2.rdbuf() << ios1.rdbuf();
+	OpcUaStackCore::dumpHex(ios);
+
+	std::stringstream ss;
+	ss	<< "4d 53 47 46 4b 00 00 00  d9 7a 25 09 01 00 00 00"
+		<< "8c 00 00 00 5a 00 00 00  01 00 d9 01 04 01 00 12"
+		<< "34 56 78 9a bc de f0 12  34 56 78 9a bc de f0 00"
+		<< "00 00 00 00 00 00 00 02  00 00 00 00 00 00 00 ff"
+		<< "ff ff ff 10 27 00 00 00  00 00 01";
+	BOOST_REQUIRE(OpcUaStackCore::compare(ios, ss.str(), pos) == true);
+
+	// decode MessageHeader
+	messageHeaderSPtr = MessageHeader::construct();
+	messageHeaderSPtr->opcUaBinaryDecode(ios);
+	BOOST_REQUIRE(messageHeaderSPtr->messageType() == MessageType_Message);
+	
+	// decode channel id
+	OpcUaNumber::opcUaBinaryDecode(ios, channelId);
+	BOOST_REQUIRE(channelId == 153451225);
+
+	// decode token id
+	OpcUaNumber::opcUaBinaryDecode(ios, tokenId);
+	BOOST_REQUIRE(tokenId == 1);
+	
+	// decode sequence header
+	sequenceHeaderSPtr = SequenceHeader::construct();
+	sequenceHeaderSPtr->opcUaBinaryDecode(ios);
+	BOOST_REQUIRE(sequenceHeaderSPtr->sequenceNumber() == 140);
+	BOOST_REQUIRE(sequenceHeaderSPtr->requestId() == 90);
+
+	// decode message type id
+	typeId.opcUaBinaryDecode(ios);
+	BOOST_REQUIRE(typeId.namespaceIndex() == 0);
+	BOOST_REQUIRE(typeId.nodeId<OpcUaUInt32>() == OpcUaId_CloseSessionRequest_Encoding_DefaultBinary);
+
+	// decode CloseSessionRequest
+	closeSessionRequestSPtr = CloseSessionRequest::construct();
+	closeSessionRequestSPtr->opcUaBinaryDecode(ios);
+
+	std::string str;
+	str = *closeSessionRequestSPtr->requestHeader()->sessionAuthenticationToken().nodeId<OpcUaGuid::SPtr>();
+	BOOST_REQUIRE(closeSessionRequestSPtr->requestHeader()->sessionAuthenticationToken().namespaceIndex() == 1);
+	BOOST_REQUIRE(str == "12345678-9ABC-DEF0-1234-56789ABCDEF0");
+	BOOST_REQUIRE(closeSessionRequestSPtr->requestHeader()->time().dateTime() == ptime);
+	BOOST_REQUIRE(closeSessionRequestSPtr->requestHeader()->requestHandle() == 2);
+	BOOST_REQUIRE(closeSessionRequestSPtr->requestHeader()->returnDisagnostics() == 0);
+	BOOST_REQUIRE(closeSessionRequestSPtr->requestHeader()->timeoutHint() == 10000);
+	BOOST_REQUIRE(closeSessionRequestSPtr->deleteSubscriptions() == true);
+}
+
+
+BOOST_AUTO_TEST_CASE(CloseSession_Response)
+{
+#if 0
+	uint32_t pos;
+	MessageHeader::SPtr messageHeaderSPtr;
+	boost::posix_time::ptime ptime = boost::posix_time::from_iso_string("16010101T120000.000000000");
+	OpcUaGuid::SPtr opcUaGuidSPtr;
+	CloseSessionResponse::SPtr closeSessionResponseSPtr;
+	SequenceHeader::SPtr sequenceHeaderSPtr;
+	OpcUaNodeId typeId;
+
+	// stream
+	boost::asio::streambuf sb1;
+	std::iostream ios1(&sb1);
+	boost::asio::streambuf sb2;
+	std::iostream ios2(&sb2);
+	boost::asio::streambuf sb;
+	std::iostream ios(&sb);
+
+	// encode channel id
+	OpcUaUInt32 channelId;
+	channelId = 153451225;
+	OpcUaNumber::opcUaBinaryEncode(ios1, channelId);
+
+	// encode token id
+	OpcUaInt32 tokenId;
+	tokenId = 1;
+	OpcUaNumber::opcUaBinaryEncode(ios1, tokenId);
+
+	// encode sequence header
+	sequenceHeaderSPtr = SequenceHeader::construct();
+	sequenceHeaderSPtr->sequenceNumber(53);
+	sequenceHeaderSPtr->requestId(3);
+	sequenceHeaderSPtr->opcUaBinaryEncode(ios1);
+
+	// encode message type id
+	typeId.nodeId(OpcUaId_CloseSessionResponse_Encoding_DefaultBinary);
+	typeId.opcUaBinaryEncode(ios1);
+
+	// encode CloseSessionResponse
+	closeSessionResponseSPtr = CloseSessionResponse::construct();
+
+	closeSessionResponseSPtr->responseHeader()->time(ptime);
+	closeSessionResponseSPtr->responseHeader()->requestHandle(1);
+	closeSessionResponseSPtr->responseHeader()->serviceResult(Success);
+
+	closeSessionResponseSPtr->opcUaBinaryEncode(ios1);
+
+	// encode MessageHeader
+	messageHeaderSPtr = MessageHeader::construct();
+	messageHeaderSPtr->messageType(MessageType_Message);
+	messageHeaderSPtr->messageSize(OpcUaStackCore::count(sb1)+8);
+	messageHeaderSPtr->opcUaBinaryEncode(ios2);
+
+	// stream
+	ios << ios2.rdbuf() << ios1.rdbuf();
+	OpcUaStackCore::dumpHex(ios);
+
+	std::stringstream ss;
+	ss	<< "4d 53 47 46 40 00 00 00  d9 7a 25 09 01 00 00 00"
+		<< "35 00 00 00 03 00 00 00  01 00 d6 01 00 00 00 00"
+		<< "00 00 00 00 01 00 00 00  00 00 00 00 00 00 00 00"
+		<< "00 00 00 00 ff ff ff ff  00 00 00 00 00 00 00 00";
+	BOOST_REQUIRE(OpcUaStackCore::compare(ios, ss.str(), pos) == true);
+
+	// decode MessageHeader
+	messageHeaderSPtr = MessageHeader::construct();
+	messageHeaderSPtr->opcUaBinaryDecode(ios);
+	BOOST_REQUIRE(messageHeaderSPtr->messageType() == MessageType_Message);
+	
+	// decode channel id
+	OpcUaNumber::opcUaBinaryDecode(ios, channelId);
+	BOOST_REQUIRE(channelId == 153451225);
+
+	// decode token id
+	OpcUaNumber::opcUaBinaryDecode(ios, tokenId);
+	BOOST_REQUIRE(tokenId == 1);
+	
+	// decode sequence header
+	sequenceHeaderSPtr = SequenceHeader::construct();
+	sequenceHeaderSPtr->opcUaBinaryDecode(ios);
+	BOOST_REQUIRE(sequenceHeaderSPtr->sequenceNumber() == 53);
+	BOOST_REQUIRE(sequenceHeaderSPtr->requestId() == 3);
+
+	// decode message type id
+	typeId.opcUaBinaryDecode(ios);
+	BOOST_REQUIRE(typeId.namespaceIndex() == 0);
+	BOOST_REQUIRE(typeId.nodeId<OpcUaUInt32>() == OpcUaId_CloseSessionResponse_Encoding_DefaultBinary);
+
+	//decode CloseSessionResponse
+	closeSessionResponseSPtr = CloseSessionResponse::construct();
+	closeSessionResponseSPtr->opcUaBinaryDecode(ios);
+
+	BOOST_REQUIRE(closeSessionResponseSPtr->responseHeader()->time().dateTime() == ptime);
+	BOOST_REQUIRE(closeSessionResponseSPtr->responseHeader()->requestHandle() == 1);
+	BOOST_REQUIRE(closeSessionResponseSPtr->responseHeader()->serviceResult() == Success);
+	BOOST_REQUIRE(closeSessionResponseSPtr->results()->size() == 0);
+	BOOST_REQUIRE(closeSessionResponseSPtr->diagnosticInfos()->size() == 0);
+#endif
+}
+
+BOOST_AUTO_TEST_SUITE_END()
