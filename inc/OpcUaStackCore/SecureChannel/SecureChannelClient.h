d331e75b0e4506729d44aaa33938980b7fb870c6
Kai Huebl <kai@huebl-sgh.de>
2014-08-18 10:43:40 +0200

    add class Log to print out log messages

--- inc/OpcUaStackCore/SecureChannel/SecureChannelClient.h
+++ inc/OpcUaStackCore/SecureChannel/SecureChannelClient.h
@@ -1,12 +1,13 @@
 #ifndef __OpcUaStackCore_SecureChannelClient_h__
 #define __OpcUaStackCore_SecureChannelClient_h__
 
+#include "OpcUaStackCore/Base/os.h"
 #include "OpcUaStackCore/SecureChannel/SecureChannel.h"
 
 namespace OpcUaStackCore
 {
 
-	class SecureChannelClient : public SecureChannel
+	class DLLEXPORT SecureChannelClient : public SecureChannel
 	{
 	  public:
 		SecureChannelClient(void);
---
+++ src/OpcUaStackCore/Base/Log.cpp
@@ -0,0 +1,87 @@
+#include "OpcUaStackCore/Base/Log.h"
+#include <iostream>
+
+namespace OpcUaStackCore
+{
+
+	LogIf* Log::logIf_ = nullptr;
+	
+	void 
+	Log::logIf(LogIf* logIf)
+	{
+		logIf_ = logIf;
+	}
+
+	LogIf*
+	Log::logIf(void)
+	{
+		return logIf_;
+	}
+	
+	bool 
+	Log::logout(LogLevel logLevel, const std::string& message)
+	{
+		if (logIf_ == nullptr) return false;
+		return logIf_->logout(logLevel, message);
+	}
+
+	Log::Log(LogLevel logLevel, const std::string& message)
+	: logLevel_(logLevel)
+	, message_(message)
+	, parameter_("")
+	{
+	}
+
+	Log::Log(const std::string& message)
+	: logLevel_(Info)
+	, message_(message)
+	, parameter_()
+	{
+	}
+
+	Log::~Log(void)
+	{
+		if (logIf() != nullptr) {
+			std::string message = message;
+			if (parameter_ != "") {
+				message += message_ + " : " + parameter_;
+			}
+			if (logIf_->logout(logLevel_, message)) {
+				return;
+			}
+		}
+
+		std::cout << logLevel() << " " << message_;
+		if  (parameter_ != "") {
+			std::cout << ": " << parameter_;
+		}
+		std::cout << std::endl; 
+	}
+
+	std::string 
+	Log::logLevel(void)
+	{
+		switch (logLevel_)
+		{
+			case Error:		return "ERR";
+			case Warning:	return "WRN";
+			case Info:		return "INF";
+			case Debug:		return "DBG";
+			default:		return "UKN";
+		}
+		return "";
+	}
+
+	void 
+	Log::format(const std::string& parameterName, const std::string& parameterValue)
+	{
+		if (parameter_ != "") {
+			parameter_.append(", ");
+		}
+		parameter_.append(parameterName);
+		parameter_.append(" = <");
+		parameter_.append(parameterValue);
+		parameter_.append(">");
+	}
+
+}
