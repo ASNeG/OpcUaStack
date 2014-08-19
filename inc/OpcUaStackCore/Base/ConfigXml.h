5e9bb7ab0527b0f15ea9d3c18a9e02fad3895754
Kai Huebl <kai@huebl-sgh.de>
2014-08-19 11:51:02 +0200

    add class configXml

---
+++ inc/OpcUaStackCore/Base/ConfigXml.h
@@ -0,0 +1,18 @@
+#ifndef __OpcUaStackCore_ConfigXml_h__
+#define __OpcUaStackCore_ConfigXml_h__
+
+#include "OpcUaStackCore/Base/os.h"
+
+namespace OpcUaStackCore
+{
+
+	class DLLEXPORT ConfigXml
+	{
+	  public:
+		ConfigXml(void);
+		~ConfigXml(void);
+	};
+
+}
+
+#endif
---
+++ src/OpcUaStackCore/Base/ConfigXml.cpp
@@ -0,0 +1,14 @@
+#include "OpcUaStackCore/Base/ConfigXml.h"
+
+namespace OpcUaStackCore
+{
+
+	ConfigXml::ConfigXml(void)
+	{
+	}
+
+	ConfigXml::~ConfigXml(void)
+	{
+	}
+
+}
