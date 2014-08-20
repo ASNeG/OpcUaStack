3f22a9009c7a1e843769bb81fed03d86ad1f4a35
Kai Huebl <kai@huebl-sgh.de>
2014-08-20 09:49:12 +0200

    implemented base functions of class Config

--- inc/OpcUaStackCore/Base/Config.h
+++ inc/OpcUaStackCore/Base/Config.h
@@ -11,14 +11,25 @@ namespace OpcUaStackCore
 	{
 	  public:
 		Config(void);
+		Config(boost::property_tree::ptree& child);
 		~Config(void);
 
+		void child(boost::property_tree::ptree& child);
+		boost::property_tree::ptree& child(void);
+
 		bool setValue(const std::string& path, const std::string& value);
+		bool setChild(const std::string& path, Config& config);
+
+		std::string get(void);
 		boost::optional<std::string> getValue(const std::string& path);
 		std::string getValue(const std::string& path, const std::string& defaultValue);
+		boost::optional<Config> getChild(const std::string& path);
+
+		bool exist(const std::string& path);
+		bool erase(const std::string& path);
 
 	  private:
-		boost::property_tree::ptree cfg_;
+		boost::property_tree::ptree child_;
 	};
 
 }
--- src/OpcUaStackCore/Base/Config.cpp
+++ src/OpcUaStackCore/Base/Config.cpp
@@ -4,7 +4,12 @@ namespace OpcUaStackCore
 {
 
 	Config::Config(void)
-	: cfg_()
+	: child_()
+	{
+	}
+
+	Config::Config(boost::property_tree::ptree& child)
+	: child_(child)
 	{
 	}
 
@@ -12,27 +17,81 @@ namespace OpcUaStackCore
 	{
 	}
 
+	void 
+	Config::child(boost::property_tree::ptree& child)
+	{
+		child_ = child;
+	}
+
+	boost::property_tree::ptree& 
+	Config::child(void)
+	{
+		return child_;
+	}
+
 	bool 
 	Config::setValue(const std::string& path, const std::string& value)
 	{
 		boost::optional<std::string> val = getValue(path);
 		if (val) return false;
-		cfg_.put(path, value);
+		child_.put(path, value);
 		return true;
+	}
+
+	bool 
+	Config::setChild(const std::string& path, Config& config)
+	{
+		boost::optional<std::string> val = getValue(path);
+		if (val) return false;
+
+		child_.add_child(path, config.child());
+		return true;
+	}
+
+	std::string 
+	Config::get(void)
+	{
+		return child_.data();
 	}
 
 	boost::optional<std::string>
 	Config::getValue(const std::string& path)
 	{
-		return cfg_.get_optional<std::string>(path);
+		return child_.get_optional<std::string>(path);
 	}
 		
 	std::string 
 	Config::getValue(const std::string& path, const std::string& defaultValue)
 	{
-		boost::optional<std::string> val;
-		val = getValue(path);
-		if (val) return *val;
+		boost::optional<std::string> value = getValue(path);
+		if (value) return *value;
 		return defaultValue;
 	}
+
+	boost::optional<Config>
+	Config::getChild(const std::string& path)
+	{
+		boost::optional<boost::property_tree::ptree&> child = child_.get_child_optional(path);
+		if (!child) return boost::none;
+		Config config(*child);
+		return config;
+	}
+
+	bool 
+	Config::exist(const std::string& path)
+	{
+		boost::optional<std::string> value = getValue(path);
+		if (!value) return false;
+		return true;
+	}
+
+	bool 
+	Config::erase(const std::string& path)
+	{
+		boost::optional<boost::property_tree::ptree&> child = child_.get_child_optional(path);
+		if (!child) return false;
+		child_.erase(path);
+		return true;
+	}
+
 }
