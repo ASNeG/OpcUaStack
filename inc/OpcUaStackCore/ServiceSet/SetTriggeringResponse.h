55e71e397334f5323cb9ad37008bbb27dcdc204c
Samuel Hübl <samuel@huebl-sgh.de>
2014-08-22 18:03:41 +0200

    Add SetTriggering.

---
+++ inc/OpcUaStackCore/ServiceSet/SetTriggeringResponse.h
@@ -0,0 +1,43 @@
+#ifndef __OpcUaStackCore_SetTriggeringResponse_h__
+#define __OpcUaStackCore_SetTriggeringResponse_h__
+
+#include <stdint.h>
+#include "OpcUaStackCore/Base/ObjectPool.h"
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+#include "OpcUaStackCore/SecureChannel/ResponseHeader.h"
+
+namespace OpcUaStackCore
+{
+
+	class DLLEXPORT SetTriggeringResponse : public  ObjectPool<SetTriggeringResponse>
+	{
+	  public:
+		SetTriggeringResponse(void);
+		virtual ~SetTriggeringResponse(void);
+
+		void responseHeader(const ResponseHeader::SPtr responseHeader);
+		ResponseHeader::SPtr responseHeader(void) const;
+		void addResults(const OpcUaStatusCodeArray::SPtr addResults);
+		OpcUaStatusCodeArray::SPtr addResults(void) const;
+		void addDiagnosticInfos(const OpcUaDiagnosticInfoArray::SPtr addDiagnosticInfos);
+		OpcUaDiagnosticInfoArray::SPtr addDiagnosticInfos(void) const;
+		void removeResults(const OpcUaStatusCodeArray::SPtr removeResults);
+		OpcUaStatusCodeArray::SPtr removeResults(void) const;
+		void removeDiagnosticInfos(const OpcUaDiagnosticInfoArray::SPtr removeDiagnosticInfos);
+		OpcUaDiagnosticInfoArray::SPtr removeDiagnosticInfos(void) const;
+
+		void opcUaBinaryEncode(std::ostream& os) const;
+		void opcUaBinaryDecode(std::istream& is);
+
+	  private:
+		ResponseHeader::SPtr responseHeaderSPtr_;
+		OpcUaStatusCodeArray::SPtr addResultArraySPtr_;
+		OpcUaDiagnosticInfoArray::SPtr addDiagnosticInfoArraySPtr_;
+		OpcUaStatusCodeArray::SPtr removeResultArraySPtr_;
+		OpcUaDiagnosticInfoArray::SPtr removeDiagnosticInfoArraySPtr_;
+	};
+
+}
+
+#endif
---
+++ src/OpcUaStackCore/ServiceSet/SetTriggeringRequest.cpp
@@ -0,0 +1,107 @@
+#include "OpcUaStackCore/ServiceSet/SetTriggeringRequest.h"
+
+namespace OpcUaStackCore
+{
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// OpcUa SetTriggeringRequest
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+
+	SetTriggeringRequest::SetTriggeringRequest(void)
+	: ObjectPool<SetTriggeringRequest>()
+	, requestHeaderSPtr_(RequestHeader::construct())
+	, subscriptionId_()
+	, triggeringItemId_()
+	, linksToAddArraySPtr_(OpcUaUInt32Array::construct())
+	, linksToRemoveArraySPtr_(OpcUaUInt32Array::construct())
+	{
+	}
+
+	SetTriggeringRequest::~SetTriggeringRequest(void)
+	{
+	}
+	
+	void 
+	SetTriggeringRequest::requestHeader(const RequestHeader::SPtr requestHeader)
+	{
+		requestHeaderSPtr_ = requestHeader;
+	}
+	
+	RequestHeader::SPtr 
+	SetTriggeringRequest::requestHeader(void) const
+	{
+		return requestHeaderSPtr_;
+	}
+		
+	void 
+	SetTriggeringRequest::subscriptionId(const OpcUaUInt32& subscriptionId)
+	{
+		subscriptionId_ = subscriptionId;
+	}
+	
+	OpcUaUInt32
+	SetTriggeringRequest::subscriptionId(void) const
+	{
+		return subscriptionId_;
+	}
+	
+	void 
+	SetTriggeringRequest::triggeringItemId(const OpcUaUInt32& triggeringItemId)
+	{
+		triggeringItemId_ = triggeringItemId;
+	}
+	
+	OpcUaUInt32 
+	SetTriggeringRequest::triggeringItemId(void) const
+	{
+		return triggeringItemId_;
+	}
+	
+	void 
+	SetTriggeringRequest::linksToAdd(const OpcUaUInt32Array::SPtr linksToAdd)
+	{
+		linksToAddArraySPtr_ = linksToAdd;
+	}
+	
+	OpcUaUInt32Array::SPtr 
+	SetTriggeringRequest::linksToAdd(void) const
+	{
+		return linksToAddArraySPtr_;
+	}
+	
+	void 
+	SetTriggeringRequest::linksToRemove(const OpcUaUInt32Array::SPtr linksToRemove)
+	{
+		linksToRemoveArraySPtr_ = linksToRemove;
+	}
+	
+	OpcUaUInt32Array::SPtr 
+	SetTriggeringRequest::linksToRemove(void) const
+	{
+		return linksToRemoveArraySPtr_;
+	}
+	
+	void 
+	SetTriggeringRequest::opcUaBinaryEncode(std::ostream& os) const
+	{
+		requestHeaderSPtr_->opcUaBinaryEncode(os);
+		OpcUaNumber::opcUaBinaryEncode(os, subscriptionId_);
+		OpcUaNumber::opcUaBinaryEncode(os, triggeringItemId_);
+		linksToAddArraySPtr_->opcUaBinaryEncode(os);
+		linksToRemoveArraySPtr_->opcUaBinaryEncode(os);
+	}
+	
+	void 
+	SetTriggeringRequest::opcUaBinaryDecode(std::istream& is)
+	{
+		requestHeaderSPtr_->opcUaBinaryDecode(is);
+		OpcUaNumber::opcUaBinaryDecode(is, subscriptionId_);
+		OpcUaNumber::opcUaBinaryDecode(is, triggeringItemId_);
+		linksToAddArraySPtr_->opcUaBinaryDecode(is);
+		linksToRemoveArraySPtr_->opcUaBinaryDecode(is);
+	}
+}
