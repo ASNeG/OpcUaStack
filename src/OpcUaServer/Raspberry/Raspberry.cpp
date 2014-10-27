5768efac9d463d8e9ad02e042bd3a1c117a35d6f
Kai Huebl <kai@huebl-sgh.de>
2014-10-27 13:59:42 +0000

    raspberry access

--- src/OpcUaServer/Raspberry/Raspberry.cpp
+++ src/OpcUaServer/Raspberry/Raspberry.cpp
@@ -1,5 +1,9 @@
+#define RASPBERRY
+
 #include "OpcUaServer/Raspberry/Raspberry.h"
+#ifdef RASPBERRY
 #include <wiringPi.h>
+#endif
 
 namespace OpcUaServer
 {
@@ -51,14 +55,20 @@ namespace OpcUaServer
 		baseNodeClass = informationModel_->find(nodeId);
 		if (baseNodeClass.get() != nullptr) readValues(baseNodeClass, inputGpioBinaryItemVec_);
 
+#ifdef RASPBERRY
 		wiringPiSetup();
+#endif
 
 		for (it=outputGpioBinaryItemVec_.begin(); it!=outputGpioBinaryItemVec_.end(); it++) {
+#ifdef RASPBERRY
 			pinMode(it->pin_, OUTPUT);
+#endif
 		}
 
 		for (it=inputGpioBinaryItemVec_.begin(); it!=inputGpioBinaryItemVec_.end(); it++) {
+#ifdef RASPBERRY
 			pinMode(it->pin_, INPUT);
+#endif
 		}
 
 		timer_ = new boost::asio::deadline_timer(ioService_->io_service());
@@ -75,12 +85,21 @@ namespace OpcUaServer
 		if (ec) return;
 
 		for (it=outputGpioBinaryItemVec_.begin(); it!=outputGpioBinaryItemVec_.end(); it++) {
- 			int value = 1;
-			digitalWrite(it->pin_, value);
+                        bool value;
+			getValue(it->nodeIdValue_, value);
+#ifdef RASPBERRY
+			if (value) digitalWrite(it->pin_, 1);
+			else digitalWrite(it->pin_, 0);
+#endif
 		}
 
 		for (it=inputGpioBinaryItemVec_.begin(); it!=inputGpioBinaryItemVec_.end(); it++) {
-			int value = digitalRead(it->pin_);
+			int value = 0;
+#ifdef RASPBERRY
+			value = digitalRead(it->pin_);
+#endif
+                        if (value == 1) setValue(it->nodeIdValue_, true);
+			else setValue(it->nodeIdValue_, false);
 		}
 
 		timer_->expires_from_now(boost::posix_time::millisec(100));
