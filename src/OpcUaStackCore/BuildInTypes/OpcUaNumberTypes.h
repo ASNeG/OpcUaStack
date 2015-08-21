93550ee9b8cd71ad13c90e4d4f1340da7cf2ec10
Kai <kai.huebl@asneg.de>
2015-08-21 23:10:13 +0200

    added number type file

---
+++ src/OpcUaStackCore/BuildInTypes/OpcUaNumberTypes.h
@@ -0,0 +1,21 @@
+#ifndef __OpcUaStackCore_OpcUaNumberTypes_h__
+#define __OpcUaStackCore_OpcUaNumberTypes_h__
+
+namespace OpcUaStackCore
+{
+
+	typedef bool OpcUaBoolean;
+	typedef int8_t OpcUaSByte;
+	typedef uint8_t OpcUaByte;
+	typedef int16_t OpcUaInt16;
+	typedef uint16_t OpcUaUInt16;
+	typedef int32_t OpcUaInt32;
+	typedef uint32_t OpcUaUInt32;
+	typedef int64_t OpcUaInt64;
+	typedef uint64_t OpcUaUInt64;
+	typedef float OpcUaFloat;
+	typedef double OpcUaDouble;
+
+}
+
+#endif
