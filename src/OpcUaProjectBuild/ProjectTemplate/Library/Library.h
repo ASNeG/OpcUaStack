3d751a8a3391525a0fa3705ef5f5741e641d57b1
Kai <kai.huebl@asneg.de>
2015-09-04 23:45:21 +0200

    added opc ua project name macro

--- src/OpcUaProjectBuild/ProjectTemplate/Library/Library.h
+++ src/OpcUaProjectBuild/ProjectTemplate/Library/Library.h
@@ -15,28 +15,23 @@
    Autor: Kai Huebl (kai@huebl-sgh.de)
  */
 
-#ifndef __OpcUaServerApplicationDemo_DemoLibrary_h__
-#define __OpcUaServerApplicationDemo_DemoLibrary_h__
+#ifndef __OpcUaProjectName_Library_h__
+#define __OpcUaProjectName_Library_h__
 
-#include "OpcUaStackCore/Base/IOService.h"
-#include "OpcUaStackCore/Utility/SlotTimer.h"
-#include "OpcUaStackCore/Application/ApplicationReadContext.h"
-#include "OpcUaStackCore/Application/ApplicationWriteContext.h"
 #include "OpcUaStackServer/Application/ApplicationIf.h"
-#include "OpcUaStackServer/AddressSpaceModel/BaseNodeClass.h"
 
 using namespace OpcUaStackCore;
 using namespace OpcUaStackServer;
 
-namespace OpcUaServerApplicationDemo
+namespace OpcUaProjectName
 {
 
-	class DemoLibrary
+	class Library
 	: public ApplicationIf
 	{
 	  public:
-		DemoLibrary(void);
-		virtual ~DemoLibrary(void);
+		Library(void);
+		virtual ~Library(void);
 
 		//- ApplicationIf -----------------------------------------------------
 		virtual bool startup(void);
@@ -44,38 +39,6 @@ namespace OpcUaServerApplicationDemo
 		//- ApplicationIf -----------------------------------------------------
 
 	  private:
-		bool getNamespaceInfo(void);
-		void readValue(ApplicationReadContext* applicationReadContext);
-		void readLoopTimeValue(ApplicationReadContext* applicationReadContext);
-		void writeValue(ApplicationWriteContext* applicationWriteContext);
-		void writeLoopTimeValue(ApplicationWriteContext* applicationWriteContext);
-		void updateSingle(const OpcUaNodeId& nodeId, const OpcUaDataValue::SPtr dataValue, const BaseNodeClass::SPtr baseNodeClass);
-		void updateArray(const OpcUaNodeId& nodeId, const OpcUaDataValue::SPtr dataValue, const BaseNodeClass::SPtr baseNodeClass);
-
-		OpcUaDataValue::SPtr createDataValue(void);
-		bool createValueMap(void);
-		bool registerCallbacks(void);
-		bool registerLoopTimeCallbacks(void);
-		bool createNodeReferences(void);
-
-		void startTimerLoop(void);
-		void timerLoop(void);
-
-		uint32_t namespaceIndex_;
-		OpcUaDataValue::SPtr loopTime_;
-		Callback readCallback_;
-		Callback readLoopTimeCallback_;
-		Callback writeCallback_;
-		Callback writeLoopTimeCallback_;
-
-		typedef std::map<OpcUaNodeId,OpcUaDataValue::SPtr> ValueMap;
-		typedef std::map<OpcUaNodeId,BaseNodeClass::WPtr> BaseNodeClassWMap;
-		ValueMap valueMap_;
-		BaseNodeClassWMap baseNodeClassWMap_;
-
-		IOService ioService_;
-		SlotTimer slotTimer_;
-		SlotTimerElement::SPtr slotTimerElement_;
 	};
 
 }
