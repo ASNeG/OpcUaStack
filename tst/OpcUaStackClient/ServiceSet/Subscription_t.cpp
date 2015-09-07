afc8cc9f9e8fde0234b3d32a3cce646f2b832d5a
Kai Huebl <kai@huebl-sgh.de>
2015-09-08 00:14:17 +0200

    added subscriptionhandler

--- tst/OpcUaStackClient/ServiceSet/Subscription_t.cpp
+++ tst/OpcUaStackClient/ServiceSet/Subscription_t.cpp
@@ -8,6 +8,67 @@
 using namespace OpcUaStackCore;
 using namespace OpcUaStackClient;
 
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+//
+// SubscriptionServiceIf
+//
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+class SubscriptionServiceHandler
+: SubscriptionServiceIf
+{
+  public:
+	void subscriptionServiceCreateSubscriptionResponse(ServiceTransactionCreateSubscription::SPtr serviceTransactionCreateSubscription)
+	{
+		createSubscriptionResponseCondition_.conditionValueDec();
+	}
+	void subscriptionServiceModifySubscriptionResponse(ServiceTransactionModifySubscription::SPtr serviceTransactionModifySubscription)
+	{
+		modifySubscriptionCondition_.conditionValueDec();
+	}
+
+	void subscriptionServiceTransferSubscriptionsResponse(ServiceTransactionTransferSubscriptions::SPtr serviceTransactionTransferSubscriptions)
+	{
+		transferSubscriptionCondition_.conditionValueDec();
+	}
+
+	void subscriptionServiceDeleteSubscriptionsResponse(ServiceTransactionDeleteSubscriptions::SPtr serviceTransactionDeleteSubscriptions)
+	{
+		deleteSubscriptionCondition_.conditionValueDec();
+	}
+
+	Condition createSubscriptionResponseCondition_;
+	Condition modifySubscriptionCondition_;
+	Condition transferSubscriptionCondition_;
+	Condition deleteSubscriptionCondition_;
+};
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+//
+// SubscriptionManagerIf
+//
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+class SubscriptionManagerHandler
+: public SubscriptionManagerIf
+ {
+   public:
+	 void dataChangeNotification(const MonitoredItemNotification::SPtr& monitoredItem)
+	 {
+		 changeNotification_.conditionValueDec();
+	 }
+
+	 void subscriptionStateUpdate(SubscriptionState subscriptionState, uint32_t subscriptionId)
+	 {
+		 stateUpdate_.conditionValueDec();
+	 }
+
+	 Condition changeNotification_;
+	 Condition stateUpdate_;
+ };
+
 BOOST_AUTO_TEST_SUITE(Subscription_)
 
 BOOST_AUTO_TEST_CASE(Subscription_)
@@ -85,6 +146,80 @@ BOOST_AUTO_TEST_CASE(Subscription_create_delete_sync)
 	//IOService::secSleep(10000);
 	client.stop();
 	client.cleanup();
+}
+
+BOOST_AUTO_TEST_CASE(Subscription_create_delete_async)
+{
+#if 0
+	SessionTestHandler sessionTestHandler;
+
+	Client client;
+	client.init();
+	client.start();
+
+	Config sessionConfig;
+	sessionConfig.setValue("TestConfig.EndpointUrl", "opc.tcp://127.0.0.1:8888");
+	sessionConfig.setValue("TestConfig.SessionName", "urn:127.0.0.1:Company:MyAppl");
+	sessionConfig.setValue("TestConfig.ApplicationDescription.ApplicationUri", "urn:127.0.0.1:Company:MyAppl");
+	sessionConfig.setValue("TestConfig.ApplicationDescription.ProductUri", "urn:Company:MyAppl");
+	sessionConfig.setValue("TestConfig.ApplicationDescription.ApplicationName.Locale", "en");
+	sessionConfig.setValue("TestConfig.ApplicationDescription.ApplicationName.Text", "MyAppl");
+
+	Config secureChannelConfig;
+	secureChannelConfig.setValue("TestConfig.EndpointUrl", "opc.tcp://127.0.0.1:8888");
+	secureChannelConfig.setValue("TestConfig.SecurityPolicyUri", "http://opcfoundation.org/UA/SecurityPolicy#None");
+
+	Session::SPtr session = client.sessionManager().getNewSession(
+		"TestConfig", sessionConfig,
+		"TestConfig", secureChannelConfig,
+		&sessionTestHandler
+	);
+
+	// open session
+	sessionTestHandler.sessionUpdateCondition_.condition(1, 0);
+	session->open();
+	BOOST_REQUIRE(sessionTestHandler.sessionUpdateCondition_.waitForCondition(1000) == true);
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// create subscription
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	SubscriptionManager subscriptionManager;
+	subscriptionManager.componentSession(session->component());
+
+	ServiceTransactionCreateSubscription::SPtr subCreateTrx = ServiceTransactionCreateSubscription::construct();
+	CreateSubscriptionRequest::SPtr subCreateReq = subCreateTrx->request();
+	CreateSubscriptionResponse::SPtr subCreateRes = subCreateTrx->response();
+	subscriptionManager.sendSync(subCreateTrx);
+	BOOST_REQUIRE(subCreateTrx->responseHeader()->serviceResult() == Success);
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// delete subscription
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	ServiceTransactionDeleteSubscriptions::SPtr subDeleteTrx = ServiceTransactionDeleteSubscriptions::construct();
+	DeleteSubscriptionsRequest::SPtr subDeleteReq = subDeleteTrx->request();
+	DeleteSubscriptionsResponse::SPtr subDeleteRes = subDeleteTrx->response();
+	subDeleteReq->subscriptionIds()->resize(1);
+	subDeleteReq->subscriptionIds()->set(0, subCreateRes->subscriptionId());
+	subscriptionManager.sendSync(subDeleteTrx);
+	BOOST_REQUIRE(subDeleteTrx->responseHeader()->serviceResult() == Success);
+	std::cout << subDeleteRes->results()->size() << std::endl;
+	BOOST_REQUIRE(subDeleteRes->results()->size() == 1);
+	OpcUaStatusCode statusCode;
+	subDeleteRes->results()->get(0, statusCode);
+	BOOST_REQUIRE(statusCode == Success);
+
+	//IOService::secSleep(10000);
+	client.stop();
+	client.cleanup();
+#endif
 }
 
 BOOST_AUTO_TEST_SUITE_END()
