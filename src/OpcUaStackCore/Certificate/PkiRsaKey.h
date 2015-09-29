d2145f60dfa8312730412e5096c23b2d4cd71d6e
Kai Huebl <kai@huebl-sgh.de>
2015-09-29 16:05:26 +0200

    aded class PkiRsaKey

---
+++ src/OpcUaStackCore/Certificate/PkiRsaKey.h
@@ -0,0 +1,42 @@
+/*
+   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackCore_PkiRsaKey_h__
+#define __OpcUaStackCore_PkiRsaKey_h__
+
+#include <openssl/x509.h>
+#include <stdint.h>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/Certificate/PkiError.h"
+
+
+namespace OpcUaStackCore
+{
+
+	class DLLEXPORT PkiRsaKey
+	: public PkiError
+	{
+	  public:
+		PkiRsaKey(void);
+		~PkiRsaKey(void);
+
+	  private:
+	};
+
+}
+
+#endif
