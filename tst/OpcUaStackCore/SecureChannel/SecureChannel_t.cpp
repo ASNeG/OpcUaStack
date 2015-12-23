5e143764cfa5ef51c84b5411ff81814e35529269
Kai Huebl <kai@huebl-sgh.de>
2015-12-23 17:05:34 +0100

    added new test file SecureChannel_t

---
+++ tst/OpcUaStackCore/SecureChannel/SecureChannel_t.cpp
@@ -0,0 +1,14 @@
+
+#include "unittest.h"
+#include "OpcUaStackCore/SecureChannel/SecureChannel.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(SecureChannel_)
+
+BOOST_AUTO_TEST_CASE(SecureChannel)
+{
+	std::cout << "SecureChannel_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
