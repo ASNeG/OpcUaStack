5f7104d7012b5628b27220879d14a318ef74cd5c
Kai Huebl <kai@huebl-sgh.de>
2015-12-25 19:50:05 +0100

    added new class SecureChannelServerConfig

---
+++ src/OpcUaStackCore/SecureChannel/SecureChannelServerConfig.cpp
@@ -0,0 +1,53 @@
+
+/*
+   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/SecureChannel/SecureChannelServerConfig.h"
+
+namespace OpcUaStackCore
+{
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// SecureChannelServerConfig
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	SecureChannelServerConfig::SecureChannelServerConfig(void)
+	: SecureChannelConfig()
+	, endpointUrl_("")
+	{
+	}
+
+	SecureChannelServerConfig::~SecureChannelServerConfig(void)
+	{
+	}
+
+	void
+	SecureChannelServerConfig::endpointUrl(const std::string& endpointUrl)
+	{
+		endpointUrl_ = endpointUrl;
+	}
+
+	std::string&
+	SecureChannelServerConfig::endpointUrl(void)
+	{
+		return endpointUrl_;
+	}
+
+}
---
+++ src/OpcUaStackCore/SecureChannel/SecureChannelServerConfig.h
@@ -0,0 +1,45 @@
+/*
+   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf modbusTCPMaster://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+
+ */
+
+#ifndef __OpUaStackCore_SecureChannelServerConfig_h__
+#define __OpUaStackCore_SecureChannelServerConfig_h__
+
+#include "OpcUaStackCore/SecureChannel/SecureChannelConfig.h"
+
+namespace OpcUaStackCore
+{
+
+	class SecureChannelServerConfig
+	: public SecureChannelConfig
+	{
+	  public:
+		typedef boost::shared_ptr<SecureChannelServerConfig> SPtr;
+
+		SecureChannelServerConfig(void);
+		~SecureChannelServerConfig(void);
+
+		void endpointUrl(const std::string& endpointUrl);
+		std::string& endpointUrl(void);
+
+	  private:
+		std::string endpointUrl_;
+	};
+
+}
+
+#endif
