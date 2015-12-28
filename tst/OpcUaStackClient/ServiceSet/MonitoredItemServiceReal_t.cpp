99189c783968eb0e2ea73208f829c72ccb5002f6
Kai Huebl <kai@huebl-sgh.de>
2015-12-28 15:47:05 +0100

    added monitored item test

--- tst/OpcUaStackClient/ServiceSet/MonitoredItemServiceReal_t.cpp
+++ tst/OpcUaStackClient/ServiceSet/MonitoredItemServiceReal_t.cpp
@@ -9,7 +9,7 @@
 using namespace OpcUaStackCore;
 using namespace OpcUaStackClient;
 
-class MonitoredItemRealTest
+class SubscriptionRealTest
 : public SessionIf
 {
   public:
@@ -23,7 +23,7 @@ class MonitoredItemRealTest
 	}
 };
 
-class MonitoredItemRealTestSubscriptionManager
+class SubscriptionRealTestSubscriptionManager
 : public SubscriptionManagerIf
 , public SubscriptionServiceIf
 , public SubscriptionServicePublishIf
@@ -109,14 +109,14 @@ class MonitoredItemRealTestSubscriptionManager
 };
 
 
-BOOST_AUTO_TEST_SUITE(MonitoredItemReal_)
+BOOST_AUTO_TEST_SUITE(SubscriptionReal_)
 
-BOOST_AUTO_TEST_CASE(MonitoredItemReal_)
+BOOST_AUTO_TEST_CASE(SubscriptionReal_)
 {
-	std::cout << "MonitoredItemReal_t" << std::endl;
+	std::cout << "SubscriptionReal_t" << std::endl;
 }
 
-BOOST_AUTO_TEST_CASE(MonitoredItemReal_async_create_delete_subscription)
+BOOST_AUTO_TEST_CASE(SubscriptionReal_async_create_delete_subscription)
 {
 	Core core;
 	core.init();
@@ -124,111 +124,140 @@ BOOST_AUTO_TEST_CASE(MonitoredItemReal_async_create_delete_subscription)
 	IOThread ioThread;
 	ioThread.startup();
 
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	//
 	// set secure channel configuration
-	//
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
 	SecureChannelClientConfig::SPtr secureChannelClientConfig = construct<SecureChannelClientConfig>();
 	secureChannelClientConfig->endpointUrl(REAL_SERVER_URI);
 	secureChannelClientConfig->debug(false);
 	secureChannelClientConfig->debugHeader(false);
 
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	//
 	// set session configuration
-	//
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
 	SessionConfig::SPtr sessionConfig = construct<SessionConfig>();
 	sessionConfig->sessionName_ = "urn:127.0.0.1:ASNeG.de:ASNeG-Client";
 	sessionConfig->applicationDescription_->applicationUri("urn:127.0.0.1:ASNeG.de:ASNeG-Client");
 	sessionConfig->applicationDescription_->productUri("urn:ASNeG.de:ASNeG-Client");
 	sessionConfig->applicationDescription_->applicationName().set("en", "ASNeG-Client");
 
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	//
 	// init session
-	//
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	MonitoredItemRealTest monitoredItemRealTest;
+	SubscriptionRealTest subscriptionRealTest;
 	Session session(&ioThread);
-	session.sessionIf(&monitoredItemRealTest);
+	session.sessionIf(&subscriptionRealTest);
 
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	//
 	// connect session
-	//
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	monitoredItemRealTest.sessionStateUpdate_.condition(1,0);
+	subscriptionRealTest.sessionStateUpdate_.condition(1,0);
 	session.asyncConnect(sessionConfig, secureChannelClientConfig);
-	BOOST_REQUIRE(monitoredItemRealTest.sessionStateUpdate_.waitForCondition(1000) == true);
-	BOOST_REQUIRE(monitoredItemRealTest.sessionState_ == SS_Connect);
+	BOOST_REQUIRE(subscriptionRealTest.sessionStateUpdate_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(subscriptionRealTest.sessionState_ == SS_Connect);
 
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	//
 	// init subscription manager
-	//
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	MonitoredItemRealTestSubscriptionManager monitoredItemRealTestSubscriptionManager;
+	SubscriptionRealTestSubscriptionManager subscriptionRealTestSubscriptionManager;
 	SubscriptionManager subscriptionManager;
-	subscriptionManager.subscriptionManagerIf(&monitoredItemRealTestSubscriptionManager);
-	subscriptionManager.subscriptionServiceIf(&monitoredItemRealTestSubscriptionManager);
-	subscriptionManager.subscriptionServicePublishIf(&monitoredItemRealTestSubscriptionManager);
+	subscriptionManager.subscriptionManagerIf(&subscriptionRealTestSubscriptionManager);
+	subscriptionManager.subscriptionServiceIf(&subscriptionRealTestSubscriptionManager);
+	subscriptionManager.subscriptionServicePublishIf(&subscriptionRealTestSubscriptionManager);
 	subscriptionManager.componentSession(session.component());
 
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	//
 	// create subscription
-	//
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
 	ServiceTransactionCreateSubscription::SPtr createTrx = construct<ServiceTransactionCreateSubscription>();
 	CreateSubscriptionRequest::SPtr createReq = createTrx->request();
-	monitoredItemRealTestSubscriptionManager.subscriptionServiceCreateSubscriptionResponse_.condition(1,0);
+	subscriptionRealTestSubscriptionManager.subscriptionServiceCreateSubscriptionResponse_.condition(1,0);
 	subscriptionManager.send(createTrx);
-	BOOST_REQUIRE(monitoredItemRealTestSubscriptionManager.subscriptionServiceCreateSubscriptionResponse_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(subscriptionRealTestSubscriptionManager.subscriptionServiceCreateSubscriptionResponse_.waitForCondition(1000) == true);
 	BOOST_REQUIRE(createTrx->responseHeader()->serviceResult() == 0);
 	CreateSubscriptionResponse::SPtr createRes = createTrx->response();
 	uint32_t subscriptionId = createRes->subscriptionId();
 
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	//
 	// delete subscription
-	//
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
 	ServiceTransactionDeleteSubscriptions::SPtr deleteTrx = construct<ServiceTransactionDeleteSubscriptions>();
 	DeleteSubscriptionsRequest::SPtr deleteReq = deleteTrx->request();
 	deleteReq->subscriptionIds()->resize(1);
 	deleteReq->subscriptionIds()->set(0, subscriptionId);
-	monitoredItemRealTestSubscriptionManager.subscriptionServiceDeleteSubscriptionsResponse_.condition(1,0);
+	subscriptionRealTestSubscriptionManager.subscriptionServiceDeleteSubscriptionsResponse_.condition(1,0);
 	subscriptionManager.send(deleteTrx);
-	BOOST_REQUIRE(monitoredItemRealTestSubscriptionManager.subscriptionServiceDeleteSubscriptionsResponse_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(subscriptionRealTestSubscriptionManager.subscriptionServiceDeleteSubscriptionsResponse_.waitForCondition(1000) == true);
 	BOOST_REQUIRE(deleteTrx->responseHeader()->serviceResult() == 0);
 
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	//
 	// disconnect session
-	//
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	monitoredItemRealTest.sessionStateUpdate_.condition(1,0);
+	subscriptionRealTest.sessionStateUpdate_.condition(1,0);
 	session.asyncDisconnect();
-	BOOST_REQUIRE(monitoredItemRealTest.sessionStateUpdate_.waitForCondition(1000) == true);
-	BOOST_REQUIRE(monitoredItemRealTest.sessionState_ == SS_Disconnect);
+	BOOST_REQUIRE(subscriptionRealTest.sessionStateUpdate_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(subscriptionRealTest.sessionState_ == SS_Disconnect);
+
+	ioThread.shutdown();
+}
+
+BOOST_AUTO_TEST_CASE(SubscriptionReal_async_create_delete_subscription_2_subscriptions)
+{
+	Core core;
+	core.init();
+
+	IOThread ioThread;
+	ioThread.startup();
+
+	// set secure channel configuration
+	SecureChannelClientConfig::SPtr secureChannelClientConfig = construct<SecureChannelClientConfig>();
+	secureChannelClientConfig->endpointUrl(REAL_SERVER_URI);
+	secureChannelClientConfig->debug(false);
+	secureChannelClientConfig->debugHeader(false);
+
+	// set session configuration
+	SessionConfig::SPtr sessionConfig = construct<SessionConfig>();
+	sessionConfig->sessionName_ = "urn:127.0.0.1:ASNeG.de:ASNeG-Client";
+	sessionConfig->applicationDescription_->applicationUri("urn:127.0.0.1:ASNeG.de:ASNeG-Client");
+	sessionConfig->applicationDescription_->productUri("urn:ASNeG.de:ASNeG-Client");
+	sessionConfig->applicationDescription_->applicationName().set("en", "ASNeG-Client");
+
+	// init session
+	SubscriptionRealTest subscriptionRealTest;
+	Session session(&ioThread);
+	session.sessionIf(&subscriptionRealTest);
+
+	// connect session
+	subscriptionRealTest.sessionStateUpdate_.condition(1,0);
+	session.asyncConnect(sessionConfig, secureChannelClientConfig);
+	BOOST_REQUIRE(subscriptionRealTest.sessionStateUpdate_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(subscriptionRealTest.sessionState_ == SS_Connect);
+
+	// init subscription manager
+	SubscriptionRealTestSubscriptionManager subscriptionRealTestSubscriptionManager;
+	SubscriptionManager subscriptionManager;
+	subscriptionManager.subscriptionManagerIf(&subscriptionRealTestSubscriptionManager);
+	subscriptionManager.subscriptionServiceIf(&subscriptionRealTestSubscriptionManager);
+	subscriptionManager.subscriptionServicePublishIf(&subscriptionRealTestSubscriptionManager);
+	subscriptionManager.componentSession(session.component());
+
+	// create subscription
+	ServiceTransactionCreateSubscription::SPtr createTrx = construct<ServiceTransactionCreateSubscription>();
+	CreateSubscriptionRequest::SPtr createReq = createTrx->request();
+	subscriptionRealTestSubscriptionManager.subscriptionServiceCreateSubscriptionResponse_.condition(1,0);
+	subscriptionManager.send(createTrx);
+	BOOST_REQUIRE(subscriptionRealTestSubscriptionManager.subscriptionServiceCreateSubscriptionResponse_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(createTrx->responseHeader()->serviceResult() == 0);
+	CreateSubscriptionResponse::SPtr createRes = createTrx->response();
+	uint32_t subscriptionId1 = createRes->subscriptionId();
+
+	subscriptionRealTestSubscriptionManager.subscriptionServiceCreateSubscriptionResponse_.condition(1,0);
+	subscriptionManager.send(createTrx);
+	BOOST_REQUIRE(subscriptionRealTestSubscriptionManager.subscriptionServiceCreateSubscriptionResponse_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(createTrx->responseHeader()->serviceResult() == 0);
+	createRes = createTrx->response();
+	uint32_t subscriptionId2 = createRes->subscriptionId();
+
+	// delete subscription
+	ServiceTransactionDeleteSubscriptions::SPtr deleteTrx = construct<ServiceTransactionDeleteSubscriptions>();
+	DeleteSubscriptionsRequest::SPtr deleteReq = deleteTrx->request();
+	deleteReq->subscriptionIds()->resize(2);
+	deleteReq->subscriptionIds()->set(0, subscriptionId1);
+	deleteReq->subscriptionIds()->set(1, subscriptionId2);
+	subscriptionRealTestSubscriptionManager.subscriptionServiceDeleteSubscriptionsResponse_.condition(1,0);
+	subscriptionManager.send(deleteTrx);
+	BOOST_REQUIRE(subscriptionRealTestSubscriptionManager.subscriptionServiceDeleteSubscriptionsResponse_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(deleteTrx->responseHeader()->serviceResult() == 0);
+
+	// disconnect session
+	subscriptionRealTest.sessionStateUpdate_.condition(1,0);
+	session.asyncDisconnect();
+	BOOST_REQUIRE(subscriptionRealTest.sessionStateUpdate_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(subscriptionRealTest.sessionState_ == SS_Disconnect);
 
 	ioThread.shutdown();
 }
