bd17cf353df6a042e513a48528086ff93b288d10
Kai Huebl <kai@huebl-sgh.de>
2015-12-31 16:29:33 +0100

    create service set unittests

---
+++ tst/OpcUaStackClient/ServiceSet/ServiceSetManagerAsyncReal_t.cpp
@@ -0,0 +1,18 @@
+#include "unittest.h"
+
+#ifdef REAL_SERVER
+
+BOOST_AUTO_TEST_SUITE(ServiceSetManagerAsyncReal_)
+
+BOOST_AUTO_TEST_CASE(ServiceSetManagerAsyncReal_)
+{
+	std::cout << "ServiceSetManagerAsyncReal_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(ServiceSetManagerAsyncReal_xx)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif
---
+++ tst/OpcUaStackClient/ServiceSet/ServiceSetManagerSyncReal_t.cpp
@@ -0,0 +1,18 @@
+#include "unittest.h"
+
+#ifdef REAL_SERVER
+
+BOOST_AUTO_TEST_SUITE(ServiceSetManagerSyncReal_)
+
+BOOST_AUTO_TEST_CASE(ServiceSetManagerSyncReal_)
+{
+	std::cout << "ServiceSetManagerSyncReal_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(ServiceSetManagerSyncReal_xx)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif
