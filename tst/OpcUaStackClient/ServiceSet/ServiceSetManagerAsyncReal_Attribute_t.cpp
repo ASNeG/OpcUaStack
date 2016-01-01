c0b9a2d6b55a43a5c75b656ee39cdf2966be9224
Kai Huebl <kai@huebl-sgh.de>
2016-01-01 15:57:36 +0100

    added unittest for attribute service

---
+++ tst/OpcUaStackClient/ServiceSet/ServiceSetManagerAsyncReal_Attribute_t.cpp
@@ -0,0 +1,71 @@
+#include "unittest.h"
+#include "OpcUaStackClient/ServiceSet/ServiceSetManager.h"
+
+using namespace OpcUaStackClient;
+
+#ifdef REAL_SERVER
+
+BOOST_AUTO_TEST_SUITE()
+
+BOOST_AUTO_TEST_CASE(ServiceSetManagerAsyncReal_Attribute_)
+{
+	std::cout << "ServiceSetManagerAsyncReal_Attribute_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(ServiceSetManagerAsyncReal_Attribute_discovery_GetEndpoints)
+{
+	AttributeServiceIfTestHandler attributeServiceIfTestHandler;
+	ServiceSetManager serviceSetManager;
+	SessionServiceIfTestHandler sessionIfTestHandler;
+	SessionServiceConfig sessionServiceConfig;
+
+	// set secure channel configuration
+	sessionServiceConfig.sessionServiceIf_ = &sessionIfTestHandler;
+	sessionServiceConfig.secureChannelClient_->endpointUrl(REAL_SERVER_URI);
+
+	// create session
+	SessionService::SPtr sessionService;
+	sessionService = serviceSetManager.sessionService(sessionServiceConfig);
+	BOOST_REQUIRE(sessionService.get() != nullptr);
+
+	// connect secure channel
+	sessionIfTestHandler.sessionStateUpdate_.condition(1,0);
+	sessionService->asyncConnect();
+	BOOST_REQUIRE(sessionIfTestHandler.sessionStateUpdate_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(sessionIfTestHandler.sessionState_ == SS_Connect);
+
+	// create attribute service
+	AttributeService::SPtr attributeService;
+	AttributeServiceConfig attributeServiceConfig;
+	attributeServiceConfig.attributeServiceIf_ = &attributeServiceIfTestHandler;
+	attributeService = serviceSetManager.attributeService(sessionService, attributeServiceConfig);
+	BOOST_REQUIRE(attributeService.get() != nullptr);
+
+	// create and send GetEndpointsRequest
+	ServiceTransactionRead::SPtr trx;
+	trx = constructSPtr<ServiceTransactionRead>();
+	ReadRequest::SPtr req = trx->request();
+	ReadValueId::SPtr readValueIdSPtr = ReadValueId::construct();
+	readValueIdSPtr->nodeId((OpcUaInt16)0, (OpcUaInt32)2259);
+	readValueIdSPtr->attributeId((OpcUaInt32) 13);
+	readValueIdSPtr->dataEncoding().namespaceIndex((OpcUaInt16) 0);
+	req->readValueIdArray()->set(readValueIdSPtr);
+
+	attributeServiceIfTestHandler.attributeServiceReadResponse_.condition(1,0);
+	attributeService->asyncSend(trx);
+	attributeServiceIfTestHandler.attributeServiceReadResponse_.waitForCondition(1000);
+	ReadResponse::SPtr res = trx->response();
+	BOOST_REQUIRE(trx->statusCode() == Success);
+	BOOST_REQUIRE(res->dataValueArray()->size() == 1);
+
+	// disconnect secure channel
+	sessionIfTestHandler.sessionStateUpdate_.condition(1,0);
+	sessionService->asyncDisconnect();
+	BOOST_REQUIRE(sessionIfTestHandler.sessionStateUpdate_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(sessionIfTestHandler.sessionState_ == SS_Disconnect);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif
