5ee0807a163cda1e54fb13fe21a76ba6104e2705
Kai Huebl <kai@huebl-sgh.de>
2016-01-01 08:49:59 +0100

    added service manager discovery tests

--- tst/OpcUaStackClient/ServiceSet/ServiceSetManagerAsyncReal_t.cpp
+++ tst/OpcUaStackClient/ServiceSet/ServiceSetManagerAsyncReal_t.cpp
@@ -5,59 +5,14 @@ using namespace OpcUaStackClient;
 
 #ifdef REAL_SERVER
 
-BOOST_AUTO_TEST_SUITE(ServiceSetManagerAsyncReal_)
+BOOST_AUTO_TEST_SUITE(ServiceSetManagerAsyncReal_Discovery_)
 
-BOOST_AUTO_TEST_CASE(ServiceSetManagerAsyncReal_)
+BOOST_AUTO_TEST_CASE(ServiceSetManagerAsyncReal_Discovery_)
 {
-	std::cout << "ServiceSetManagerAsyncReal_t" << std::endl;
+	std::cout << "ServiceSetManagerAsyncReal_Discovery_t" << std::endl;
 }
 
-
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-//
-// unittests secure channel
-//
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE(ServiceSetManagerAsyncReal_secureChannel_connect_disconnect)
-{
-	ServiceSetManager serviceSetManager;
-	SessionServiceIfTestHandler sessionIfTestHandler;
-	SessionServiceConfig sessionServiceConfig;
-
-	// set secure channel configuration
-	sessionServiceConfig.mode_ = SessionService::M_SecureChannel;
-	sessionServiceConfig.sessionServiceIf_ = &sessionIfTestHandler;
-	sessionServiceConfig.secureChannelClient_->endpointUrl(REAL_SERVER_URI);
-
-	// create session
-	SessionService::SPtr sessionService;
-	sessionService = serviceSetManager.sessionService(sessionServiceConfig);
-	BOOST_REQUIRE(sessionService.get() != nullptr);
-
-	// connect secure channel
-	sessionIfTestHandler.sessionStateUpdate_.condition(1,0);
-	sessionService->asyncConnect();
-	BOOST_REQUIRE(sessionIfTestHandler.sessionStateUpdate_.waitForCondition(1000) == true);
-	BOOST_REQUIRE(sessionIfTestHandler.sessionState_ == SS_Connect);
-
-	// disconnect secure channel
-	sessionIfTestHandler.sessionStateUpdate_.condition(1,0);
-	sessionService->asyncDisconnect();
-	BOOST_REQUIRE(sessionIfTestHandler.sessionStateUpdate_.waitForCondition(1000) == true);
-	BOOST_REQUIRE(sessionIfTestHandler.sessionState_ == SS_Disconnect);
-}
-
-
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-//
-// unittests discovery service
-//
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE(ServiceSetManagerAsyncReal_discovery_GetEndpoints)
+BOOST_AUTO_TEST_CASE(ServiceSetManagerAsyncReal_Discovery_discovery_GetEndpoints)
 {
 	DiscoveryServiceIfTestHandler discoveryServiceIfTestHandler;
 	ServiceSetManager serviceSetManager;
