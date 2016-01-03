490d30d43cb270bf0b85393d6029fe802727efad
Kai Huebl <kai@huebl-sgh.de>
2016-01-03 22:38:32 +0100

    added nwe unittest to test sync connecting

---
+++ tst/OpcUaStackClient/ServiceSet/ServiceSetManagerSyncReal_Session_t.cpp
@@ -0,0 +1,44 @@
+#include "unittest.h"
+#include "OpcUaStackClient/ServiceSet/ServiceSetManager.h"
+
+using namespace OpcUaStackClient;
+
+#ifdef REAL_SERVER
+
+BOOST_AUTO_TEST_SUITE(ServiceSetManagerSyncReal_Session_)
+
+BOOST_AUTO_TEST_CASE(ServiceSetManagerSyncReal_Session_)
+{
+	std::cout << "ServiceSetManagerSyncReal_Session_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(ServiceSetManagerSyncReal_Session_session_connect_disconnect)
+{
+	SessionServiceIfTestHandler sessionServiceIfTestHandler;
+	ServiceSetManager serviceSetManager;
+	SessionServiceIfTestHandler sessionIfTestHandler;
+	SessionServiceConfig sessionServiceConfig;
+
+	// set secure channel configuration
+	sessionServiceConfig.sessionServiceIf_ = &sessionServiceIfTestHandler;
+	sessionServiceConfig.secureChannelClient_->endpointUrl(REAL_SERVER_URI);
+
+	// create session
+	SessionService::SPtr sessionService;
+	sessionService = serviceSetManager.sessionService(sessionServiceConfig);
+	BOOST_REQUIRE(sessionService.get() != nullptr);
+
+	// connect session
+	BOOST_REQUIRE(sessionService->syncConnect() == Success);
+
+	// disconnect session
+	sessionServiceIfTestHandler.sessionStateUpdate_.condition(1,0);
+	sessionService->asyncDisconnect();
+	BOOST_REQUIRE(sessionServiceIfTestHandler.sessionStateUpdate_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(sessionServiceIfTestHandler.sessionState_ == SS_Disconnect);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif
