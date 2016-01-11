1241abd433bfea431a12c51178294b95c1791650
Kai Huebl <kai@huebl-sgh.de>
2016-01-11 21:21:43 +0100

    add test to send sync write request

--- tst/OpcUaStackClient/ServiceSet/ServiceSetManagerSyncReal_Attribute_t.cpp
+++ tst/OpcUaStackClient/ServiceSet/ServiceSetManagerSyncReal_Attribute_t.cpp
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_CASE(ServiceSetManagerSyncReal_Attribute_)
 	std::cout << "ServiceSetManagerSyncReal_Attribute_t" << std::endl;
 }
 
-BOOST_AUTO_TEST_CASE(ServiceSetManagerSyncReal_Attribute_discovery_GetEndpoints)
+BOOST_AUTO_TEST_CASE(ServiceSetManagerSyncReal_Attribute_read)
 {
 	SessionServiceIfTestHandler sessionIfTestHandler;
 	AttributeServiceIfTestHandler attributeServiceIfTestHandler;
@@ -53,6 +53,56 @@ BOOST_AUTO_TEST_CASE(ServiceSetManagerSyncReal_Attribute_discovery_GetEndpoints)
 	ReadResponse::SPtr res = trx->response();
 	BOOST_REQUIRE(trx->statusCode() == Success);
 	BOOST_REQUIRE(res->dataValueArray()->size() == 1);
+
+	// disconnect secure channel
+	BOOST_REQUIRE(sessionService->syncDisconnect() == Success);
+}
+
+
+BOOST_AUTO_TEST_CASE(ServiceSetManagerSyncReal_Attribute_write)
+{
+	SessionServiceIfTestHandler sessionIfTestHandler;
+	AttributeServiceIfTestHandler attributeServiceIfTestHandler;
+	ServiceSetManager serviceSetManager;
+
+	// set secure channel configuration
+	SessionServiceConfig sessionServiceConfig;
+	sessionServiceConfig.sessionServiceIf_ = &sessionIfTestHandler;
+	sessionServiceConfig.secureChannelClient_->endpointUrl(REAL_SERVER_URI);
+	sessionServiceConfig.session_->sessionName(REAL_SESSION_NAME);
+
+	// create session service
+	SessionService::SPtr sessionService;
+	sessionService = serviceSetManager.sessionService(sessionServiceConfig);
+	BOOST_REQUIRE(sessionService.get() != nullptr);
+
+	// connect secure channel
+	BOOST_REQUIRE(sessionService->syncConnect() == Success);
+
+	// create attribute service
+	AttributeService::SPtr attributeService;
+	AttributeServiceConfig attributeServiceConfig;
+	attributeServiceConfig.attributeServiceIf_ = &attributeServiceIfTestHandler;
+	attributeService = serviceSetManager.attributeService(sessionService, attributeServiceConfig);
+	BOOST_REQUIRE(attributeService.get() != nullptr);
+
+	// create and send WriteRequest
+	ServiceTransactionWrite::SPtr trx;
+	trx = constructSPtr<ServiceTransactionWrite>();
+	WriteRequest::SPtr req = trx->request();
+
+	OpcUaBoolean value = 1;
+	WriteValue::SPtr writeValue = WriteValue::construct();
+	writeValue->nodeId()->set("Demo.Static.Scalar.Boolean", 2);
+	writeValue->attributeId((OpcUaInt32) 13);
+	writeValue->dataValue().variant()->set(value);
+	req->writeValueArray()->resize(1);
+	req->writeValueArray()->set(writeValue);
+
+	attributeService->syncSend(trx);
+	WriteResponse::SPtr res = trx->response();
+	BOOST_REQUIRE(trx->statusCode() == Success);
+	BOOST_REQUIRE(res->results()->size() == 1);
 
 	// disconnect secure channel
 	BOOST_REQUIRE(sessionService->syncDisconnect() == Success);
