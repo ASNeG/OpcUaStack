e02738557f3b6bd172548d793daa8a4d4a5513d4
Kai Huebl <kai@huebl-sgh.de>
2016-01-13 23:44:16 +0100

    added unittest to test vbi sync create subscription

---
+++ tst/OpcUaStackClient/ValueBasedInterface/VBISyncReal_Subscription_t.cpp
@@ -0,0 +1,37 @@
+#include "unittest.h"
+#include "OpcUaStackClient/ValueBasedInterface/VBIClient.h"
+#include "OpcUaStackClient/ValueBasedInterface/VBIClientHandlerTest.h"
+
+using namespace OpcUaStackClient;
+
+#ifdef REAL_SERVER
+
+BOOST_AUTO_TEST_SUITE(VBISyncReal_Subscription_)
+
+BOOST_AUTO_TEST_CASE(VBISyncReal_Subscription_)
+{
+	std::cout << "VBISyncReal_Subscription_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(VBISyncReal_Subscription_session_connect_disconnect)
+{
+	OpcUaStatusCode statusCode;
+	VBIClient client;
+
+	// connect session
+	ConnectContext connectContext;
+	connectContext.endpointUrl_ = REAL_SERVER_URI;
+	connectContext.sessionName_ = REAL_SESSION_NAME;
+	BOOST_REQUIRE(client.syncConnect(connectContext) == Success);
+
+	// create subscription
+	//uint32_t subscriptionId;
+	//BOOST_REQUIRE(client.syncCreateSubscription(subscriptionId) == Success);
+
+	// disconnect session
+	BOOST_REQUIRE(client.syncDisconnect() == Success);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif
