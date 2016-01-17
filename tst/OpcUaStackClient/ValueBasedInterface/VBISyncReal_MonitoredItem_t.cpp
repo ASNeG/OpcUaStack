abd1377ea219dfc9cdb9a8d4fd460d2347521b88
Kai Huebl <kai@huebl-sgh.de>
2016-01-17 19:38:29 +0100

    added data change unittests into vbi interface

--- tst/OpcUaStackClient/ValueBasedInterface/VBISyncReal_MonitoredItem_t.cpp
+++ tst/OpcUaStackClient/ValueBasedInterface/VBISyncReal_MonitoredItem_t.cpp
@@ -86,6 +86,53 @@ BOOST_AUTO_TEST_CASE(VBISyncReal_MonitoredItem_create_delete_callback)
 	BOOST_REQUIRE(client.syncDisconnect() == Success);
 }
 
+BOOST_AUTO_TEST_CASE(VBISyncReal_MonitoredItem_data_change)
+{
+	VBIClientHandlerTest vbiClientHandlerTest;
+	OpcUaStatusCode statusCode;
+	VBIClient client;
+
+	// connect session
+	ConnectContext connectContext;
+	connectContext.endpointUrl_ = REAL_SERVER_URI;
+	connectContext.sessionName_ = REAL_SESSION_NAME;
+	BOOST_REQUIRE(client.syncConnect(connectContext) == Success);
+
+	// set data change callback
+	client.setDataChangeCallback(
+		boost::bind(&VBIClientHandlerTest::dataChangeCallback, &vbiClientHandlerTest, _1, _2)
+	);
+
+	// create subscription
+	uint32_t subscriptionId;
+	BOOST_REQUIRE(client.syncCreateSubscription(subscriptionId) == Success);
+
+	// create monitored item
+	uint32_t clientHandle = 3344;
+	OpcUaNodeId nodeId;
+	nodeId.set((OpcUaUInt32)2258);
+	uint32_t monitoredItemId;
+	vbiClientHandlerTest.dataChangeCallback_.initEvent();
+	BOOST_REQUIRE(client.syncCreateMonitoredItem(nodeId, subscriptionId, clientHandle, monitoredItemId) == Success);
+	BOOST_REQUIRE(vbiClientHandlerTest.dataChangeCallback_.waitForEvent(1000) == true);
+	BOOST_REQUIRE(vbiClientHandlerTest.clientHandle_ == clientHandle);
+
+	// data change
+	for (uint32_t idx=0; idx<2; idx++) {
+		vbiClientHandlerTest.dataChangeCallback_.initEvent();
+		BOOST_REQUIRE(vbiClientHandlerTest.dataChangeCallback_.waitForEvent(3000) == true);
+	}
+
+	// delete monitored item
+	BOOST_REQUIRE(client.syncDeleteMonitoredItem(subscriptionId, monitoredItemId) == Success);
+
+	// delete subscription
+	BOOST_REQUIRE(client.syncDeleteSubscription(subscriptionId) == Success);
+
+	// disconnect session
+	BOOST_REQUIRE(client.syncDisconnect() == Success);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 #endif
