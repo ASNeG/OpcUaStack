a252295e225b87315463e1cb3346164d4532bdf3
Kai Huebl <kai@huebl-sgh.de>
2016-02-10 13:33:59 +0100

    added pki identity test

---
+++ tst/OpcUaStackCore/Certificate/PkiIdentity_t.cpp
@@ -0,0 +1,27 @@
+#include "unittest.h"
+#include "OpcUaStackCore/Certificate/PkiIdentity.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(PkiIdentity_t)
+
+BOOST_AUTO_TEST_CASE(PkiIdentity_)
+{
+	std::cout << "PkiIdentity_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(PkiIdentity_xx)
+{
+	PkiIdentity pkiIdentity;
+	pkiIdentity.organization("Company-Name");
+	pkiIdentity.organizationUnit("OPC UA Products");
+	pkiIdentity.locality("Neukirchen");
+	pkiIdentity.state("Hesse");
+	pkiIdentity.country("DE");
+	pkiIdentity.commonName("urn:Hostname:Application-Name");
+	pkiIdentity.domainComponent("Company-Name.de");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
