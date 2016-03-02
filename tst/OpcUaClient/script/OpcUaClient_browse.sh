da78666b98faf8729941c9515d501e07bc1ca9cd
Kai Huebl <kai@huebl-sgh.de>
2016-03-02 15:26:55 +0100

    browse more then one nodeid in service browse class

--- tst/OpcUaClient/script/OpcUaClient_browse.sh
+++ tst/OpcUaClient/script/OpcUaClient_browse.sh
@@ -7,3 +7,9 @@ OpcUaClient \
  -Command Connect 	-Session "TestSession" -EndpointUrl ${EndpointUrl} \
  -Command Browse \
  -Command Disconnect 
+
+
+OpcUaClient \
+ -Command Connect 	-Session "TestSession" -EndpointUrl ${EndpointUrl} \
+ -Command Browse -NodeId "i=84" -NodeId "i=85" -NodeId "i=87" \
+ -Command Disconnect
