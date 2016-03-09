c239cc879283a0e0bf3f1d2411fb17d9f8327cf2
Kai Huebl <kai@huebl-sgh.de>
2016-03-09 08:32:48 +0100

    added class CommandFunction

---
+++ src/OpcUaClient/ClientCommand/CommandFunction.cpp
@@ -0,0 +1,143 @@
+
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include <boost/lexical_cast.hpp>
+#include <sstream>
+#include "OpcUaStackCore/Base/ObjectPool.h"
+#include "OpcUaClient/ClientCommand/CommandFunction.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaClient
+{
+
+	CommandFunction::CommandFunction(void)
+	: CommandBase(CommandBase::Cmd_Write)
+	, functionNodeId_()
+	, objectNodeId_()
+	, inputValueVec_()
+	{
+	}
+
+	CommandFunction::~CommandFunction(void)
+	{
+	}
+
+	CommandBase::SPtr
+	CommandFunction::createCommand(void)
+	{
+		CommandBase::SPtr commandBase = constructSPtr<CommandFunction>();
+		return commandBase;
+	}
+
+	bool
+	CommandFunction::validateCommand(void)
+	{
+		if (functionNodeId_.nodeIdType() == OpcUaBuildInType_Unknown) {
+			std::stringstream ss;
+			ss << "neeed at least one function node id parameter";
+			errorMessage(ss.str());
+			return false;
+		}
+
+		if (objectNodeId_.nodeIdType() == OpcUaBuildInType_Unknown) {
+			std::stringstream ss;
+			ss << "neeed at least one object node id parameter";
+			errorMessage(ss.str());
+			return false;
+		}
+
+		return true;
+	}
+
+	bool
+	CommandFunction::addParameter(const std::string& parameterName, const std::string& parameterValue)
+	{
+		if (parameterName == "-FUNCTIONNODEID") {
+			if (!functionNodeId_.fromString(parameterValue)) {
+				std::stringstream ss;
+				ss << "function node id parameter invalid (" << parameterValue << ")";
+				errorMessage(ss.str());
+				return false;
+			}
+		}
+		else if (parameterName == "-OBJECTNODEID") {
+			if (!functionNodeId_.fromString(parameterValue)) {
+				std::stringstream ss;
+				ss << "function node id parameter invalid (" << parameterValue << ")";
+				errorMessage(ss.str());
+				return false;
+			}
+		}
+		else if (parameterName == "-INPUTVALUE") {
+			OpcUaVariant variant;
+			if (!variant.fromString(parameterValue)) {
+				std::stringstream ss;
+				ss << "input value parameter invalid (" << parameterValue << ")";
+				errorMessage(ss.str());
+				return false;
+			}
+
+			OpcUaDataValue::SPtr dataValue = constructSPtr<OpcUaDataValue>();
+			OpcUaDateTime dateTime(boost::posix_time::microsec_clock::universal_time());
+			dataValue->variant()->copyFrom(variant);
+			dataValue->sourceTimestamp(dateTime);
+			dataValue->serverTimestamp(dateTime);
+			dataValue->statusCode(Success);
+			inputValueVec_.push_back(dataValue);
+		}
+		else {
+			std::stringstream ss;
+			ss << "invalid parameter " << parameterName;
+			errorMessage(ss.str());
+			return false;
+		}
+		return true;
+	}
+
+	std::string
+	CommandFunction::help(void)
+	{
+		std::stringstream ss;
+		ss << "  -Function: call a opc ua server function\n"
+		   << "    -Session (0..1): Name of the session.\n"
+		   << "    -FunctionNodeId (1): Function node id\n"
+		   << "    -ObjectNodeId (1): Object node id\n"
+		   << "    -InputValue (0..N): Input values of the function\n";
+		return ss.str();
+	}
+
+	OpcUaNodeId&
+	CommandFunction::functionNodeId(void)
+	{
+		return functionNodeId_;
+	}
+
+	OpcUaNodeId&
+	CommandFunction::objectNodeId(void)
+	{
+		return objectNodeId_;
+	}
+
+	OpcUaDataValue::Vec&
+	CommandFunction::inputValueVec(void)
+	{
+		return inputValueVec_;
+	}
+
+}
---
+++ src/OpcUaClient/ClientCommand/CommandFunction.h
@@ -0,0 +1,59 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaClient_CommandFunction_h__
+#define __OpcUaClient_CommandFunction_h__
+
+#include <boost/shared_ptr.hpp>
+#include "OpcUaStackCore/BuildInTypes/OpcUaNodeId.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaDataValue.h"
+#include "OpcUaClient/ClientCommand/CommandBase.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaClient
+{
+
+	class CommandFunction
+	: public CommandBase
+	{
+	  public:
+		typedef boost::shared_ptr<CommandFunction> SPtr;
+
+		CommandFunction(void);
+		virtual ~CommandFunction(void);
+
+		//- CommandFunction interface ---------------------------------------------
+		virtual CommandBase::SPtr createCommand(void);
+		virtual bool validateCommand(void);
+		virtual bool addParameter(const std::string& parameterName, const std::string& parameterValue);
+		virtual std::string help(void);
+		//- CommandFunction interface ---------------------------------------------
+
+		OpcUaNodeId& functionNodeId(void);
+		OpcUaNodeId& objectNodeId(void);
+		OpcUaDataValue::Vec& inputValueVec(void);
+
+	  private:
+		OpcUaNodeId functionNodeId_;
+		OpcUaNodeId objectNodeId_;
+		OpcUaDataValue::Vec inputValueVec_;
+	};
+
+}
+
+#endif
