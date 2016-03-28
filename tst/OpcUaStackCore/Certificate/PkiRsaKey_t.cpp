4045fe4114df37e60ef9b26e26ec5d3efc65afef
Kai Huebl <kai@huebl-sgh.de>
2016-03-28 21:29:29 +0200

    added DSA unittests

--- tst/OpcUaStackCore/Certificate/PkiRsaKey_t.cpp
+++ tst/OpcUaStackCore/Certificate/PkiRsaKey_t.cpp
@@ -1,54 +1,33 @@
 #include "unittest.h"
-#include "OpcUaStackCore/Certificate/PkiRsaKey.h"
+#include "OpcUaStackCore/Certificate/PkiDsaKey.h"
 
 using namespace OpcUaStackCore;
 
-BOOST_AUTO_TEST_SUITE(PkiRsaKey_t)
+BOOST_AUTO_TEST_SUITE(PkiDsaKey_t)
 
-BOOST_AUTO_TEST_CASE(PkiRsaKey_)
+BOOST_AUTO_TEST_CASE(PkiDsaKey_)
 {
-	std::cout << "PkiRsaKey_t" << std::endl;
+	std::cout << "PkiDsaKey_t" << std::endl;
 }
 
-BOOST_AUTO_TEST_CASE(PkiRsaKey_createKey)
+#if 0 // FIXME: todo
+BOOST_AUTO_TEST_CASE(PkiDsaKey_createKey)
 {
-	PkiRsaKey rsaKey;
-	BOOST_REQUIRE(rsaKey.createKey(2048) == true);
+	PkiDsaKey dsaKey;
+	BOOST_REQUIRE(dsaKey.createKey(2048) == true);
 }
 
-BOOST_AUTO_TEST_CASE(PkiRsaKey_writePEM_nocrypt)
+BOOST_AUTO_TEST_CASE(PkiDsaKey_writePEM_nocrypt)
 {
-	PkiRsaKey rsaKey;
-	BOOST_REQUIRE(rsaKey.createKey(2048) == true);
-	BOOST_REQUIRE(rsaKey.writePEMFile("../tst/data/rsa_nocrypt.key", "") == true);
+	PkiDsaKey dsaKey;
+	BOOST_REQUIRE(dsaKey.createKey(2048) == true);
+	BOOST_REQUIRE(dsaKey.writePEMFile("../tst/data/dsa.key", "") == true);
 }
 
-BOOST_AUTO_TEST_CASE(PkiRsaKey_readPEM_nocrypt)
+BOOST_AUTO_TEST_CASE(PkiDsaKey_readPEM_nocrypt)
 {
-	PkiRsaKey rsaKey;
-	BOOST_REQUIRE(rsaKey.readPEMFile("../tst/data/rsa_nocrypt.key", "") == true);
-}
-
-#if 0
-BOOST_AUTO_TEST_CASE(PkiRsaKey_writePEM_crypt)
-{
-	PkiRsaKey rsaKey;
-	BOOST_REQUIRE(rsaKey.createKey(2048) == true);
-	BOOST_REQUIRE(rsaKey.writePEMFile("../tst/data/rsa_crypt.key", "passwd") == true);
-}
-
-BOOST_AUTO_TEST_CASE(PkiRsaKey_readPEM_crypt)
-{
-	PkiRsaKey rsaKey;
-	BOOST_REQUIRE(rsaKey.readPEMFile("../tst/data/rsa_crypt.key", "passwd") == false);
-	rsaKey.logError(Error, "read PEM file error");
-}
-
-BOOST_AUTO_TEST_CASE(PkiRsaKey_readPEM_crypt_error)
-{
-	PkiRsaKey rsaKey;
-	BOOST_REQUIRE(rsaKey.readPEMFile("../tst/data/rsa_crypt.key", "") == false);
-	rsaKey.logError(Error, "read PEM file error");
+	PkiDsaKey dsaKey;
+	BOOST_REQUIRE(dsaKey.readPEMFile("../tst/data/dsa.key", "") == true);
 }
 #endif
 
