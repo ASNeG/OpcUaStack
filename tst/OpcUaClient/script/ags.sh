c874fcc6d205995dc1f635e1f97fad709d135d61
Kai Huebl <kai@huebl-sgh.de>
2016-04-06 14:25:42 +0200

    change file name of nodeset

--- tst/OpcUaClient/script/ags.sh
+++ tst/OpcUaClient/script/ags.sh
@@ -6,5 +6,5 @@ EndpointUrl=opc.tcp://127.0.0.1:4880
 
 OpcUaClient \
  -Command Connect 	-Session "TestSession" -EndpointUrl ${EndpointUrl} \
- -Command NodeSetServer	-NodeSetName "NodeSet-daq.xml" -NamespaceUri "http://yourorganisation.org/Test-Server/"  
+ -Command NodeSetServer	-NodeSetName "NodeSet-ags.xml" -NamespaceUri "http://yourorganisation.org/Test-Server/"  
 # -Command Disconnect 
