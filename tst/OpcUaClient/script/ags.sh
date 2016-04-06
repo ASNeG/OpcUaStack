8a0793340e4b8aecc2c76ff0b6195a05a42449cc
Kai Huebl <kai@huebl-sgh.de>
2016-04-06 15:54:59 +0200

    change namespace uri filter

--- tst/OpcUaClient/script/ags.sh
+++ tst/OpcUaClient/script/ags.sh
@@ -6,5 +6,5 @@ EndpointUrl=opc.tcp://127.0.0.1:4880
 
 OpcUaClient \
  -Command Connect 	-Session "TestSession" -EndpointUrl ${EndpointUrl} \
- -Command NodeSetServer	-NodeSetName "NodeSet-ags.xml" -NamespaceUri "http://yourorganisation.org/Test-Server/"  
+ -Command NodeSetServer	-NodeSetName "NodeSet-ags.xml"  
 # -Command Disconnect 
--- tst/OpcUaClient/script/daq.sh
+++ tst/OpcUaClient/script/daq.sh
@@ -6,5 +6,6 @@ EndpointUrl=opc.tcp://192.168.50.151:14000
 
 OpcUaClient \
  -Command Connect 	-Session "TestSession" -EndpointUrl ${EndpointUrl} \
- -Command NodeSetServer	-NodeSetName "NodeSet-daq.xml" -NamespaceUri "http://yourorganisation.org/Test-Server/"  
+ -Command NodeSetServer	-NodeSetName "NodeSet-daq.xml" -NamespaceUri "http://BakerHughes.com/DAQ/HSE/"   
 # -Command Disconnect 
+
