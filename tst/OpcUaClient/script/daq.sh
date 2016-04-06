4d6026b0da2d3f60bed5bf3a76740e510d3008cd
Kai Huebl <kai@huebl-sgh.de>
2016-04-06 14:04:44 +0200

    added daq test

---
+++ tst/OpcUaClient/script/daq.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. ./TestConfig.sh
+EndpointUrl=opc.tcp://192.168.50.151:14000
+
+
+OpcUaClient \
+ -Command Connect 	-Session "TestSession" -EndpointUrl ${EndpointUrl} \
+ -Command NodeSetServer	-NodeSetName "NodeSet-daq.xml" -NamespaceUri "http://yourorganisation.org/Test-Server/"  
+# -Command Disconnect 
