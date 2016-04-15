3f1ebcf3668a11c171a9019019f6f2da7fd8bd52
Kai Huebl <kai@huebl-sgh.de>
2016-04-15 22:15:03 +0200

    added method call service

--- src/OpcUaStackServer/ServiceSet/MethodService.cpp
+++ src/OpcUaStackServer/ServiceSet/MethodService.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2016 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -15,7 +15,10 @@
    Autor: Kai Huebl (kai@huebl-sgh.de)
  */
 
+#include "OpcUaStackCore/Base/Log.h"
 #include "OpcUaStackCore/BuildInTypes/OpcUaIdentifier.h"
+#include "OpcUaStackCore/Application/ApplicationCallContext.h"
+#include "OpcUaStackCore/ServiceSet/MethodServiceTransaction.h"
 #include "OpcUaStackServer/ServiceSet/MethodService.h"
 
 namespace OpcUaStackServer
@@ -47,8 +50,97 @@ namespace OpcUaStackServer
 	void 
 	MethodService::receiveCallRequest(ServiceTransaction::SPtr serviceTransaction)
 	{
-		// FIXME:
-		serviceTransaction->statusCode(BadInternalError);
+		ServiceTransactionCall::SPtr trx = boost::static_pointer_cast<ServiceTransactionCall>(serviceTransaction);
+
+		CallRequest::SPtr callRequest = trx->request();
+		CallResponse::SPtr callResponse = trx->response();
+
+		Log(Debug, "method service call request")
+			.parameter("Trx", serviceTransaction->transactionId())
+			.parameter("NumberCalls", callRequest->methodsToCall()->size());
+
+		// check node id array
+		if (callRequest->methodsToCall()->size() == 0) {
+			trx->statusCode(BadNothingToDo);
+			trx->componentSession()->send(serviceTransaction);
+			return;
+		}
+		if (callRequest->methodsToCall()->size() > 1000) { // FIXME: todo
+			trx->statusCode(BadTooManyOperations);
+			trx->componentSession()->send(serviceTransaction);
+			return;
+		}
+
+		// call methods
+		callResponse->results()->resize(callRequest->methodsToCall()->size());
+		for (uint32_t idx = 0; idx < callRequest->methodsToCall()->size(); idx++) {
+			CallMethodResult::SPtr callMethodResult = constructSPtr<CallMethodResult>();
+			callResponse->results()->set(idx, callMethodResult);
+
+			// determine node information
+			CallMethodRequest::SPtr callMethod;
+			if (!callRequest->methodsToCall()->get(idx, callMethod)) {
+				callMethodResult->statusCode(BadNodeIdInvalid);
+				Log(Debug, "call method error, because call request parameter invalid")
+					.parameter("Trx", serviceTransaction->transactionId())
+					.parameter("Idx", idx);
+				continue;
+			}
+
+			// find object node class instance for the node
+			BaseNodeClass::SPtr baseNodeClass = informationModel_->find(callMethod->objectId());
+			if (baseNodeClass.get() == nullptr) {
+				callMethodResult->statusCode(BadNodeIdUnknown);
+				Log(Debug, "call method error, because object node not exist in information model")
+					.parameter("Trx", serviceTransaction->transactionId())
+					.parameter("Idx", idx)
+					.parameter("ObjectNode", *callMethod->objectId());
+				continue;
+			}
+
+			// check if forward callback exists
+			ForwardInfoSync::SPtr forwardInfoSync = baseNodeClass->forwardInfoSync();
+			if (forwardInfoSync.get() == nullptr) {
+				callMethodResult->statusCode(BadServiceUnsupported);
+				Log(Debug, "call method error, because service not supported")
+					.parameter("Trx", serviceTransaction->transactionId())
+					.parameter("Idx", idx)
+					.parameter("ObjectNode", *callMethod->objectId());
+				continue;
+			}
+			if (!forwardInfoSync->isMethodCallback()) {
+				callMethodResult->statusCode(BadServiceUnsupported);
+				Log(Debug, "call method error, because service not supported")
+					.parameter("Trx", serviceTransaction->transactionId())
+					.parameter("Idx", idx)
+					.parameter("ObjectNode", *callMethod->objectId());
+				continue;
+			}
+
+			// call forward calbacks
+			ApplicationCallContext applicationCallContext;
+			applicationCallContext.objectNodeId_ = *callMethod->objectId();
+			applicationCallContext.methodNodeId_ = *callMethod->methodId();
+			applicationCallContext.inputArguments_ = callMethod->inputArguments();
+			applicationCallContext.statusCode_ = Success;
+			applicationCallContext.applicationContext_ = forwardInfoSync->applicationContext();
+			forwardInfoSync->methodCallback()(&applicationCallContext);
+
+			// check response
+			callMethodResult->statusCode(applicationCallContext.statusCode_);
+			if (applicationCallContext.statusCode_ != Success) {
+				Log(Debug, "call value error, because service process failed")
+					.parameter("Trx", serviceTransaction->transactionId())
+					.parameter("Idx", idx)
+					.parameter("ObjectNode", *callMethod->objectId())
+					.parameter("StatusCode", OpcUaStatusCodeMap::shortString(applicationCallContext.statusCode_));
+				continue;
+			}
+
+			// process response
+			callMethodResult->outputArguments(applicationCallContext.outputArguments_);
+		}
+
 		serviceTransaction->componentSession()->send(serviceTransaction);
 	}
 
