7cdea2fa59716e297ed36f04d147753222a5344d
Kai Huebl <kai@huebl-sgh.de>
2016-04-26 19:39:01 +0200

    added new class Function

---
+++ src/OpcUaServerApplicationDemo/DemoLibrary/Function.cpp
@@ -0,0 +1,121 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaStackCore/Base/ConfigXml.h"
+#include "OpcUaStackCore/Utility/Environment.h"
+#include "OpcUaStackServer/ServiceSetApplication/ApplicationService.h"
+#include "OpcUaStackServer/ServiceSetApplication/NodeReferenceApplication.h"
+#include "OpcUaServerApplicationDemo/DemoLibrary/Function.h"
+
+namespace OpcUaServerApplicationDemo
+{
+
+	Function::Function(void)
+	: ioThread_(nullptr)
+	, applicationServiceIf_(nullptr)
+	, applicationInfo_(nullptr)
+	, namespaceIndex_(0)
+	, baseNodeClassWMap_()
+	{
+	}
+
+	Function::~Function(void)
+	{
+	}
+
+	bool
+	Function::startup(
+		IOThread& ioThread,
+		ApplicationServiceIf& applicationServiceIf,
+		ApplicationInfo* applicationInfo
+	)
+	{
+		Log(Debug, "Function::startup");
+
+		ioThread_ = &ioThread;
+		applicationServiceIf_ = &applicationServiceIf;
+		applicationInfo_ = applicationInfo;
+
+		// read namespace array from opc ua server
+		if (!getNamespaceInfo()) {
+			return false;
+		}
+
+		// register functions
+		if (!registerFunctions()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	bool
+	Function::shutdown(void)
+	{
+		Log(Debug, "Function::shutdown");
+
+		return true;
+	}
+
+	bool
+	Function::getNamespaceInfo(void)
+	{
+		Log(Debug, "get namespace info");
+
+		ServiceTransactionNamespaceInfo::SPtr trx = ServiceTransactionNamespaceInfo::construct();
+		NamespaceInfoRequest::SPtr req = trx->request();
+		NamespaceInfoResponse::SPtr res = trx->response();
+
+		applicationServiceIf_->sendSync(trx);
+		if (trx->statusCode() != Success) {
+			Log(Error, "NamespaceInfoResponse error")
+			    .parameter("StatusCode", OpcUaStatusCodeMap::shortString(trx->statusCode()));
+			return false;
+		}
+
+		NamespaceInfoResponse::Index2NamespaceMap::iterator it;
+		for (
+		    it = res->index2NamespaceMap().begin();
+			it != res->index2NamespaceMap().end();
+			it++
+		)
+		{
+			if (it->second == "http://ASNeG.de/Function/") {
+				namespaceIndex_ = it->first;
+				return true;
+			}
+ 		}
+
+		Log(Error, "namespace not found in configuration")
+	        .parameter("NamespaceUri", "http://ASNeG.de/Function/");
+
+		return false;
+	}
+
+	bool
+	Function::registerFunctions(void)
+	{
+		Log(Debug, "register functions");
+
+		return true;
+	}
+
+}
