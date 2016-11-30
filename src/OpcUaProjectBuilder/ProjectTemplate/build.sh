f86fb19c50f8251183c6bf0fdc4bc57244fd8b78
Kai Huebl <kai@huebl-sgh.de>
2016-11-30 09:13:39 +0100

    rework build script

--- src/OpcUaProjectBuilder/ProjectTemplate/build.sh
+++ src/OpcUaProjectBuilder/ProjectTemplate/build.sh
@@ -15,10 +15,29 @@ CMAKE_GENERATOR_LOCAL=-G"Eclipse CDT4 - Unix Makefiles"
 
 usage()
 {
-   echo "build.sh [info, local, deb]"
+   echo "build.sh <Command>"
+   echo ""
+   echo "Commands:"
+   echo "  info" 
+   echo "    create version and dependency files"
+   echo "  local [renew]"
+   echo "    create or renew local build and install in folder ${HOME}/install"
+   echo "  deb [renew]"
+   echo "    create or renew deb package"
+   echo "  rpm"
+   echo "    create or renew rpm package"
+   echo "  clean"
+   echo "    delete all build directories"
 }
 
 
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+#
+# create version and dependency files
+#
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 version_info()
 {
     echo "version info"
@@ -34,6 +53,14 @@ version_info_clean()
     rm -rf build_info
 }
 
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# 
+# build and install local
+#
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 build_local()
 {
     echo "build local start"
@@ -43,7 +70,7 @@ build_local()
     mkdir build_local
     cd build_local
 
-    # install build local
+    # build local
     : ${OPCUASTACK_INSTALL_PREFIX:=${HOME}/install}
     set -x
     cmake ../src \
@@ -55,6 +82,41 @@ build_local()
         echo "cmake error"
         return $?
     fi
+
+    # install local
+    make DESTDIR="${HOME}/install" install
+     if [ $? -ne 0 ] ;
+    then
+        echo "make install error"
+        return $?
+    fi
+
+    return 0
+}
+
+build_local_renew()
+{
+    echo "renew local start"
+
+    # build local directory
+    if [ ! -d "build_local" ] ; 
+    then
+        echo "error - directory build_local do not exist"
+        return 1;
+    fi
+    cd build_local
+
+    # build local
+    set -x
+    cmake .
+    set +x
+    if [ $? -ne 0 ] ;
+    then
+        echo "cmake error"
+        return $?
+    fi
+
+    # install local
     make DESTDIR="${HOME}/install" install
      if [ $? -ne 0 ] ;
     then
@@ -72,6 +134,13 @@ build_local_clean()
 }
 
 
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+#
+# build deb package
+#
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 build_deb()
 {
     echo "build deb package start"
@@ -113,11 +182,50 @@ build_deb()
 }
 
 
+build_deb_renew()
+{
+    echo "renew deb package start"
+
+    # build package directory
+    if [ ! -d "build_deb" ] ; 
+    then
+        echo "error - directory build_deb do not exist"
+        return 1;
+    fi
+    cd build_deb
+
+    # build package
+    cmake .
+    if [ $? -ne 0 ] ;
+    then
+        echo "cmake error"
+        return $?
+    fi
+    make package
+    if [ $? -ne 0 ] ;
+    then
+        echo "make package error"
+        return $?
+    fi
+
+    return 0
+}
+
+
 build_deb_clean()
 {
     rm -rf build_deb
 }
 
+
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+#
+# build rpm package
+#
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 build_rpm()
 {
     echo "build rpm package start"
@@ -158,6 +266,47 @@ build_rpm()
 
     return 0
 }
+
+
+build_rpm_renew()
+{
+    echo "build rpm package start"
+
+    # check if rpm package to be produced
+    if which rpmbuild > /dev/null ;
+    then
+        echo "build rpm package enable"
+    else
+        echo "build rpm package disable"
+        return -1
+    fi
+    
+    # build package directory
+    if [ ! -d "build_rpm" ] ; 
+    then
+        echo "error - directory build_rpm do not exist"
+        return 1;
+    fi
+    cd build_rpm
+
+    # build package
+    echo "${CPACK_BINARY}"
+    cmake .
+    if [ $? -ne 0 ] ;
+    then
+        echo "cmake error"
+        return $?
+    fi
+    make package
+    if [ $? -ne 0 ] ;
+    then
+        echo "make package error"
+        return $?
+    fi
+
+    return 0
+}
+
 
 
 build_rpm_clean()
@@ -221,28 +370,59 @@ clean()
 #
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
-if [ $# -ne 1 ] ; 
+if [ $# -eq 1 ] ; 
 then
+    RENEW=0
+elif [ $# -eq 2 ] ;
+then
+    if [ "$2" = "renew" ] ;
+    then
+        RENEW=1
+    else
+        usage
+        exit 1
+    fi
+else
     usage
-    exit
-fi
+    exit 1
+fi 
 
 if [ "$1" = "info" ] ;
 then
     version_info
     exit $?
+elif [ "$1" = "clean" ] ;
+then 
+    clean 
 elif [ "$1" = "local" ] ;
 then 
-    build_local
+    if [ ${RENEW} -eq 1 ] ;
+    then
+        build_local_renew
+    else
+        build_local
+    fi 
     exit $?
 elif [ "$1" = "deb" ] ;
 then 
-    build_deb
+    if [ ${RENEW} -eq 1 ] ;
+    then
+        build_deb_renew
+    else
+        build_deb 
+    fi
     exit $?
 elif [ "$1" = "rpm" ] ;
 then 
-    build_rpm
+    if [ ${RENEW} -eq 1 ] ;
+    then
+        build_rpm_renew
+    else 
+        build_rpm
+    fi 
     exit $?
 else
     usage
 fi
+
+exit 0
