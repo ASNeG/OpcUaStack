834b886b9adb1605d9da339d17b1e9c7347c9a43
Kai Huebl <kai@huebl-sgh.de>
2016-12-11 19:25:33 +0100

    added getter base node class function

--- src/OpcUaStackServer/ServiceSet/MonitorItem.cpp
+++ src/OpcUaStackServer/ServiceSet/MonitorItem.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2016 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -193,6 +193,13 @@ namespace OpcUaStackServer
 		if (actQueueSize >= queSize_) return;
 		monitoredItemNotification->clientHandle(clientHandle_);
 		monitorItemList_.push_back(monitoredItemNotification);
+	}
+
+	BaseNodeClass::SPtr
+	MonitorItem::baseNodeClass(void)
+	{
+		BaseNodeClass::SPtr baseNodeClass = baseNodeClass_.lock();
+		return baseNodeClass;
 	}
 
 }
--- src/OpcUaStackServer/ServiceSet/MonitorItem.h
+++ src/OpcUaStackServer/ServiceSet/MonitorItem.h
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2016 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -53,6 +53,7 @@ namespace OpcUaStackServer
 		uint32_t samplingInterval(void);
 		uint32_t queSize(void);
 		uint32_t size(void);
+		BaseNodeClass::SPtr baseNodeClass(void);
 
 		SlotTimerElement::SPtr slotTimerElement(void);
 		MonitoredItemCreateRequest::SPtr monitoredItemCreateRequest(void);
