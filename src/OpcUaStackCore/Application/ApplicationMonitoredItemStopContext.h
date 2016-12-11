d651ef61c53b2d67c18e72b082b8c1b4b96ce823
Kai Huebl <kai@huebl-sgh.de>
2016-12-11 19:34:32 +0100

    added new class ApplicationMonitoredItemStopContext

---
+++ src/OpcUaStackCore/Application/ApplicationMonitoredItemStopContext.h
@@ -0,0 +1,37 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackCore_ApplicationMonitoredItemStopContext_h__
+#define __OpcUaStackCore_ApplicationMonitoredItemStopContext_h__
+
+#include "OpcUaStackCore/Base/BaseClass.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaNodeId.h"
+
+namespace OpcUaStackCore
+{
+
+	class ApplicationMonitoredItemStopContext
+	{
+	  public:
+		BaseClass::SPtr applicationContext_;	// IN - application context from register call
+		OpcUaNodeId nodeId_;					// IN - node id to be monitored
+		uint32_t monitoredItemId_;				// IN - id of the monitored item
+	};
+
+}
+
+#endif
