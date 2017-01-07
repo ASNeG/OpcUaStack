88a36b49a4b7ae999245dd17eafbc6b5599b293e
Kai Huebl <kai@huebl-sgh.de>
2017-01-07 10:50:31 +0100

    added complex data value test

---
+++ tst/OpcUaStackCore/ComplexDataTypes/ComplexDataValue_t.cpp
@@ -0,0 +1,14 @@
+#include "unittest.h"
+#include "OpcUaStackCore/ComplexDataTypes/ComplexDataValue.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(ComplexDataValue_t)
+
+
+BOOST_AUTO_TEST_CASE(ComplexDataValue_cccc)
+{
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
