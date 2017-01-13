c5c8a59abd524a45ee2d608bf215576df99f3e3a
Kai Huebl <kai@huebl-sgh.de>
2017-01-13 11:09:18 +0100

    added complex data parser unittest

---
+++ tst/OpcUaStackCore/ComplexDataTypes/ComplexDataParser_t.cpp
@@ -0,0 +1,15 @@
+#include "unittest.h"
+#include "OpcUaStackCore/ComplexDataTypes/ComplexDataTypeParser.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(ComplexDataTypeParser_t)
+
+BOOST_AUTO_TEST_CASE(ComplexDataTypeParser_xx)
+{
+	ComplexDataTypeParser parser;
+
+	BOOST_REQUIRE(parser.parse("../tst/data/OpcUaComplexDataTypes.xml") == true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
