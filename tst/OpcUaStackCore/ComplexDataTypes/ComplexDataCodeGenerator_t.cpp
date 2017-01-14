910ee60eede8822642fae3d47e854653b42022ba
Kai Huebl <kai@huebl-sgh.de>
2017-01-14 20:49:24 +0100

    added first code generation test

--- tst/OpcUaStackCore/ComplexDataTypes/ComplexDataCodeGenerator_t.cpp
+++ tst/OpcUaStackCore/ComplexDataTypes/ComplexDataCodeGenerator_t.cpp
@@ -8,11 +8,21 @@ BOOST_AUTO_TEST_SUITE(ComplexDataCodeGenerator_t)
 
 BOOST_AUTO_TEST_CASE(ComplexDataCodeGenerator_CodeGenerator)
 {
-	ComplexDataType::SPtr type;
+	// parse configuration
 	ComplexDataTypeParser parser;
-
 	BOOST_REQUIRE(parser.parse("../tst/data/OpcUaComplexDataTypes.xml") == true);
 
+	// get type
+	ComplexDataType::SPtr type;
+	type = parser.complexDataType("MyComplexType1");
+	BOOST_REQUIRE(type.get() != nullptr);
+
+	// run code generator
+	ComplexDataCodeGenerator generator;
+	generator.classTemplateFile("../tst/data/OpcUaComplexDataType.h.tmpl");
+	BOOST_REQUIRE(generator.generate(*type) == true);
+
+	std::cout << generator.content() << std::endl;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
