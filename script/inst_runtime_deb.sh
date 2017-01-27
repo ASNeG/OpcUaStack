d7eba4c214495305bedfd14f6eb03a716ccaa349
Kai Huebl <kai@huebl-sgh.de>
2017-01-27 13:24:30 +0100

    added runtime installation script

---
+++ script/inst_runtime_deb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get install libssl1.0.0
+apt-get install libboost-system1.54.0
+apt-get install libboost-filesystem1.54.0
+apt-get install libboost-thread1.54.0
+apt-get install libboost-date-time1.54.0
+apt-get install libboost-chrono1.54.0
+apt-get install libboost-regex1.54.0
