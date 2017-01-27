78f16a723bc1465ac80f2a94c5f0b95f6e3b211e
Kai Huebl <kai@huebl-sgh.de>
2017-01-27 14:40:47 +0100

    use environment variables in build process

--- script/instenv_runtime_deb.sh
+++ script/instenv_runtime_deb.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+#
+# list of possible environment variables
+#   - OPENSSL_VERSION_MAJOR
+#   - OPENSSL_VERSION_MINOR
+#   - OPENSSL_VERSION_PATCH
+#   - BOOST_VERSION_MAJOR
+#   - BOOST_VERSION_MINOR
+#
 
-apt-get install libssl1.0.0
-apt-get install libboost-system1.54.0
-apt-get install libboost-filesystem1.54.0
-apt-get install libboost-thread1.54.0
-apt-get install libboost-date-time1.54.0
-apt-get install libboost-chrono1.54.0
-apt-get install libboost-regex1.54.0
+OPENSSL_VERSION_MAJOR_=${OPENSSL_VERSION_MAJOR:-1}
+OPENSSL_VERSION_MINOR_=${OPENSSL_VERSION_MINOR:-0}
+OPENSSL_VERSION_PATCH_=${OPENSSL_VERSION_PATCH:-0}
+
+BOOST_VERSION_MAJOR_=${BOOST_VERSION_MAJOR:-1}
+BOOST_VERSION_MINOR_=${BOOST_VERSION_MINOR:-54}
+
+VERSION_OPENSSL=${OPENSSL_VERSION_MAJOR}.${OPENSSL_VERSION_MINOR_}.${OPENSSL_VERSION_PATCH_}
+VERSION_BOOST=${BOOST_VERSION_MAJOR_}.${BOOST_VERSION_MINOR_}.0
+
+apt-get install libboost-system${VERSION_BOOST}
+apt-get install libboost-filesystem${VERSION_BOOST}
+apt-get install libboost-thread${VERSION_BOOST}
+apt-get install libboost-date-time${VERSION_BOOST}
+apt-get install libboost-chrono${VERSION_BOOST}
+apt-get install libboost-regex${VERSION_BOOST}
+apt-get install libssl${VERSION_OPENSSL}
+
