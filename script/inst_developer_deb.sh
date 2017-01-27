77421553b950c6f6ee0bc8d9dd777bc3bea8bde4
Kai Huebl <kai@huebl-sgh.de>
2017-01-27 13:26:09 +0100

    added developer installation script

---
+++ script/inst_developer_deb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install libssl-dev
+apt-get install libboost-test
+apt-get install libboost-system1.54.0-dev
+apt-get install libboost-filesystem1.54.0-dev
+apt-get install libboost-thread1.54.0-dev
+apt-get install libboost-date-time1.54.0-dev
+apt-get install libboost-chrono1.54.0-dev
+apt-get install libboost-regex1.54.0-dev
