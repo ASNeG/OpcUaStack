3e749965d1e8a1945c92ba80c7ff2308438442e3
Kai Huebl <kai@huebl-sgh.de>
2017-02-03 08:39:41 +0100

    remove header encoder/decoder

--- src/OpcUaStackCore/ServiceSet/ApplicationDescription.h
+++ src/OpcUaStackCore/ServiceSet/ApplicationDescription.h
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -41,6 +41,7 @@ namespace OpcUaStackCore
 	{
 	  public:
 		typedef boost::shared_ptr<ApplicationDescription> SPtr;
+		typedef std::vector<ApplicationDescription::SPtr> Vec;
 
 		ApplicationDescription(void);
 		virtual ~ApplicationDescription(void);
