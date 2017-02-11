5dfc689fc5e1d6d26767372d85a07cbc985ab9aa
Kai Huebl <kai@huebl-sgh.de>
2017-02-11 19:58:59 +0100

    remove unused source code

--- src/OpcUaStackCore/BuildInTypes/OpcUaDateTime.cpp
+++ src/OpcUaStackCore/BuildInTypes/OpcUaDateTime.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -95,18 +95,6 @@ namespace OpcUaStackCore
 	bool 
 	OpcUaDateTime::fromISOString(const std::string& dateTimeString)
 	{
-#if 0
-		std::stringstream ss;
-
-		// do not delete this memory by yourself
-		boost::posix_time::time_input_facet* facet = new boost::posix_time::time_input_facet();
-		facet->set_iso_extended_format();
-		ss.imbue(std::locale(ss.getloc(), facet));
-		ss.str(dateTimeString);
-		boost::posix_time::ptime timeFromString; 
-		ss >> timeFromString;
-#endif
-
 		boost::posix_time::ptime timeFromString;
 		try {
 		    timeFromString = boost::posix_time::from_iso_string(dateTimeString);
