e33cacba4baf26ee45da62f4a431855d07ee78f1
Kai Huebl <kai@huebl-sgh.de>
2017-02-13 15:41:50 +0100

    added unittest to check function from Address class

---
+++ tst/OpcUaStackCore/Base/Address_t.cpp
@@ -0,0 +1,23 @@
+#include "unittest.h"
+#include "OpcUaStackCore/Base/Address.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(Address_t)
+
+BOOST_AUTO_TEST_CASE(Address_)
+{
+	std::cout << "Address_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(Address_HostName)
+{
+	std::cout << "HostName=" << Address::hostName() << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(Address_decode)
+{
+	Address::getAllIPv4sFromHost();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
