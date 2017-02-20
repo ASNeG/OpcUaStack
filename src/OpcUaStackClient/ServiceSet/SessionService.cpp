15ce256c90150b9c6ab9839399c1bd28f3b9f6e7
Kai Huebl <kai@huebl-sgh.de>
2017-02-20 15:29:47 +0100

    fix bug in register server request

--- src/OpcUaStackClient/ServiceSet/SessionService.cpp
+++ src/OpcUaStackClient/ServiceSet/SessionService.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -517,7 +517,8 @@ namespace OpcUaStackClient
 
 		if (!sessionConnect_ &&
 			requestType != OpcUaId_GetEndpointsRequest_Encoding_DefaultBinary &&
-			requestType != OpcUaId_FindServersRequest_Encoding_DefaultBinary
+			requestType != OpcUaId_FindServersRequest_Encoding_DefaultBinary &&
+			requestType != OpcUaId_RegisterServerRequest_Encoding_DefaultBinary
 		) {
 			serviceTransaction->statusCode(BadSessionClosed);
 			Component* componentService = serviceTransaction->componentService();
