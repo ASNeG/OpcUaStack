d45d76ef1dbc41b10ff42b69885e2d12869776f7
Kai Huebl <kai@huebl-sgh.de>
2017-03-09 11:21:54 +0100

    added new X509 unittest

---
+++ tst/OpcUaStackCore/Certificate/OpcUaX509_t.cpp
@@ -0,0 +1,20 @@
+#include "unittest.h"
+#include "OpcUaStackCore/Certificate/OpcUaX509.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(OpcUaX509_t)
+
+BOOST_AUTO_TEST_CASE(OpcUaX509_)
+{
+	std::cout << "OpcUaX509_t" << std::endl;
+}
+
+
+BOOST_AUTO_TEST_CASE(OpcUaX509_xx)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
