004c4c84a2efecdafc740a94e424fafbdac7d26d
Kai Huebl <kai@huebl-sgh.de>
2017-04-11 17:36:59 +0200

    implemented complex type test

--- tst/OpcUaStackServer/NodeSet/DataTypeDefinition_t.cpp
+++ tst/OpcUaStackServer/NodeSet/DataTypeDefinition_t.cpp
@@ -241,6 +241,38 @@ BOOST_AUTO_TEST_CASE(DataTypeDefinition_write_Structure_Simple)
     BOOST_REQUIRE(definition->dataFields()[2]->description() == OpcUaLocalizedText());
 }
 
+BOOST_AUTO_TEST_CASE(DataTypeDefinition_read_Structure_Complex)
+{
+	bool success;
+
+	// read opc ua nodeset
+	ConfigXml configXml;
+    success = configXml.parse("../tst/data/DataTypeDefinition.Structure.Complex.xml");
+    BOOST_REQUIRE(success == true);
+
+    // decode simple enum type
+    DataTypeDefinition::SPtr definition = constructSPtr<DataTypeDefinition>();
+    definition->dataSubType(Structure);
+    success = definition->decode(configXml.ptree().get_child("Definition"));
+
+    BOOST_REQUIRE(success == true);
+    BOOST_REQUIRE(definition->name() == OpcUaQualifiedName("MyStructureType",1));
+    BOOST_REQUIRE(definition->dataFields().size() == 3);
+    BOOST_REQUIRE(definition->dataFields()[0]->name().value() == "Element1");
+    BOOST_REQUIRE(definition->dataFields()[0]->isOptional() == false);
+    BOOST_REQUIRE(definition->dataFields()[0]->dataType() == OpcUaNodeId("MyType"));
+    BOOST_REQUIRE(definition->dataFields()[0]->description() == OpcUaLocalizedText("", "todo"));
+    BOOST_REQUIRE(definition->dataFields()[1]->name().value() == "Element2");
+    BOOST_REQUIRE(definition->dataFields()[1]->isOptional() == false);
+    BOOST_REQUIRE(definition->dataFields()[1]->dataType() == OpcUaNodeId(OpcUaBuildInType_OpcUaDouble));
+    BOOST_REQUIRE(definition->dataFields()[1]->description() == OpcUaLocalizedText());
+    BOOST_REQUIRE(definition->dataFields()[2]->name().value() == "Element3");
+    BOOST_REQUIRE(definition->dataFields()[2]->isOptional() == true);
+    BOOST_REQUIRE(definition->dataFields()[2]->dataType() == OpcUaNodeId(OpcUaBuildInType_OpcUaDouble));
+    BOOST_REQUIRE(definition->dataFields()[2]->description() == OpcUaLocalizedText());
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
 
 
