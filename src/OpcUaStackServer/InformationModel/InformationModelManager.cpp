b5afc599a7e1ffad12bf55d362b3f4aaf0d245ea
Kai Huebl <kai@huebl-sgh.de>
2017-06-13 13:42:39 +0200

    added new class InformationModelManager

---
+++ src/OpcUaStackServer/InformationModel/InformationModelManager.cpp
@@ -0,0 +1,77 @@
+/*
+   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/BuildInTypes/OpcUaIdentifier.h"
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaStackServer/InformationModel/InformationModelManager.h"
+#include "OpcUaStackServer/AddressSpaceModel/ObjectNodeClass.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackServer
+{
+
+	InformationModelManager::InformationModelManager(void)
+	: informationModel_()
+	{
+	}
+
+	InformationModelManager::InformationModelManager(InformationModel::SPtr informationModel)
+	{
+		informationModel_ = informationModel;
+	}
+
+	InformationModelManager::~InformationModelManager(void)
+	{
+	}
+
+	void
+	InformationModelManager::informationModel(InformationModel::SPtr informationModel)
+	{
+		informationModel_ = informationModel;
+	}
+
+	bool
+	InformationModelManager::addObject(
+		OpcUaNodeId& parentNodeId,
+		OpcUaNodeId& nodeId,
+		OpcUaLocalizedText& displayName,
+		OpcUaQualifiedName& browseName,
+		OpcUaNodeId& referenceType,
+		OpcUaNodeId& objectType
+	)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	InformationModelManager::addVariable(
+		OpcUaNodeId& parentNodeId,
+		OpcUaNodeId& nodeId,
+		OpcUaLocalizedText& displayName,
+		OpcUaQualifiedName& browseName,
+		OpcUaNodeId& referenceType,
+		OpcUaNodeId& variableType
+	)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+}
+
---
+++ src/OpcUaStackServer/InformationModel/InformationModelManager.h
@@ -0,0 +1,71 @@
+/*
+   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackServer_InformationModelManager_h__
+#define __OpcUaStackServer_InformationModelManager_h__
+
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackServer/InformationModel/InformationModel.h"
+
+namespace OpcUaStackServer
+{
+
+	class MergeIf
+	{
+	  public:
+		MergeIf(void) {}
+		virtual ~MergeIf(void) {}
+
+		virtual void handleMerge(uint32_t maxCount, uint32_t actCount) = 0;
+	};
+
+
+	class DLLEXPORT InformationModelManager
+	{
+	  public:
+		typedef boost::shared_ptr<InformationModelManager> SPtr;
+
+		InformationModelManager(void);
+		InformationModelManager(InformationModel::SPtr informationModel);
+		~InformationModelManager(void);
+
+		void informationModel(InformationModel::SPtr informationModel);
+
+		bool addObject(
+			OpcUaNodeId& parentNodeId,
+			OpcUaNodeId& nodeId,
+			OpcUaLocalizedText& displayName,
+			OpcUaQualifiedName& browseName,
+			OpcUaNodeId& referenceType,
+			OpcUaNodeId& objectType
+		);
+		bool addVariable(
+			OpcUaNodeId& parentNodeId,
+			OpcUaNodeId& nodeId,
+			OpcUaLocalizedText& displayName,
+			OpcUaQualifiedName& browseName,
+			OpcUaNodeId& referenceType,
+			OpcUaNodeId& variableType
+		);
+
+	  private:
+		InformationModel::SPtr informationModel_;
+	};
+
+}
+
+#endif
