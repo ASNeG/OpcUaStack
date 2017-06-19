995009f7621480595b527b2f3753dd8d69e4a77e
Kai Huebl <kai@huebl-sgh.de>
2017-06-19 21:21:25 +0200

    added new class CreateNodeInstanceRequest

---
+++ src/OpcUaStackCore/ServiceSetApplication/CreateNodeInstanceRequest.h
@@ -0,0 +1,46 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackCore_CreateNodeInstanceRequest_h__
+#define __OpcUaStackCore_CreateNodeInstanceRequest_h__
+
+#include <stdint.h>
+#include "OpcUaStackCore/Base/ObjectPool.h"
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+
+namespace OpcUaStackCore
+{
+
+	class DLLEXPORT CreateNodeInstanceRequest
+	: public  Object
+	{
+	  public:
+		typedef boost::shared_ptr<CreateNodeInstanceRequest> SPtr;
+
+		CreateNodeInstanceRequest(void);
+		virtual ~CreateNodeInstanceRequest(void);
+
+		void opcUaBinaryEncode(std::ostream& os) const;
+		void opcUaBinaryDecode(std::istream& is);
+
+	  private:
+	};
+
+}
+
+#endif
