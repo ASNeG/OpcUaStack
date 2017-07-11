b0ce120fa6e6852b9d03dc52ba5586697f72fde7
Kai Huebl <kai@huebl-sgh.de>
2017-07-11 17:11:22 +0200

    added xml number interface functions

--- src/OpcUaStackCore/BuildInTypes/XmlNumber.cpp
+++ src/OpcUaStackCore/BuildInTypes/XmlNumber.cpp
@@ -23,26 +23,282 @@
 namespace OpcUaStackCore
 {
 
-
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// encoder - element
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
 	bool
-	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaBoolean& value)
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaBoolean& value, const std::string& element)
 	{
-		std::string sourceValue = pt.get_value<std::string>();
-		boost::to_upper(sourceValue);
-		if (sourceValue == "TRUE") value = true;
-		else value = false;
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
 		return true;
 	}
 
 	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaByte& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaSByte& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaUInt16& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaInt16& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaUInt32& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaInt32& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaUInt64& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaInt64& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaFloat& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaDouble& value, const std::string& element)
+	{
+		boost::property_tree::ptree elementTree;
+		if (!xmlEncode(pt, value)) return false;
+		pt.push_back(std::make_pair(element, elementTree));
+		return true;
+	}
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// encoder
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaBoolean& value)
+	{
+		if (value) {
+			pt.put_value("true");
+		}
+		else {
+			pt.put_value("false");
+		}
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaByte& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaSByte& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaUInt16& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaInt16& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaUInt32& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaInt32& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaUInt64& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaInt64& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaFloat& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlEncode(boost::property_tree::ptree& pt, OpcUaDouble& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// decoder
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	bool
 	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaBoolean& value)
 	{
-		std::string sourceValue = pt.get_value<std::string>();
-		try {
-			value = (OpcUaByte)boost::lexical_cast<OpcUaUInt16>(sourceValue);
-		} catch(boost::bad_lexical_cast& e) {
-			return false;
-		}
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaByte& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaSByte& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaUInt16& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaInt16& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaUInt32& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaInt32& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaUInt64& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaInt64& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaFloat& value)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	XmlNumber::xmlDecode(boost::property_tree::ptree& pt, OpcUaDouble& value)
+	{
+		// FIXME: todo
 		return true;
 	}
 
