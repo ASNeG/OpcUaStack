aa6e5f718e92de4db45b4479661afa7d686dec24
Kai Huebl <kai@huebl-sgh.de>
2017-07-13 09:45:35 +0200

    print xml output

--- tst/OpcUaStackCore/BuildInTypes/XmlEncoderDecoder_t.cpp
+++ tst/OpcUaStackCore/BuildInTypes/XmlEncoderDecoder_t.cpp
@@ -1,6 +1,7 @@
 #include "unittest.h"
 #include "OpcUaStackCore/BuildInTypes/XmlNumber.h"
 #include "OpcUaStackCore/Base/Utility.h"
+#include "OpcUaStackCore/Base/ConfigXml.h"
 #include <boost/iostreams/stream.hpp>
 
 using namespace OpcUaStackCore;
@@ -14,11 +15,16 @@ BOOST_AUTO_TEST_CASE(XmlEncoderDecoder_)
 
 BOOST_AUTO_TEST_CASE(XmlEncoderDecoder_Boolean)
 {
+	ConfigXml xml;
 	boost::property_tree::ptree pt;
 	OpcUaBoolean value1, value2;
 
 	value1 = false;
 	XmlNumber::xmlEncode(pt, value1);
+
+	xml.ptree(pt);
+	xml.write(std::cout);
+	std::cout << std::endl;
 
 	XmlNumber::xmlDecode(pt, value2);
 	BOOST_REQUIRE(value2 == false);
@@ -26,11 +32,16 @@ BOOST_AUTO_TEST_CASE(XmlEncoderDecoder_Boolean)
 
 BOOST_AUTO_TEST_CASE(XmlEncoderDecoder_Byte)
 {
+	ConfigXml xml;
 	boost::property_tree::ptree pt;
 	OpcUaByte value1, value2;
 
 	value1 = 12;
 	XmlNumber::xmlEncode(pt, value1);
+
+	xml.ptree(pt);
+	xml.write(std::cout);
+	std::cout << std::endl;
 
 	XmlNumber::xmlDecode(pt, value2);
 	BOOST_REQUIRE(value2 == 12);
@@ -38,14 +49,53 @@ BOOST_AUTO_TEST_CASE(XmlEncoderDecoder_Byte)
 
 BOOST_AUTO_TEST_CASE(XmlEncoderDecoder_SByte)
 {
+	ConfigXml xml;
 	boost::property_tree::ptree pt;
 	OpcUaSByte value1, value2;
 
 	value1 = -12;
 	XmlNumber::xmlEncode(pt, value1);
 
+	xml.ptree(pt);
+	xml.write(std::cout);
+	std::cout << std::endl;
+
 	XmlNumber::xmlDecode(pt, value2);
 	BOOST_REQUIRE(value2 == -12);
+}
+
+BOOST_AUTO_TEST_CASE(XmlEncoderDecoder_UInt16)
+{
+	ConfigXml xml;
+	boost::property_tree::ptree pt;
+	OpcUaUInt16 value1, value2;
+
+	value1 = 123;
+	XmlNumber::xmlEncode(pt, value1);
+
+	xml.ptree(pt);
+	xml.write(std::cout);
+	std::cout << std::endl;
+
+	XmlNumber::xmlDecode(pt, value2);
+	BOOST_REQUIRE(value2 == 123);
+}
+
+BOOST_AUTO_TEST_CASE(XmlEncoderDecoder_Int16)
+{
+	ConfigXml xml;
+	boost::property_tree::ptree pt;
+	OpcUaInt16 value1, value2;
+
+	value1 = -123;
+	XmlNumber::xmlEncode(pt, value1);
+
+	xml.ptree(pt);
+	xml.write(std::cout);
+	std::cout << std::endl;
+
+	XmlNumber::xmlDecode(pt, value2);
+	BOOST_REQUIRE(value2 == -123);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
