d3507baedd0255209abc2358043cbb607692e797
Kai Huebl <kai@huebl-sgh.de>
2017-07-14 21:46:34 +0200

    rework class OpcUaLocalizedText (xml encoder function)

--- src/OpcUaStackCore/BuildInTypes/OpcUaLocalizedText.cpp
+++ src/OpcUaStackCore/BuildInTypes/OpcUaLocalizedText.cpp
@@ -236,7 +236,7 @@ namespace OpcUaStackCore
 	OpcUaLocalizedText::xmlEncode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns)
 	{
 		boost::property_tree::ptree elementTree;
-		if (!xmlEncode(pt, xmlns)) {
+		if (!xmlEncode(elementTree, xmlns)) {
 			Log(Error, "OpcUaLocalizedText xml encoder error")
 				.parameter("Element", element);
 			return false;
