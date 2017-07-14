eb5847d0423137993ccc2e71f27bd8072cede327
Kai Huebl <kai@huebl-sgh.de>
2017-07-14 21:42:56 +0200

    rework class OpcUaNodeId (xml encoder function)

--- src/OpcUaStackCore/BuildInTypes/OpcUaNodeIdBase.cpp
+++ src/OpcUaStackCore/BuildInTypes/OpcUaNodeIdBase.cpp
@@ -492,7 +492,7 @@ namespace OpcUaStackCore
 	OpcUaNodeIdBase::xmlEncode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns)
 	{
 		boost::property_tree::ptree elementTree;
-		if (!xmlEncode(pt, xmlns)) {
+		if (!xmlEncode(elementTree, xmlns)) {
 			Log(Error, "OpcUaNodeId xml encoder error")
 				.parameter("Element", element);
 			return false;
