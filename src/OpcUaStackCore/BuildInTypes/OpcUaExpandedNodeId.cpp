cd934645e561d98e50673ac260791ac445a3a766
Kai Huebl <kai@huebl-sgh.de>
2017-07-15 23:39:22 +0200

    fix bug in class ExpandedNodeId

--- src/OpcUaStackCore/BuildInTypes/OpcUaExpandedNodeId.cpp
+++ src/OpcUaStackCore/BuildInTypes/OpcUaExpandedNodeId.cpp
@@ -195,7 +195,7 @@ namespace OpcUaStackCore
 	OpcUaExpandedNodeId::xmlEncode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns)
 	{
 		boost::property_tree::ptree elementTree;
-		if (!xmlEncode(pt, xmlns)) {
+		if (!xmlEncode(elementTree, xmlns)) {
 			Log(Error, "OpcUaExpandedNodeId xml encoder error")
 				.parameter("Element", element);
 			return false;
