f08664763bf366a72227cd591634cff184c2e155
Kai Huebl <kai@huebl-sgh.de>
2017-07-20 12:53:22 +0200

    added unittest to validate class DataMemArray

---
+++ tst/OpcUaStackCore/Base/DataMemArray_t.cpp
@@ -0,0 +1,19 @@
+#include "unittest.h"
+
+#include <iostream>
+#include "OpcUaStackCore/Base/DataMemArray.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(DataMemArray_t)
+
+BOOST_AUTO_TEST_CASE(DataMemArray_)
+{
+	std::cout << "DataMemArray_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(DataMemArray_constructor)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
