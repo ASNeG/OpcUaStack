36115fdee9c7247584d5216381a7b6afa3a72202
Kai Huebl <kai@huebl-sgh.de>
2017-07-28 22:15:36 +0200

    added new class MonitorItemId

---
+++ src/OpcUaStackServer/ServiceSet/MonitorItemId.cpp
@@ -0,0 +1,34 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackServer/ServiceSet/MonitorItemId.h"
+
+namespace OpcUaStackServer
+{
+
+	boost::mutex MonitorItemId::mutex_;
+	uint32_t MonitorItemId::uniqueMonitorItemId_ = 0;
+
+	uint32_t 
+	MonitorItemId::monitorItemId(void)
+	{
+		boost::mutex::scoped_lock g(mutex_);
+		uniqueMonitorItemId_++;
+		return uniqueMonitorItemId_;
+	}
+
+}
