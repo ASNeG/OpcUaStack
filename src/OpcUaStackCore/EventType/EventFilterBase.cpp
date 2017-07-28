00058c86a8ba5fd7f8b05832ccc1353efa246166
Kai Huebl <kai@huebl-sgh.de>
2017-07-28 13:35:05 +0200

    added new class EventFilterBase

---
+++ src/OpcUaStackCore/EventType/EventFilterBase.cpp
@@ -0,0 +1,39 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/EventType/EventFilterBase.h"
+#include "OpcUaStackCore/Base/Log.h"
+
+namespace OpcUaStackCore
+{
+
+	EventFilterBase::EventFilterBase(void)
+	{
+	}
+
+	EventFilterBase::~EventFilterBase(void)
+	{
+	}
+
+	bool
+	EventFilterBase::fireEvent(OpcUaNodeId& nodeId, EventBase::SPtr& eventBase)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+}
