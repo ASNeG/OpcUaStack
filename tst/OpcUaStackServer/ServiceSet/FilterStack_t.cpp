4401845e836bc08635b51529b3b51bd2825c2cb5
Aleksey Timin <timin-ayu@nefteavtomatika.ru>
2017-08-04 11:06:29 +0500

    add test for equals operator

--- tst/OpcUaStackServer/ServiceSet/FilterStack_t.cpp
+++ tst/OpcUaStackServer/ServiceSet/FilterStack_t.cpp
@@ -17,7 +17,7 @@ BOOST_AUTO_TEST_CASE(FilterStack_returns_True_if_empty)
 }
 
 
-BOOST_AUTO_TEST_CASE(FilterStack_Equals)
+BOOST_AUTO_TEST_CASE(FilterStack_Equals_returns_false)
 {
     FilterStack stack;
 
@@ -41,7 +41,38 @@ BOOST_AUTO_TEST_CASE(FilterStack_Equals)
 
     filter.elements()->push_back(eqElement);
 
-    stack.receive(filter, nullptr);
+    ContentFilterResult result;
+    stack.receive(filter, result);
+
+    BOOST_REQUIRE(!stack.process());
+}
+
+BOOST_AUTO_TEST_CASE(FilterStack_Equals_returns_true)
+{
+    FilterStack stack;
+
+    ContentFilter filter;
+    ContentFilterElement::SPtr eqElement = constructSPtr<ContentFilterElement>();
+
+    ExtensibleParameter::SPtr arg1 = constructSPtr<ExtensibleParameter>();
+    arg1->registerFactoryElement<LiteralOperand>((OpcUaUInt32)OpcUaId_LiteralOperand);
+    arg1->parameterTypeId().set((OpcUaUInt32)OpcUaId_LiteralOperand);
+    arg1->parameter<LiteralOperand>()->value().set<OpcUaUInt32>(100);
+
+    ExtensibleParameter::SPtr arg2 = constructSPtr<ExtensibleParameter>();
+    arg2->registerFactoryElement<LiteralOperand>((OpcUaUInt32)OpcUaId_LiteralOperand);
+    arg2->parameterTypeId().set((OpcUaUInt32)OpcUaId_LiteralOperand);
+    arg2->parameter<LiteralOperand>()->value().set<OpcUaUInt32>(100);
+
+    eqElement->filterOperator(BasicFilterOperator::BasicFilterOperator_Equals);
+    eqElement->filterOperands()->resize(2);
+    eqElement->filterOperands()->push_back(arg1);
+    eqElement->filterOperands()->push_back(arg2);
+
+    filter.elements()->push_back(eqElement);
+
+    ContentFilterResult result;
+    stack.receive(filter, result);
 
     BOOST_REQUIRE(!stack.process());
 }
