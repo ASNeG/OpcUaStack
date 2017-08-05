14804dbabfbe7ca33fe6aae9df4fa80bfb2626f7
Kai Huebl <kai@huebl-sgh.de>
2017-08-05 14:14:38 +0200

    added new class SimpleAttributeFilterNode

---
+++ src/OpcUaStackServer/Filter/SimpleAttributeFilterNode.h
@@ -0,0 +1,46 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Aleksey Timin (timin-ayu@nefteavtomatika.ru)
+ */
+
+#ifndef __OpcUaStackServer_SimpleAttributeFilterNode_h__
+#define __OpcUaStackServer_SimpleAttributeFilterNode_h__
+
+#include "OpcUaStackServer/Filter/FilterNode.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackServer
+{
+    class DLLEXPORT SimpleAttributeFilterNode
+	: public FilterNode
+    {
+      public:
+
+        typedef boost::shared_ptr<SimpleAttributeFilterNode> SPtr;
+
+        SimpleAttributeFilterNode(const OpcUaVariant& value);
+        virtual ~SimpleAttributeFilterNode(void);
+
+        virtual OpcUaVariant evaluate() override;
+
+      private:
+        OpcUaVariant value_;
+
+
+    };
+
+}
+#endif
