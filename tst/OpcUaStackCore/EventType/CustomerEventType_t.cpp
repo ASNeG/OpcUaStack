85ea682c95db1bce64c7c9d2b333b9d45419c238
Kai Huebl <kai@huebl-sgh.de>
2017-08-09 19:43:11 +0200

    added unittest CustomerEventType_t

---
+++ tst/OpcUaStackCore/EventType/CustomerEventType_t.cpp
@@ -0,0 +1,13 @@
+#include "unittest.h"
+#include "OpcUaStackCore/EventType/BaseEventType.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(CustomerEventType_)
+
+BOOST_AUTO_TEST_CASE(CustomerEventType_)
+{
+	std::cout << "CustomerEventType_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
