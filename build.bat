ca7c4168131f35fa2bafcb2d1bac64fcb8dfdca5
Kai Huebl <kai@huebl-sgh.de>
2017-09-09 22:15:46 +0200

    added boost version 1.58 to windows build script

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 
 rmdir ./build
 
+set BOOST_VERSION_MAJOR=1
+set BOOST_VERSION_MINOR=58
 set BOOST_LIBRARYDIR=C:\local\boost_1_58_0\lib64-msvc-12.0
 
 cmake.exe -G"Visual Studio 12 2013" -H./src/ -B./build
