d3abb9499895093150ada987e05bd2d773e7ff1e
Kai Huebl <kai@huebl-sgh.de>
2017-09-14 09:33:41 +0200

    rework build script

--- build.bat
+++ build.bat
@@ -1,16 +1,72 @@
+@echo off
+REM
+REM OpcUaStack build and install script
+REM
 
-rmdir ./build
+rmdir ./build_local
 
 set BOOST_VERSION_MAJOR=1
 set BOOST_VERSION_MINOR=58
 set BOOST_LIBRARYDIR=C:\local\boost_1_58_0\lib32-msvc-12.0
-cmake.exe -G"Visual Studio 12 2013" -H./src/ -B./build 
+
+set CMAKE=cmake.exe
+set MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 
 
+REM ---------------------------------------------------------------------------
+REM 
+REM main
+REM
+REM ---------------------------------------------------------------------------
+set COMAMND=%1
 
-set DESTDIR=C:\install
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe ./build/INSTALL.vcxproj
+if "%COMMAND%" == "" (
+    call:build_local
+	
+	pause
+	goto:eof
+)
 
+if "%COMMAND%" == "local" (
+    call:build_local
+	
+	pause
+	goto:eof
+)
 
+call:usage
 
 pause
+goto:eof
+
+
+REM ---------------------------------------------------------------------------
+REM
+REM build local function
+REM
+REM ---------------------------------------------------------------------------
+:build_local
+	REM
+	REM build OpcUaStack
+	REM
+	%CMAKE% -G"Visual Studio 12 2013" -H./src/ -B./build_local
+
+	REM
+	REM install OpcUaStack
+	REM
+	set DESTDIR=C:\install
+	%MSBUILD% ./build_local/INSTALL.vcxproj
+goto:eof
+
+
+REM ---------------------------------------------------------------------------
+REM
+REM usage function
+REM
+REM ---------------------------------------------------------------------------
+:usage
+   echo build.bat (local)
+   echo.
+   echo   local - create local build and install in folder C:/install
+   echo.
+goto:eof
