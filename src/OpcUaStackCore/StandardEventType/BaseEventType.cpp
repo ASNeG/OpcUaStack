c9067846770bdbedf995b82791beddcd35ef0b8b
Kai Huebl <kai@huebl-sgh.de>
2017-09-16 21:05:44 +0200

    added class BaseEventType class to StandardEventType directory

---
+++ src/OpcUaStackCore/StandardEventType/BaseEventType.cpp
@@ -0,0 +1,224 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/StandardEventType/BaseEventType.h"
+#include "OpcUaStackCore/Base/Log.h"
+
+namespace OpcUaStackCore
+{
+
+	BaseEventType::BaseEventType(void)
+	: EventBase()
+	, eventVariables_()
+	{
+		eventVariables_.registerEventVariable("EventId", OpcUaBuildInType_OpcUaByteString);
+		eventVariables_.registerEventVariable("EventType", OpcUaBuildInType_OpcUaNodeId);
+		eventVariables_.registerEventVariable("SourceName", OpcUaBuildInType_OpcUaString);
+		eventVariables_.registerEventVariable("LocalTime", OpcUaBuildInType_OpcUaDateTime);
+		eventVariables_.registerEventVariable("Message", OpcUaBuildInType_OpcUaLocalizedText);
+		eventVariables_.registerEventVariable("ReceiveTime", OpcUaBuildInType_OpcUaDateTime);
+		eventVariables_.registerEventVariable("Severity", OpcUaBuildInType_OpcUaUInt16);
+		eventVariables_.registerEventVariable("SourceNode", OpcUaBuildInType_OpcUaNodeId);
+		eventVariables_.registerEventVariable("Time", OpcUaBuildInType_OpcUaDateTime);
+
+		eventVariables_.eventType(OpcUaNodeId((OpcUaUInt32)2041));
+		eventVariables_.namespaceIndex(0);
+		eventVariables_.browseName(OpcUaQualifiedName("BaseEventType"));
+		eventVariables_.namespaceUri("");
+	}
+
+	BaseEventType::~BaseEventType(void)
+	{
+	}
+
+	bool
+	BaseEventType::eventId(OpcUaVariant::SPtr& eventId)
+	{
+		return eventVariables_.setValue("EventId", eventId);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::eventId(void)
+	{
+		OpcUaVariant::SPtr value;
+		eventVariables_.getValue("EventId", value);
+		return value;
+	}
+
+	bool
+	BaseEventType::eventType(OpcUaVariant::SPtr& eventType)
+	{
+		return eventVariables_.setValue("EventType", eventType);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::eventType(void)
+	{
+		OpcUaVariant::SPtr value;
+		eventVariables_.getValue("EventType", value);
+		return value;
+	}
+
+	bool
+	BaseEventType::sourceName(OpcUaVariant::SPtr& sourceName)
+	{
+		return eventVariables_.setValue("SourceName", sourceName);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::sourceName(void)
+	{
+		OpcUaVariant::SPtr value;
+		eventVariables_.getValue("SourceName", value);
+		return value;
+	}
+
+	bool
+	BaseEventType::localTime(OpcUaVariant::SPtr& localTime)
+	{
+		return eventVariables_.setValue("LocalTime", localTime);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::localTime(void)
+	{
+		OpcUaVariant::SPtr value;
+		eventVariables_.getValue("LocalTime", value);
+		return value;
+	}
+
+	bool
+	BaseEventType::message(OpcUaVariant::SPtr& message)
+	{
+		return eventVariables_.setValue("Message", message);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::message(void)
+	{
+		OpcUaVariant::SPtr value;
+		eventVariables_.getValue("Message", value);
+		return value;
+	}
+
+	bool
+	BaseEventType::receiveTime(OpcUaVariant::SPtr& receiveTime)
+	{
+		return eventVariables_.setValue("ReceiveTime", receiveTime);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::receiveTime(void)
+	{
+		OpcUaVariant::SPtr value;
+		eventVariables_.getValue("ReceiveTime", value);
+		return value;
+	}
+
+	bool
+	BaseEventType::severity(OpcUaVariant::SPtr& severity)
+	{
+		return eventVariables_.setValue("Severity", severity);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::severity(void)
+	{
+		OpcUaVariant::SPtr value;
+		eventVariables_.getValue("Severity", value);
+		return value;
+	}
+
+	bool
+	BaseEventType::sourceNode(OpcUaVariant::SPtr& sourceNode)
+	{
+		return eventVariables_.setValue("SourceNode", sourceNode);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::sourceNode(void)
+	{
+		OpcUaVariant::SPtr value;
+		eventVariables_.getValue("SourceNode", value);
+		return value;
+	}
+
+	bool
+	BaseEventType::time(OpcUaVariant::SPtr& time)
+	{
+		return eventVariables_.setValue("Time", time);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::time(void)
+	{
+		OpcUaVariant::SPtr value;
+		eventVariables_.getValue("Time", value);
+		return value;
+	}
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// EventBase interface
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	void
+	BaseEventType::mapNamespaceUri(void)
+	{
+		std::cout << "BaseEventType::mapNamespaceUri" << std::endl;
+
+		uint32_t namespaceIndex;
+		EventBase::mapNamespaceUri();
+
+		OpcUaVariant::SPtr eventTypeVariable = constructSPtr<OpcUaVariant>();
+		eventTypeVariable->setValue(eventVariables_.eventType());
+
+		setNamespaceIndex(eventVariables_.namespaceUri(), namespaceIndex, eventVariables_.browseName(), eventTypeVariable);
+
+		std::cout << "1 set event type: " << *eventTypeVariable << std::endl;
+		eventType(eventTypeVariable);
+		eventVariables_.eventType(eventTypeVariable);
+		eventVariables_.namespaceIndex(namespaceIndex);
+	}
+
+	OpcUaVariant::SPtr
+	BaseEventType::get(
+		OpcUaNodeId& eventType,
+		std::list<OpcUaQualifiedName::SPtr>& browseNameList,
+		EventResult::Code& resultCode
+	)
+	{
+		resultCode = EventResult::Success;
+
+		// check whether eventType and typeNodeId are identical
+		if (eventType == eventVariables_.eventType()) {
+			return eventVariables_.get(browseNameList, resultCode);
+		}
+
+		// the start item was not found. We delegate the search to the base class
+		OpcUaVariant::SPtr variant;
+		variant = EventBase::get(eventType, browseNameList, resultCode);
+		if (resultCode != EventResult::Success || browseNameList.empty()) {
+			return variant;
+		}
+
+		return eventVariables_.get(browseNameList, resultCode);
+	}
+
+}
---
+++ src/OpcUaStackCore/StandardEventType/BaseEventType.h
@@ -0,0 +1,72 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackCore_BaseEventType_h__
+#define __OpcUaStackCore_BaseEventType_h__
+
+#include <boost/shared_ptr.hpp>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/EventType/EventBase.h"
+#include "OpcUaStackCore/EventType/EventVariables.h"
+
+namespace OpcUaStackCore
+{
+
+	class DLLEXPORT BaseEventType
+	: public EventBase
+	{
+	  public:
+		typedef boost::shared_ptr<BaseEventType> SPtr;
+
+		BaseEventType(void);
+		virtual ~BaseEventType(void);
+
+		bool eventId(OpcUaVariant::SPtr& eventId);
+		OpcUaVariant::SPtr eventId(void);
+		bool eventType(OpcUaVariant::SPtr& eventType);
+		OpcUaVariant::SPtr eventType(void);
+		bool sourceName(OpcUaVariant::SPtr& sourceName);
+		OpcUaVariant::SPtr sourceName(void);
+		bool localTime(OpcUaVariant::SPtr& localTime);
+		OpcUaVariant::SPtr localTime(void);
+		bool message(OpcUaVariant::SPtr& message);
+		OpcUaVariant::SPtr message(void);
+		bool receiveTime(OpcUaVariant::SPtr& receiveTime);
+		OpcUaVariant::SPtr receiveTime(void);
+		bool severity(OpcUaVariant::SPtr& severity);
+		OpcUaVariant::SPtr severity(void);
+		bool sourceNode(OpcUaVariant::SPtr& sourceNode);
+		OpcUaVariant::SPtr sourceNode(void);
+		bool time(OpcUaVariant::SPtr& time);
+		OpcUaVariant::SPtr time(void);
+
+		//- EventBase interface
+		virtual void mapNamespaceUri(void);
+		virtual OpcUaVariant::SPtr get(
+			OpcUaNodeId& eventType,
+			std::list<OpcUaQualifiedName::SPtr>& browseNameList,
+			EventResult::Code& resultCode
+		);
+		//- EventBase interface
+
+	  private:
+		EventVariables eventVariables_;
+	};
+
+}
+
+#endif
