5419b14c52b763fab9fe2032d36ae82f0f5051b7
Kai Huebl <kai@huebl-sgh.de>
2017-09-17 21:08:03 +0200

    added Duration data type

--- src/OpcUaStackCore/BuildInTypes/OpcUaType.h
+++ src/OpcUaStackCore/BuildInTypes/OpcUaType.h
@@ -38,6 +38,7 @@ namespace OpcUaStackCore
 		OpcUaBuildInType_OpcUaUInt64 = 9,
 		OpcUaBuildInType_OpcUaFloat = 10,
 		OpcUaBuildInType_OpcUaDouble = 11,
+		OpcUaBuildInType_OpcUaDuration = 11,
 		OpcUaBuildInType_OpcUaString = 12,
 		OpcUaBuildInType_OpcUaDateTime = 13,
 		OpcUaBuildInType_OpcUaGuid = 14,
