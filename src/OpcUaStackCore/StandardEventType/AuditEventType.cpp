26cc06f1704a2f3555c38eba51be01be194586f4
Kai Huebl <kai@huebl-sgh.de>
2017-09-17 13:38:14 +0200

    added generated class AuditEventType

---
+++ src/OpcUaStackCore/StandardEventType/AuditEventType.cpp
@@ -0,0 +1,153 @@
+/*
+    EventTypeClass: AuditEventType
+
+    Generated Source Code - please do not change this source code
+
+    EventTypeCodeGenerator Version:
+        OpcUaStackCore - 3.0.1
+
+    Autor: Kai Huebl (kai@huebl-sgh.de)
+*/
+
+#include "OpcUaStackCore/StandardEventType/AuditEventType.h"
+
+namespace OpcUaStackCore
+{
+    
+    AuditEventType::AuditEventType(void)
+    : BaseEventType()
+    , eventVariables_()
+    {
+        eventVariables_.registerEventVariable("ActionTimeStamp", OpcUaBuildInType_OpcUaNodeId);
+        eventVariables_.registerEventVariable("Status", OpcUaBuildInType_OpcUaNodeId);
+        eventVariables_.registerEventVariable("ServerId", OpcUaBuildInType_OpcUaNodeId);
+        eventVariables_.registerEventVariable("ClientAuditEntryId", OpcUaBuildInType_OpcUaNodeId);
+        eventVariables_.registerEventVariable("ClientUserId", OpcUaBuildInType_OpcUaNodeId);
+    
+        OpcUaVariant::SPtr eventType = constructSPtr<OpcUaVariant>();
+        eventType->setValue(OpcUaNodeId((OpcUaUInt32)2052));
+        eventVariables_.setValue("AuditEventType", eventType);
+        eventVariables_.namespaceIndex(0);
+        eventVariables_.browseName(OpcUaQualifiedName("AuditEventType"));
+        eventVariables_.namespaceUri("");
+    }
+    
+    AuditEventType::~AuditEventType(void)
+    {
+    }
+    
+    OpcUaVariant::SPtr 
+    AuditEventType::actionTimeStamp(void)
+    {
+    	OpcUaVariant::SPtr value;
+    	eventVariables_.getValue("actionTimeStamp", value);
+    	return value;
+    }
+    
+    OpcUaVariant::SPtr 
+    AuditEventType::status(void)
+    {
+    	OpcUaVariant::SPtr value;
+    	eventVariables_.getValue("status", value);
+    	return value;
+    }
+    
+    OpcUaVariant::SPtr 
+    AuditEventType::serverId(void)
+    {
+    	OpcUaVariant::SPtr value;
+    	eventVariables_.getValue("serverId", value);
+    	return value;
+    }
+    
+    OpcUaVariant::SPtr 
+    AuditEventType::clientAuditEntryId(void)
+    {
+    	OpcUaVariant::SPtr value;
+    	eventVariables_.getValue("clientAuditEntryId", value);
+    	return value;
+    }
+    
+    OpcUaVariant::SPtr 
+    AuditEventType::clientUserId(void)
+    {
+    	OpcUaVariant::SPtr value;
+    	eventVariables_.getValue("clientUserId", value);
+    	return value;
+    }
+    
+    bool 
+    AuditEventType::actionTimeStamp(OpcUaVariant::SPtr& actionTimeStamp)
+    {
+    	return eventVariables_.setValue("ActionTimeStamp", actionTimeStamp);
+    }
+    
+    bool 
+    AuditEventType::status(OpcUaVariant::SPtr& status)
+    {
+    	return eventVariables_.setValue("Status", status);
+    }
+    
+    bool 
+    AuditEventType::serverId(OpcUaVariant::SPtr& serverId)
+    {
+    	return eventVariables_.setValue("ServerId", serverId);
+    }
+    
+    bool 
+    AuditEventType::clientAuditEntryId(OpcUaVariant::SPtr& clientAuditEntryId)
+    {
+    	return eventVariables_.setValue("ClientAuditEntryId", clientAuditEntryId);
+    }
+    
+    bool 
+    AuditEventType::clientUserId(OpcUaVariant::SPtr& clientUserId)
+    {
+    	return eventVariables_.setValue("ClientUserId", clientUserId);
+    }
+    
+    void
+    AuditEventType::mapNamespaceUri(void)
+    {
+        uint32_t namespaceIndex;
+        BaseEventType::mapNamespaceUri();
+    
+        OpcUaVariant::SPtr eventType;
+        eventVariables_.getValue("AuditEventType", eventType);
+    
+        setNamespaceIndex(eventVariables_.namespaceUri(), namespaceIndex, eventVariables_.browseName(), eventType);
+    
+    	eventVariables_.setValue("AuditEventType", eventType);
+    	eventVariables_.namespaceIndex(namespaceIndex);
+    }
+
+    OpcUaVariant::SPtr
+    AuditEventType::get(
+    	OpcUaNodeId& eventType,
+    	std::list<OpcUaQualifiedName::SPtr>& browseNameList,
+    	EventResult::Code& resultCode
+    )
+    {
+    	resultCode = EventResult::Success;
+
+    	OpcUaNodeId typeNodeId;
+    	OpcUaVariant::SPtr tmpVariant;
+    	eventVariables_.getValue("AuditEventType", tmpVariant);
+    	tmpVariant->getValue(typeNodeId);
+
+    	// check whether eventType and typeNodeId are identical
+    	if (eventType == typeNodeId) {
+    		return eventVariables_.get(browseNameList, resultCode);
+    	}
+
+    	// the start item was not found. We delegate the search to the base class
+    	OpcUaVariant::SPtr variant;
+    	variant = BaseEventType::get(eventType, browseNameList, resultCode);
+    	if (resultCode != EventResult::Success || browseNameList.empty()) {
+    		return variant;
+    	}
+
+    	return eventVariables_.get(browseNameList, resultCode);
+    }
+
+}
---
+++ src/OpcUaStackCore/StandardEventType/AuditEventType.h
@@ -0,0 +1,66 @@
+/*
+    EventTypeClass: AuditEventType
+
+    Generated Source Code - please do not change this source code
+
+    EventTypeCodeGenerator Version:
+        OpcUaStackCore - 3.0.1
+
+    Autor: Kai Huebl (kai@huebl-sgh.de)
+*/
+
+#ifndef __OpcUaStackCore_AuditEventType_h__
+#define __OpcUaStackCore_AuditEventType_h__
+
+#include <boost/shared_ptr.hpp>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+#include "OpcUaStackCore/StandardEventType/BaseEventType.h"
+
+namespace OpcUaStackCore
+{
+    
+    class AuditEventType
+    : public BaseEventType
+    {
+      public:
+        typedef boost::shared_ptr<AuditEventType> SPtr;
+    
+        AuditEventType(void);
+        virtual ~AuditEventType(void);
+        
+        bool actionTimeStamp(OpcUaVariant::SPtr& variable);
+        OpcUaVariant::SPtr actionTimeStamp(void);
+        
+        bool status(OpcUaVariant::SPtr& variable);
+        OpcUaVariant::SPtr status(void);
+        
+        bool serverId(OpcUaVariant::SPtr& variable);
+        OpcUaVariant::SPtr serverId(void);
+        
+        bool clientAuditEntryId(OpcUaVariant::SPtr& variable);
+        OpcUaVariant::SPtr clientAuditEntryId(void);
+        
+        bool clientUserId(OpcUaVariant::SPtr& variable);
+        OpcUaVariant::SPtr clientUserId(void);
+        
+        
+        //- EventBase interface
+        virtual void mapNamespaceUri(void);
+        
+        virtual OpcUaVariant::SPtr get(
+            OpcUaNodeId& eventType,
+            std::list<OpcUaQualifiedName::SPtr>& browseNameList,
+            EventResult::Code& resultCode
+        );
+        //- EventBase interface
+        
+    
+      private:
+        EventVariables eventVariables_;
+    
+    };
+
+}
+
+#endif
