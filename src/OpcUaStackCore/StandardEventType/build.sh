aa08dfce3dc8a9f304d6706286fb18af1d835a69
Kai Huebl <kai@huebl-sgh.de>
2017-09-26 10:33:00 +0200

    test new command line parameter

--- src/OpcUaStackCore/StandardEventType/build.sh
+++ src/OpcUaStackCore/StandardEventType/build.sh
@@ -4,3 +4,5 @@ OpcUaEventTypeGenerator3 --nodeset Opc.Ua.NodeSet.xml --eventtype AuditEventType
 OpcUaEventTypeGenerator3 --nodeset Opc.Ua.NodeSet.xml --eventtype ConditionType
 OpcUaEventTypeGenerator3 --nodeset Opc.Ua.NodeSet.xml --eventtype AcknowledgeableConditionType
 OpcUaEventTypeGenerator3 --nodeset Opc.Ua.NodeSet.xml --eventtype AlarmConditionType
+
+OpcUaEventTypeGenerator3 --nodeset Opc.Ua.NodeSet.xml --eventtype BaseEventType --buildSubTypes 1 
