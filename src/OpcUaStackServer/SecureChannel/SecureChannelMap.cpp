e883b1395c9e6b04cfc92662e85838e1631b833c
Kai Huebl <kai@huebl-sgh.de>
2017-10-04 22:09:16 +0200

    added class SecureChannelMap

---
+++ src/OpcUaStackServer/SecureChannel/SecureChannelMap.cpp
@@ -0,0 +1,111 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaStackServer/SecureChannel/SecureChannelMap.h"
+
+namespace OpcUaStackServer
+{
+
+	SecureChannelMap::SecureChannelMap(void)
+	: secureChannelElementMap_()
+	{
+	}
+	
+	SecureChannelMap::~SecureChannelMap(void)
+	{
+		secureChannelElementMap_.clear();
+	}
+
+	uint32_t 
+	SecureChannelMap::size(void)
+	{
+		return secureChannelElementMap_.size();
+	}
+
+	bool 
+	SecureChannelMap::insert(OpcUaInt32 channelId, SecureChannelServerOld::SPtr secureChannelServer)
+	{
+		SecureChannelElementOld::SPtr secureChannelElement = this->secureChannelElement(channelId);
+		if (secureChannelElement.get() != nullptr) {
+			Log(Error, "channel already exist in secure channel element map")
+				.parameter("ChannelId", channelId);
+			return false;
+		}
+
+		secureChannelElement = constructSPtr<SecureChannelElementOld>();
+		secureChannelElement->channelId_ = channelId;
+		secureChannelElement->secureChannelServer_ = secureChannelServer;
+		secureChannelElement->secureChannelState_ = SecureChannelState_Init;
+		secureChannelElementMap_.insert(std::make_pair(channelId, secureChannelElement));
+		return true;
+	}
+
+	bool 
+	SecureChannelMap::connect(OpcUaInt32 channelId)
+	{
+		SecureChannelElementOld::SPtr secureChannelElement = this->secureChannelElement(channelId);
+		if (secureChannelElement.get() == nullptr) {
+			Log(Error, "channel not exist in secure channel element map")
+				.parameter("ChannelId", channelId);
+			return false;
+		}
+
+		secureChannelElement->secureChannelState_ = SecureChannelState_Connect;
+		return true;
+	}
+
+	bool 
+	SecureChannelMap::disconnect(OpcUaInt32 channelId)
+	{
+		SecureChannelElementOld::SPtr secureChannelElement = this->secureChannelElement(channelId);
+		if (secureChannelElement.get() == nullptr) {
+			Log(Error, "channel not exist in secure channel element map")
+				.parameter("ChannelId", channelId);
+			return false;
+		}
+
+		secureChannelElementMap_.erase(channelId);
+		return true;
+	}
+
+	SecureChannelServerOld::SPtr
+	SecureChannelMap::get(OpcUaUInt32 channelId)
+	{
+		SecureChannelElementOld::SPtr secureChannelElement = this->secureChannelElement(channelId);
+		if (secureChannelElement.get() != nullptr) {
+			return secureChannelElement->secureChannelServer_;
+		}
+
+		SecureChannelServerOld::SPtr secureChannel;
+		return secureChannel;
+	}
+
+	SecureChannelElementOld::SPtr
+	SecureChannelMap::secureChannelElement(OpcUaInt32 channelId)
+	{
+		SecureChannelElementOldMap::iterator it;
+		it = secureChannelElementMap_.find(channelId);
+		if (it != secureChannelElementMap_.end()) {
+			return it->second;
+		}
+
+		SecureChannelElementOld::SPtr secureChannelElement;
+		return secureChannelElement;
+	}
+
+}
---
+++ src/OpcUaStackServer/SecureChannel/SecureChannelMap.h
@@ -0,0 +1,73 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackServer_SecureChannelMap_h__
+#define __OpcUaStackServer_SecureChannelMap_h__
+
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/Base/ObjectPool.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaNumber.h"
+#include "OpcUaStackServer/SecureChannel/SecureChannelServerOld.h"
+#include <map>
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackServer
+{
+
+	typedef enum {
+		SecureChannelState_Init,
+		SecureChannelState_Connect
+	} SecureChannelState;
+
+
+	class DLLEXPORT SecureChannelElementOld
+	: public Object
+	{
+	  public:
+		typedef boost::shared_ptr<SecureChannelElementOld> SPtr;
+
+		OpcUaUInt32 channelId_;
+		SecureChannelState secureChannelState_;
+		SecureChannelServerOld::SPtr secureChannelServer_;
+	};
+
+
+	class DLLEXPORT SecureChannelMap
+	{
+	  public:
+		typedef std::map<OpcUaInt32, SecureChannelElementOld::SPtr> SecureChannelElementOldMap;
+
+		SecureChannelMap(void);
+		~SecureChannelMap(void);
+
+		uint32_t size(void);
+		bool insert(OpcUaInt32 channelId, SecureChannelServerOld::SPtr secureChannelServer);
+		bool connect(OpcUaInt32 channelId);
+		bool disconnect(OpcUaInt32 channelId);
+
+		SecureChannelServerOld::SPtr get(OpcUaUInt32 channelId);
+
+	  private:
+		SecureChannelElementOld::SPtr secureChannelElement(OpcUaInt32 channelId);
+
+		SecureChannelElementOldMap secureChannelElementMap_;
+	};
+
+}
+
+#endif
