b208a47ce9faed78eb76fb5364e3566e46661b52
Kai Huebl <kai@huebl-sgh.de>
2017-10-05 16:58:46 +0200

    added new class ChannelSessionHandleMap

---
+++ src/OpcUaStackServer/ServiceSet/ChannelSessionHandleMap.h
@@ -0,0 +1,42 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackServer_ChannelSessionHandleMap_h__
+#define __OpcUaStackServer_ChannelSessionHandleMap_h__
+
+#include <boost/shared_ptr.hpp>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackServer/ServiceSet/ChannelSessionHandle.h"
+
+namespace OpcUaStackServer
+{
+
+	class DLLEXPORT ChannelSessionHandleMap
+	{
+	  public:
+
+		typedef boost::shared_ptr<ChannelSessionHandleMap> SPtr;
+
+		ChannelSessionHandleMap(void);
+		~ChannelSessionHandleMap(void);
+
+	  private:
+	};
+
+}
+
+#endif
