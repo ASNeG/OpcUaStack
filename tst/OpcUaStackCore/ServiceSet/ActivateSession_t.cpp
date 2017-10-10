4468abd10e38edb2f5849f1f620db92934cda5a8
Kai Huebl <kai@huebl-sgh.de>
2017-10-10 12:52:13 +0200

    update activate session request unittest

--- tst/OpcUaStackCore/ServiceSet/ActivateSession_t.cpp
+++ tst/OpcUaStackCore/ServiceSet/ActivateSession_t.cpp
@@ -68,13 +68,6 @@ BOOST_AUTO_TEST_CASE(ActivateSession_Request)
 	*opcUaGuidSPtr = "12345678-9ABC-DEF0-1234-56789ABCDEF0";
 	activateSessionRequestSPtr = constructSPtr<ActivateSessionRequest>();
 
-	activateSessionRequestSPtr->requestHeader()->sessionAuthenticationToken().namespaceIndex(1);
-	activateSessionRequestSPtr->requestHeader()->sessionAuthenticationToken().nodeId(opcUaGuidSPtr);
-	activateSessionRequestSPtr->requestHeader()->time(ptime);
-	activateSessionRequestSPtr->requestHeader()->requestHandle(2);
-	activateSessionRequestSPtr->requestHeader()->returnDisagnostics(0);
-	activateSessionRequestSPtr->requestHeader()->timeoutHint(10000);
-
 	activateSessionRequestSPtr->localeIds()->resize(1);
 	localeIdSPtr = constructSPtr<OpcUaString>();
 	*localeIdSPtr = "en";
@@ -97,15 +90,12 @@ BOOST_AUTO_TEST_CASE(ActivateSession_Request)
 	OpcUaStackCore::dumpHex(ios);
 
 	std::stringstream ss;
-	ss	<< "4d 53 47 46 7f 00 00 00  d9 7a 25 09 01 00 00 00"
-		<< "35 00 00 00 03 00 00 00  01 00 d3 01 04 01 00 12"
-		<< "34 56 78 9a bc de f0 12  34 56 78 9a bc de f0 00"
-		<< "00 00 00 00 00 00 00 02  00 00 00 00 00 00 00 ff"
-		<< "ff ff ff 10 27 00 00 00  00 00 ff ff ff ff ff ff"
-		<< "ff ff 00 00 00 00 01 00  00 00 02 00 00 00 65 6e"
-		<< "01 00 41 01 01 0e 00 00  00 0a 00 00 00 4f 70 63"
-		<< "55 61 53 74 61 63 6b ff  ff ff ff ff ff ff ff";
-
+	ss << "4d 53 47 46 51 00 00 00  d9 7a 25 09 01 00 00 00"
+	   << "35 00 00 00 03 00 00 00  01 00 d3 01 ff ff ff ff"
+	   << "ff ff ff ff 00 00 00 00  01 00 00 00 02 00 00 00"
+	   << "65 6e 01 00 41 01 01 0e  00 00 00 0a 00 00 00 4f"
+	   << "70 63 55 61 53 74 61 63  6b ff ff ff ff ff ff ff"
+	   << "ff";
 	BOOST_REQUIRE(OpcUaStackCore::compare(ios, ss.str(), pos) == true);
 
 	// decode MessageHeader
@@ -137,15 +127,6 @@ BOOST_AUTO_TEST_CASE(ActivateSession_Request)
 	activateSessionRequestSPtr->opcUaBinaryDecode(ios);
 
 	std::string str;
-	str = *activateSessionRequestSPtr->requestHeader()->sessionAuthenticationToken().nodeId<OpcUaGuid::SPtr>();
-	BOOST_REQUIRE(activateSessionRequestSPtr->requestHeader()->sessionAuthenticationToken().namespaceIndex() == 1);
-	BOOST_REQUIRE(str == "12345678-9ABC-DEF0-1234-56789ABCDEF0");
-	BOOST_REQUIRE(activateSessionRequestSPtr->requestHeader()->time().dateTime() == ptime);
-	BOOST_REQUIRE(activateSessionRequestSPtr->requestHeader()->requestHandle() == 2);
-	BOOST_REQUIRE(activateSessionRequestSPtr->requestHeader()->returnDisagnostics() == 0);
-	BOOST_REQUIRE(activateSessionRequestSPtr->requestHeader()->timeoutHint() == 10000);
-	BOOST_REQUIRE(activateSessionRequestSPtr->localeIds()->size() == 1);
-	BOOST_REQUIRE(activateSessionRequestSPtr->localeIds()->get(0, localeIdSPtr) == true);
 	str = *localeIdSPtr;
 	BOOST_REQUIRE(str == "en");
 	BOOST_REQUIRE(activateSessionRequestSPtr->userIdentityToken()->parameterTypeId().nodeId<OpcUaUInt32>() == OpcUaId_AnonymousIdentityToken_Encoding_DefaultBinary);
