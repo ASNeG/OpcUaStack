c937282fde6e66602fab544d6cf1577ac5129ef9
Kai Huebl <kai@huebl-sgh.de>
2017-11-05 16:05:01 +0100

    added new class BrowsePathToNodeIdRequest

---
+++ src/OpcUaStackCore/ServiceSetApplication/BrowsePathToNodeIdRequest.cpp
@@ -0,0 +1,103 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/ServiceSetApplication/BrowsePathToNodeIdRequest.h"
+
+namespace OpcUaStackCore
+{
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// BrowseNameArray
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	BrowseName::BrowseName(void)
+	: nodeId_()
+	, pathNames_()
+	{
+	}
+
+	BrowseName::~BrowseName(void)
+	{
+	}
+
+	void
+	BrowseName::nodeId(OpcUaNodeId& nodeId)
+	{
+		nodeId_ = nodeId;
+	}
+
+	OpcUaNodeId&
+	BrowseName::nodeId(void)
+	{
+		return nodeId_;
+	}
+
+	void
+	BrowseName::pathNames(OpcUaQualifiedNameArray::SPtr& pathNames)
+	{
+		pathNames_ = pathNames;
+	}
+
+	OpcUaQualifiedNameArray::SPtr
+	BrowseName::pathNames(void)
+	{
+		return pathNames_;
+	}
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// OpcUa BrowsePathToNodeIdRequest
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	BrowsePathToNodeIdRequest::BrowsePathToNodeIdRequest(void)
+	: Object()
+	, browseNameArray_()
+	{
+	}
+
+	BrowsePathToNodeIdRequest::~BrowsePathToNodeIdRequest(void)
+	{
+	}
+
+	void
+	BrowsePathToNodeIdRequest::browseNameArray(BrowseNameArray::SPtr& browseNameArray)
+	{
+		browseNameArray_ = browseNameArray;
+	}
+
+	BrowseNameArray::SPtr
+	BrowsePathToNodeIdRequest::browseNameArray(void)
+	{
+		return browseNameArray_;
+	}
+
+	void
+	BrowsePathToNodeIdRequest::opcUaBinaryEncode(std::ostream& os) const
+	{
+	}
+
+	void
+	BrowsePathToNodeIdRequest::opcUaBinaryDecode(std::istream& is)
+	{
+	}
+
+}
---
+++ src/OpcUaStackCore/ServiceSetApplication/BrowsePathToNodeIdRequest.h
@@ -0,0 +1,80 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackCore_BrowsePathToNodeIdRequest_h__
+#define __OpcUaStackCore_BrowsePathToNodeIdRequest_h__
+
+#include <stdint.h>
+#include "OpcUaStackCore/Base/ObjectPool.h"
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaNodeId.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaQualifiedName.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaArray.h"
+
+namespace OpcUaStackCore
+{
+
+	class DLLEXPORT BrowseName
+	{
+	  public:
+		typedef boost::shared_ptr<BrowseName> SPtr;
+
+		BrowseName(void);
+		~BrowseName(void);
+
+		void nodeId(OpcUaNodeId& nodeId);
+		OpcUaNodeId& nodeId(void);
+		void pathNames(OpcUaQualifiedNameArray::SPtr& pathNames);
+		OpcUaQualifiedNameArray::SPtr pathNames(void);
+
+	  private:
+		OpcUaNodeId nodeId_;
+		OpcUaQualifiedNameArray::SPtr pathNames_;
+	};
+
+
+	class BrowseNameArray
+	: public OpcUaArray<BrowseName::SPtr, SPtrTypeCoder<BrowseName> >
+	, public Object
+	{
+	  public:
+		typedef boost::shared_ptr<BrowseNameArray> SPtr;
+	};
+
+
+	class DLLEXPORT BrowsePathToNodeIdRequest
+	: public  Object
+	{
+	  public:
+		typedef boost::shared_ptr<BrowsePathToNodeIdRequest> SPtr;
+
+		BrowsePathToNodeIdRequest(void);
+		virtual ~BrowsePathToNodeIdRequest(void);
+
+		void browseNameArray(BrowseNameArray::SPtr& browseNameArray);
+		BrowseNameArray::SPtr browseNameArray(void);
+
+		void opcUaBinaryEncode(std::ostream& os) const;
+		void opcUaBinaryDecode(std::istream& is);
+
+	  private:
+		BrowseNameArray::SPtr browseNameArray_;
+	};
+
+}
+
+#endif
