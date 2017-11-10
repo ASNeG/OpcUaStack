81c758f8e3561791e6a5ea1da5d7020a4f92cdf7
Kai Huebl <kai@huebl-sgh.de>
2017-11-10 21:53:21 +0100

    added new class DataSetMessageHeader

---
+++ src/OpcUaStackPubSub/DataSetMessage/DataSetMessageHeader.cpp
@@ -0,0 +1,43 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackPubSub/DataSetMessage/DataSetMessageHeader.h"
+
+namespace OpcUaStackPubSub
+{
+
+	DataSetMessageHeader::DataSetMessageHeader(void)
+	{
+	}
+
+	DataSetMessageHeader::~DataSetMessageHeader(void)
+	{
+	}
+
+	void
+	DataSetMessageHeader::opcUaBinaryEncode(std::ostream& os) const
+	{
+		// FIXME: todo
+	}
+
+	void
+	DataSetMessageHeader::opcUaBinaryDecode(std::istream& is)
+	{
+		// FIXME: todo
+	}
+
+}
