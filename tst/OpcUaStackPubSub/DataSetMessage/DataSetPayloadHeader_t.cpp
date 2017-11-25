150f96f087d1ee6caad0bc9ea4a89efd687e141c
Aleksey Timin <atimin@gmail.com>
2017-11-25 12:36:33 +0500

    fix DataSetPayloadHeader test

--- tst/OpcUaStackPubSub/DataSetMessage/DataSetPayloadHeader_t.cpp
+++ tst/OpcUaStackPubSub/DataSetMessage/DataSetPayloadHeader_t.cpp
@@ -18,6 +18,7 @@ BOOST_AUTO_TEST_CASE(DataSetPayloadHeader_encode_decode)
 	OpcUaUInt16 val;
 	DataSetPayloadHeader value1, value2;
 
+	value1.dataSetArrayEnabled(true);
 	value1.dataSetWriterIds()->resize(5);
 	value1.dataSetWriterIds()->push_back(1);
 	value1.dataSetWriterIds()->push_back(2);
@@ -26,6 +27,7 @@ BOOST_AUTO_TEST_CASE(DataSetPayloadHeader_encode_decode)
 	value1.dataSetWriterIds()->push_back(5);
 	value1.opcUaBinaryEncode(ss);
 
+	value2.dataSetArrayEnabled(true);
 	value2.opcUaBinaryDecode(ss);
 	BOOST_REQUIRE(value2.dataSetWriterIds()->size() == 5);
 	value2.dataSetWriterIds()->get(0, val);
