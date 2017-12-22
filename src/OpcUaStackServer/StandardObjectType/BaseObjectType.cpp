d55c37db2294d8a5f92b52a6f97f6b4e75cd3c91
Kai Huebl <kai@huebl-sgh.de>
2017-12-22 20:40:38 +0100

    added new class BaseObjectType

---
+++ src/OpcUaStackServer/StandardObjectType/BaseObjectType.cpp
@@ -0,0 +1,41 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaStackServer/StandardObjectType/BaseObjectType.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackServer
+{
+
+	BaseObjectType::BaseObjectType(void)
+	: ObjectBase()
+	{
+	}
+
+	BaseObjectType::~BaseObjectType(void)
+	{
+	}
+
+	bool
+	BaseObjectType::linkInstanceWithModel(const OpcUaNodeId& nodeId)
+	{
+		return ObjectBase::linkInstanceWithModel(nodeId);
+	}
+
+}
---
+++ src/OpcUaStackServer/StandardObjectType/BaseObjectType.h
@@ -0,0 +1,46 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackServer_BaseObjectType_h__
+#define __OpcUaStackServer_BaseObjectType_h__
+
+#include <boost/shared_ptr.hpp>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackServer/ObjectType/ObjectBase.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackServer
+{
+
+	class DLLEXPORT BaseObjectType
+	: public ObjectBase
+	{
+	  public:
+		typedef boost::shared_ptr<BaseObjectType> SPtr;
+
+		BaseObjectType(void);
+		virtual ~BaseObjectType(void);
+
+		virtual bool linkInstanceWithModel(const OpcUaNodeId& nodeId);
+
+	  private:
+	};
+
+}
+
+#endif
