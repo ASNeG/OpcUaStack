f6140cb359fdfcdccb7fc8f7623f00875c0c7265
Kai Huebl <kai@huebl-sgh.de>
2018-01-31 08:46:40 +0100

    implemented class PublicKey

--- src/OpcUaStackCore/Certificate/PublicKey.h
+++ src/OpcUaStackCore/Certificate/PublicKey.h
@@ -18,17 +18,40 @@
 #ifndef __OpcUaStackCore_PublicKey_h__
 #define __OpcUaStackCore_PublicKey_h__
 
+#include <openssl/x509.h>
+
 #include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/Certificate/OpenSSLError.h"
 
 namespace OpcUaStackCore
 {
+
+    typedef enum
+    {
+        KeyType_RSA        = EVP_PKEY_RSA,
+        KeyType_DSA        = EVP_PKEY_DSA,
+        KeyType_Unknown    = EVP_PKEY_NONE
+    } KeyType;
+
 	class DLLEXPORT PublicKey
+	: public OpenSSLError
 	{
 	  public:
 		PublicKey(void);
+		PublicKey(EVP_PKEY *pKey);
+		PublicKey(const PublicKey& copy);
 		~PublicKey(void);
 
+		PublicKey& operator=(const PublicKey& copy);
+		operator EVP_PKEY*(void) const;
+		uint32_t keySize(void) const;
+		KeyType keyType(void) const;
+
+		bool fromDER(const char* buf, uint32_t bufLen);
+		bool toDER(char* buf, uint32_t& bufLen);
+
 	  private:
+		X509_PUBKEY *publicKey_;
 	};
 
 }
