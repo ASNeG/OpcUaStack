1258bffe28e2d349cc6eee1c7fb07d358cc861b7
Kai Huebl <kai@huebl-sgh.de>
2018-01-31 17:54:01 +0100

    added new class KeyType

---
+++ src/OpcUaStackCore/Certificate/KeyType.h
@@ -0,0 +1,37 @@
+/*
+   Copyright 2018 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackCore_KeyType_h__
+#define __OpcUaStackCore_KeyType_h__
+
+#include <openssl/x509.h>
+
+#include "OpcUaStackCore/Base/os.h"
+
+namespace OpcUaStackCore
+{
+
+    typedef enum
+    {
+        KeyType_RSA        = EVP_PKEY_RSA,
+        KeyType_DSA        = EVP_PKEY_DSA,
+        KeyType_Unknown    = EVP_PKEY_NONE
+    } KeyType;
+
+}
+
+#endif
