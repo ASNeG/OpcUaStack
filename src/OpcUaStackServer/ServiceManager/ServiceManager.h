c648d90e734eb48624fd3ce95249f2b78d574ef1
Kai Huebl <kai@huebl-sgh.de>
2018-02-02 13:09:38 +0100

    rework class ServiceManager

--- src/OpcUaStackServer/ServiceManager/ServiceManager.h
+++ src/OpcUaStackServer/ServiceManager/ServiceManager.h
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2018 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -21,7 +21,6 @@
 #include "OpcUaStackCore/Base/os.h"
 #include "OpcUaStackCore/Base/IOService.h"
 #include "OpcUaStackServer/InformationModel/InformationModel.h"
-//#include "OpcUaStackServer/ServiceSet/SessionManagerOld.h"
 #include "OpcUaStackServer/ServiceSet/SessionManager.h"
 #include "OpcUaStackServer/ServiceSet/AttributeService.h"
 #include "OpcUaStackServer/ServiceSet/MethodService.h"
@@ -44,7 +43,6 @@ namespace OpcUaStackServer
 		ServiceManager(void);
 		~ServiceManager(void);
 
-		//bool init(SessionManagerOld& sessionManager);
 		bool init(SessionManager& sessionManager);
 		bool informatinModel(InformationModel::SPtr informatinModel);
 		bool ioThread(IOThread* ioThread);
@@ -56,6 +54,8 @@ namespace OpcUaStackServer
 
 	  private:
 		void initForwardGlobalSync(void);
+
+		ForwardGlobalSync::SPtr forwardGlobalSync_;
 
 		TransactionManager::SPtr transactionManager_;
 		AttributeService::SPtr attributeService_;
