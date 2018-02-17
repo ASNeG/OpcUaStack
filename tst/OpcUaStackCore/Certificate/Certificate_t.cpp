419f406501b0566393bed03e554572c58efcbfbf
Kai Huebl <kai@huebl-sgh.de>
2018-02-17 19:56:32 +0100

    added test to read certificate from file

--- tst/OpcUaStackCore/Certificate/Certificate_t.cpp
+++ tst/OpcUaStackCore/Certificate/Certificate_t.cpp
@@ -37,9 +37,65 @@ BOOST_AUTO_TEST_CASE(Certificate__createSelfSignedCertificate)
 	Certificate certificate(info, identity, key);
 	BOOST_REQUIRE(certificate.isError() == false);
 	BOOST_REQUIRE(certificate.isSelfSigned() == true);
+}
+
+BOOST_AUTO_TEST_CASE(Certificate__writeFile)
+{
+	RSAKey key(2048);
+	Info info;
+	Identity identity;
+
+	info.uri("urn:localhost:ASNeG:MyServiceApplication");
+	info.ipAddresses().push_back("127.0.0.1");
+	info.dnsNames().push_back("ASNeG.de");
+	info.eMail("info@ASNeG.de");
+	info.validTime(3600*24*5);
+	info.serialNumber(123);
+	info.validFrom(0);
+
+	identity.organization("ASNeG");
+	identity.organizationUnit("OPC UA Service Department");
+	identity.commonName("MyServiceApplication");
+	identity.locality("Neukirchen");
+	identity.state("Hessen");
+	identity.country("DE");
+	identity.domainComponent("asneg.de");
+
+	Certificate certificate(info, identity, key);
+	BOOST_REQUIRE(certificate.isError() == false);
+	BOOST_REQUIRE(certificate.isSelfSigned() == true);
 	BOOST_REQUIRE(certificate.toDERFile("selfSignedCertificate.DER") == true);
+}
 
+BOOST_AUTO_TEST_CASE(Certificate__readFile)
+{
+	RSAKey key(2048);
+	Info info;
+	Identity identity;
 
+	info.uri("urn:localhost:ASNeG:MyServiceApplication");
+	info.ipAddresses().push_back("127.0.0.1");
+	info.dnsNames().push_back("ASNeG.de");
+	info.eMail("info@ASNeG.de");
+	info.validTime(3600*24*5);
+	info.serialNumber(123);
+	info.validFrom(0);
+
+	identity.organization("ASNeG");
+	identity.organizationUnit("OPC UA Service Department");
+	identity.commonName("MyServiceApplication");
+	identity.locality("Neukirchen");
+	identity.state("Hessen");
+	identity.country("DE");
+	identity.domainComponent("asneg.de");
+
+	Certificate certificate1(info, identity, key);
+	BOOST_REQUIRE(certificate1.isError() == false);
+	BOOST_REQUIRE(certificate1.isSelfSigned() == true);
+	BOOST_REQUIRE(certificate1.toDERFile("selfSignedCertificate.DER") == true);
+
+	Certificate certificate2;
+	BOOST_REQUIRE(certificate2.fromDERFile("selfSignedCertificate.DER") == true);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
