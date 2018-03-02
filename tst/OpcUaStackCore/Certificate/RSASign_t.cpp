cd4909c9d1ef12116e74ebc8eeeae7c0a1305285
Kai Huebl <kai@huebl-sgh.de>
2018-03-02 22:58:06 +0100

    added sign unittest

---
+++ tst/OpcUaStackCore/Certificate/RSASign_t.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include "OpcUaStackCore/Certificate/RSAKey.h"
+#include "OpcUaStackCore/Certificate/PrivateKey.h"
+#include "unittest.h"
+#include <openssl/bio.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(RSASign_)
+
+BOOST_AUTO_TEST_CASE(RSASign_)
+{
+	std::cout << "RSASign_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(RSASign_sign)
+{
+	char digest[20];
+	memcpy(digest, "01234567890123456789", 20);
+
+	RSAKey rsaKey(2048);
+	PrivateKey privateKey = rsaKey.privateKey();
+	EVP_PKEY* key = privateKey;
+	RSA* r = key->pkey.rsa;
+
+	unsigned char *sig = NULL;
+	unsigned int sig_len = 0;
+
+	sig = (unsigned char*)malloc(RSA_size(r));
+
+	int rc = RSA_sign(NID_sha1, (const unsigned char*)digest, 20, sig, &sig_len, r);
+	BOOST_REQUIRE(rc == 1);
+	BOOST_REQUIRE(sig_len == 256);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
