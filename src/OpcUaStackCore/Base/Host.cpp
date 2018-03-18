1a3ef1c48895f47c9a0f2d1b3e7a8706ecabe2ab
Kai Huebl <kai@huebl-sgh.de>
2018-03-18 10:18:41 +0100

    added new class Host

---
+++ src/OpcUaStackCore/Base/Host.cpp
@@ -0,0 +1,41 @@
+/*
+   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include <boost/lexical_cast.hpp>
+#include <boost/regex.hpp>
+#include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
+#include "OpcUaStackCore/Base/Host.h"
+
+namespace OpcUaStackCore
+{
+
+	Host::Host(void)
+	: hostname_("")
+	{
+	}
+
+	Host::Host(const std::string& hostname)
+	: hostname_(hostname)
+	{
+	}
+
+	Host::~Host(void)
+	{
+	}
+
+}
