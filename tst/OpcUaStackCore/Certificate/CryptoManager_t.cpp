ff3c519147e8d230f1405b1ff16b41c7e0385968
Kai Huebl <kai@huebl-sgh.de>
2018-03-28 21:54:53 +0200

    added new unittest CryptoManager_tz

---
+++ tst/OpcUaStackCore/Certificate/CryptoManager_t.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "unittest.h"
+
+
+//using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(CryptoManager_)
+
+BOOST_AUTO_TEST_CASE(CryptoManager_)
+{
+	std::cout << "CryptoManager_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
