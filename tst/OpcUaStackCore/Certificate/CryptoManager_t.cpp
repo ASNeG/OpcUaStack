1749ec5094ff62377eb0a5b93badccc285fb7652
Kai Huebl <kai@huebl-sgh.de>
2018-03-29 11:22:14 +0200

    added unittest BASIC128RSA15_assymmetic_encrypt_decrypt

--- tst/OpcUaStackCore/Certificate/CryptoManager_t.cpp
+++ tst/OpcUaStackCore/Certificate/CryptoManager_t.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
+#include "OpcUaStackCore/Certificate/RSAKey.h"
+#include "OpcUaStackCore/Certificate/CryptoManager.h"
 #include "unittest.h"
 
 
-//using namespace OpcUaStackCore;
+using namespace OpcUaStackCore;
 
 BOOST_AUTO_TEST_SUITE(CryptoManager_)
 
 BOOST_AUTO_TEST_CASE(CryptoManager_)
 {
 	std::cout << "CryptoManager_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(CryptoManager_BASIC128RSA15_assymmetic_encrypt_decrypt)
+{
+	OpcUaStatusCode statusCode;
+
+	RSAKey key(2048);
+	PublicKey publicKey = key.publicKey();
+	PrivateKey privateKey = key.privateKey();
+
+	CryptoManager cryptoManager;
+	CryptoBase::SPtr cryptoBase = cryptoManager.get("http://opcfoundation.org/UA/SecurityPolicy#Basic128Rsa15");
+	BOOST_REQUIRE(cryptoBase.get() != nullptr);
+
+	MemoryBuffer plainText1(100);
+	MemoryBuffer encryptText(256);
+	uint32_t encryptTextLen = 256;
+
+	for (uint32_t idx=0; idx<100; idx++) plainText1.memBuf()[idx] = idx;
+
+	statusCode = cryptoBase->asymmetricEncrypt(
+		plainText1.memBuf(),
+		plainText1.memLen(),
+		publicKey,
+		encryptText.memBuf(),
+		&encryptTextLen
+	);
+	BOOST_REQUIRE(statusCode == Success);
+	BOOST_REQUIRE(encryptTextLen == 256);
+
+	MemoryBuffer plainText2(100);
+	uint32_t plainTextLen = 100;
+
+	statusCode = cryptoBase->asymmetricDecrypt(
+		encryptText.memBuf(),
+		encryptText.memLen(),
+		privateKey,
+		plainText2.memBuf(),
+		&plainTextLen
+	);
+	BOOST_REQUIRE(statusCode == Success);
+	BOOST_REQUIRE(plainTextLen == 100);
+	BOOST_REQUIRE(memcmp(plainText1.memBuf(), plainText2.memBuf(), 100) == 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
