df080ee566c2233b2c02437c373f7eb0beaa9d88
Kai Huebl <kai@huebl-sgh.de>
2018-03-30 23:23:01 +0200

    added unittest to encode/decode a certificate chain

--- tst/OpcUaStackCore/Certificate/CertificateChain_t.cpp
+++ tst/OpcUaStackCore/Certificate/CertificateChain_t.cpp
@@ -7,9 +7,53 @@ using namespace OpcUaStackCore;
 
 BOOST_AUTO_TEST_SUITE(CertificateChain_)
 
+Certificate::SPtr createCertificate(void)
+{
+	RSAKey key(2048);
+	CertificateInfo info;
+	Identity identity;
+
+	info.uri("urn:localhost:ASNeG:MyServiceApplication");
+	info.ipAddresses().push_back("127.0.0.1");
+	info.dnsNames().push_back("ASNeG.de");
+	info.eMail("info@ASNeG.de");
+	info.validTime(boost::posix_time::microsec_clock::local_time() + boost::posix_time::seconds(3600*24*365*5));
+	info.serialNumber(123);
+	info.validFrom(boost::posix_time::microsec_clock::local_time());
+
+	identity.organization("ASNeG");
+	identity.organizationUnit("OPC UA Service Department");
+	identity.commonName("MyServiceApplication");
+	identity.locality("Neukirchen");
+	identity.state("Hessen");
+	identity.country("DE");
+	identity.domainComponent("asneg.de");
+
+	return constructSPtr<Certificate>(info, identity, key);
+}
+
 BOOST_AUTO_TEST_CASE(CertificateChain_)
 {
 	std::cout << "CertificateChain_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(CertificateChain_encode_decode)
+{
+	// create certificate chain
+	CertificateChain certificateChain1;
+	for (uint32_t idx=0; idx<10; idx++) {
+		Certificate::SPtr certificate = createCertificate();
+		certificateChain1.addCertificate(certificate);
+	}
+
+	// encode certificate chain
+	OpcUaByteString byteString;
+	BOOST_REQUIRE(certificateChain1.toByteString(byteString) == true);
+
+	// decode certificate chain
+	CertificateChain certificateChain2;
+	BOOST_REQUIRE(certificateChain2.fromByteString(byteString) == true);
+	BOOST_REQUIRE(certificateChain2.size() == 10);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
