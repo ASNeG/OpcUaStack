f26f968a5bec10da1edbeacc162a1d7c30c1f6ea
Kai Huebl <kai@huebl-sgh.de>
2018-03-30 21:35:34 +0200

    added new unittest CertificateChain_t

---
+++ tst/OpcUaStackCore/Certificate/CertificateChain_t.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "unittest.h"
+#include "OpcUaStackCore/Certificate/CertificateChain.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(CertificateChain_)
+
+BOOST_AUTO_TEST_CASE(CertificateChain_)
+{
+	std::cout << "CertificateChain_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
