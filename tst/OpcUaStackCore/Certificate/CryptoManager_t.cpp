6c0fb85ba1318bdc62cf33b8975a6ff30da0f4a2
Kai Huebl <kai@huebl-sgh.de>
2018-03-30 11:12:41 +0200

    added unittest CryptoManager_BASIC256SHA256_asymmetric_encrypt_decrypt

--- tst/OpcUaStackCore/Certificate/CryptoManager_t.cpp
+++ tst/OpcUaStackCore/Certificate/CryptoManager_t.cpp
@@ -20,7 +20,7 @@ BOOST_AUTO_TEST_CASE(CryptoManager_)
 //
 // ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE(CryptoManager_BASIC128RSA15_assymmetic_encrypt_decrypt)
+BOOST_AUTO_TEST_CASE(CryptoManager_BASIC128RSA15_asymmetric_encrypt_decrypt)
 {
 	OpcUaStatusCode statusCode;
 
@@ -30,8 +30,8 @@ BOOST_AUTO_TEST_CASE(CryptoManager_BASIC128RSA15_assymmetic_encrypt_decrypt)
 
 	CryptoManager cryptoManager;
 	CryptoBase::SPtr cryptoBase = cryptoManager.get("http://opcfoundation.org/UA/SecurityPolicy#Basic128Rsa15");
-	cryptoBase->isLogging(true);
 	BOOST_REQUIRE(cryptoBase.get() != nullptr);
+	cryptoBase->isLogging(true);
 
 	MemoryBuffer plainText1(100);
 	MemoryBuffer encryptText(256);
@@ -76,8 +76,8 @@ BOOST_AUTO_TEST_CASE(CryptoManager_BASIC128RSA15_symmetric_encrypt_decrypt)
 
 	CryptoManager cryptoManager;
 	CryptoBase::SPtr cryptoBase = cryptoManager.get("http://opcfoundation.org/UA/SecurityPolicy#Basic128Rsa15");
-	cryptoBase->isLogging(true);
 	BOOST_REQUIRE(cryptoBase.get() != nullptr);
+	cryptoBase->isLogging(true);
 
 	MemoryBuffer plainText1(256);
 	MemoryBuffer encryptText(256);
@@ -122,8 +122,8 @@ BOOST_AUTO_TEST_CASE(CryptoManager_BASIC128RSA15_asymmetric_sign_verify)
 
 	CryptoManager cryptoManager;
 	CryptoBase::SPtr cryptoBase = cryptoManager.get("http://opcfoundation.org/UA/SecurityPolicy#Basic128Rsa15");
-	cryptoBase->isLogging(true);
 	BOOST_REQUIRE(cryptoBase.get() != nullptr);
+	cryptoBase->isLogging(true);
 
 	MemoryBuffer plainText1(100);
 	MemoryBuffer signText(256);
@@ -163,8 +163,8 @@ BOOST_AUTO_TEST_CASE(CryptoManager_BASIC128RSA15_symmetric_sign_verify)
 
 	CryptoManager cryptoManager;
 	CryptoBase::SPtr cryptoBase = cryptoManager.get("http://opcfoundation.org/UA/SecurityPolicy#Basic128Rsa15");
-	cryptoBase->isLogging(true);
 	BOOST_REQUIRE(cryptoBase.get() != nullptr);
+	cryptoBase->isLogging(true);
 
 	MemoryBuffer plainText1(100);
 	MemoryBuffer signText(20);
@@ -199,6 +199,50 @@ BOOST_AUTO_TEST_CASE(CryptoManager_BASIC128RSA15_symmetric_sign_verify)
 //
 // ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(CryptoManager_BASIC256SHA256_asymmetric_encrypt_decrypt)
+{
+	OpcUaStatusCode statusCode;
+
+	RSAKey key(2048);
+	PublicKey publicKey = key.publicKey();
+	PrivateKey privateKey = key.privateKey();
+
+	CryptoManager cryptoManager;
+	CryptoBase::SPtr cryptoBase = cryptoManager.get("http://opcfoundation.org/UA/SecurityPolicy#Basic256Sha256");
+	BOOST_REQUIRE(cryptoBase.get() != nullptr);
+	cryptoBase->isLogging(true);
+
+	MemoryBuffer plainText1(100);
+	MemoryBuffer encryptText(256);
+	uint32_t encryptTextLen = 256;
+
+	for (uint32_t idx=0; idx<100; idx++) plainText1.memBuf()[idx] = idx;
+
+	statusCode = cryptoBase->asymmetricEncrypt(
+		plainText1.memBuf(),
+		plainText1.memLen(),
+		publicKey,
+		encryptText.memBuf(),
+		&encryptTextLen
+	);
+	BOOST_REQUIRE(statusCode == Success);
+	BOOST_REQUIRE(encryptTextLen == 256);
+
+	MemoryBuffer plainText2(100);
+	uint32_t plainTextLen = 100;
+
+	statusCode = cryptoBase->asymmetricDecrypt(
+		encryptText.memBuf(),
+		encryptText.memLen(),
+		privateKey,
+		plainText2.memBuf(),
+		&plainTextLen
+	);
+	BOOST_REQUIRE(statusCode == Success);
+	BOOST_REQUIRE(plainTextLen == 100);
+	BOOST_REQUIRE(memcmp(plainText1.memBuf(), plainText2.memBuf(), 100) == 0);
+}
+
 
 // ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
