6a7b2f26a6f23c930b82ce3f430d857465f19d38
Kai Huebl <kai@huebl-sgh.de>
2018-04-23 19:26:13 +0200

    added BASIC256 test

--- tst/OpcUaStackCore/Certificate/Random_t.cpp
+++ tst/OpcUaStackCore/Certificate/Random_t.cpp
@@ -45,4 +45,34 @@ BOOST_AUTO_TEST_CASE(Random__keyDerivePSHA1_BASIC128RSA15)
 	BOOST_REQUIRE(key == keyCheck);
 }
 
+BOOST_AUTO_TEST_CASE(Random__keyDerivePSHA256_BASIC256)
+{
+	Random random;
+
+	MemoryBuffer secret(32);	// remote nonce (32 Byte)
+    MemoryBuffer seed(32);		// local nonce (32 Byte)
+	MemoryBuffer key(72);		// len = sig key + enc key + iv
+								// sig key:	24 (derivedSignatureKeyLen)
+								// enc key:	32 (derivedEncryptionKeyLen)
+								// iv:		16 (symmetricKeyLen)
+
+	memset(secret.memBuf(), 0x01, 32);
+	memset(seed.memBuf(), 0x02, 32);
+
+	BOOST_REQUIRE(random.keyDerivePSHA1(secret, seed, key) == Success);
+	BOOST_REQUIRE(key.memLen() == 72);
+
+	MemoryBuffer keyCheck(72);
+	hexStringToByteSequence(
+		"e7 de e6 95 89 9a f2 c5 b5 60 3c 57 cc 0b bf b1"
+		"ce f0 1b 0c 5b 4c 1f 11 3a 95 b0 64 ce bd 65 ed"
+		"59 2a 4d 53 dd 0b 0e b6 24 5c 0c 64 25 7d 8d b0"
+		"03 bb f1 53 9d 01 a1 c1 81 90 27 e9 20 11 48 87"
+		"f9 ba 1a d0 70 a4 b8 87",
+		(uint8_t*)keyCheck.memBuf()
+	);
+	BOOST_REQUIRE(key == keyCheck);
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
