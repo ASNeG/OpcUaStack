414edbab1d4f7285bf2cedc713024afbb2b942dd
Kai Huebl <kai@huebl-sgh.de>
2018-04-26 20:47:48 +0200

    added a new getter method

--- src/OpcUaStackCore/ServiceSet/CreateSessionResponse.cpp
+++ src/OpcUaStackCore/ServiceSet/CreateSessionResponse.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2018 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -97,6 +97,12 @@ namespace OpcUaStackCore
 		serverCertificate_.value(buf, bufLen);
 	}
 
+	OpcUaByteString&
+	CreateSessionResponse::serverCertificate(void)
+	{
+		return serverCertificate_;
+	}
+
 	void 
 	CreateSessionResponse::serverEndpoints(const EndpointDescriptionArray::SPtr serverEndpoints)
 	{
@@ -119,6 +125,12 @@ namespace OpcUaStackCore
 	CreateSessionResponse::serverSoftwareCertificate(const OpcUaByte* buf, OpcUaInt32 bufLen)
 	{
 		serverSoftwareCertificate_.value(buf, bufLen);
+	}
+
+	OpcUaByteString&
+	CreateSessionResponse::serverSoftwareCertificate(void)
+	{
+		return serverSoftwareCertificate_;
 	}
 
 	void 
