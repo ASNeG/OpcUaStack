3f264b6413824399b283f5621d6dc6d3bc93cd03
Kai Huebl <kai@huebl-sgh.de>
2018-04-28 09:14:54 +0200

    added crypto base to secure channel transaction

--- src/OpcUaStackCore/SecureChannel/SecureChannelTransaction.cpp
+++ src/OpcUaStackCore/SecureChannel/SecureChannelTransaction.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2018 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -23,7 +23,11 @@ namespace OpcUaStackCore
 	SecureChannelTransaction::SecureChannelTransaction(void)
 	: is_()
 	, os_()
+	, requestTypeNodeId_()
+	, responseTypeNodeId_()
+	, securityTokenId_()
 	, requestId_(0)
+	, cryptoBase_()
 	{
 	}
 
