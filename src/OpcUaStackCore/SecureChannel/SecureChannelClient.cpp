c6ccdc3ed405bf02072dffda2d404d7a95d4cbfb
Kai Huebl <kai@huebl-sgh.de>
2018-07-03 09:53:02 +0200

    use logging flag in secure channel client

--- src/OpcUaStackCore/SecureChannel/SecureChannelClient.cpp
+++ src/OpcUaStackCore/SecureChannel/SecureChannelClient.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2018 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -98,6 +98,7 @@ namespace OpcUaStackCore
 		SecureChannelClientConfig::SPtr config;
 		config = boost::static_pointer_cast<SecureChannelClientConfig>(secureChannel->config_);
 
+		secureChannel->isLogging_ = config->secureChannelLog();
 		secureChannel->receivedBufferSize_ = config->receivedBufferSize();
 		secureChannel->sendBufferSize_ = config->sendBufferSize();
 		secureChannel->maxMessageSize_ = config->maxMessageSize();
