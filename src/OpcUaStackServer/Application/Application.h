1503d6fc5b051f98b9f6075daae191b95da3e5b4
Kai Huebl <kai@huebl-sgh.de>
2018-07-14 22:50:27 +0200

    added getter function

--- src/OpcUaStackServer/Application/Application.h
+++ src/OpcUaStackServer/Application/Application.h
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2018 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -49,6 +49,7 @@ namespace OpcUaStackServer
 		~Application(void);
 
 		void applicationIf(ApplicationIf* applicationIf);
+		ApplicationIf* applicationIf(void);
 		void reloadIf(ReloadIf* reloadIf);
 		void applicationName(const std::string& applicationName);
 		void serviceComponent(Component* serviceComponent);
