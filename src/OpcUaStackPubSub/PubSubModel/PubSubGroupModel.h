cc2eeb2e640ce9801f498076f5d7c52a0768b03f
Kai Huebl <kai@huebl-sgh.de>
2018-08-16 12:13:15 +0200

    added new class PubSubGroupModel

---
+++ src/OpcUaStackPubSub/PubSubModel/PubSubGroupModel.h
@@ -0,0 +1,47 @@
+/*
+   Copyright 2018 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackBroker_PubSubGroupModel_h__
+#define __OpcUaStackBroker_PubSubGroupModel_h__
+
+#include <boost/shared_ptr.hpp>
+#include <map>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackPubSub
+{
+
+	class DLLEXPORT PubSubGroupModel
+	{
+	  public:
+		typedef boost::shared_ptr<PubSubGroupModel> SPtr;
+		typedef std::map<OpcUaNodeId, PubSubGroupModel::SPtr> Map;
+
+		PubSubGroupModel(void);
+		virtual ~PubSubGroupModel(void);
+
+	  private:
+
+	};
+
+}
+
+#endif
+
