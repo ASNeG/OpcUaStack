2de3ca27a62bd77dbafe2bf0a78bc35d05117828
Kai Huebl <kai@huebl-sgh.de>
2018-10-04 13:11:48 +0200

    added new class PubSubWriterGroupModel

---
+++ src/OpcUaStackPubSub/PubSubModel/PubSubWriterGroupModel.h
@@ -0,0 +1,58 @@
+/*
+   Copyright 2018 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackPubSub_PubSubWriterGroupModel_h__
+#define __OpcUaStackPubSub_PubSubWriterGroupModel_h__
+
+#include <boost/shared_ptr.hpp>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+
+#include "OpcUaStackPubSub/PubSubModel/PubSubState.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackPubSub
+{
+
+	/**
+	 * This class is used to represent the configuration parameters for WriterGroups.
+	 */
+	class DLLEXPORT PubSubWriterGroupModel
+	: public PubSubState
+	{
+	  public:
+		typedef boost::shared_ptr<PubSubWriterGroupModel> SPtr;
+
+		/**
+		 * constructor
+		 */
+		PubSubWriterGroupModel(void);
+
+		/**
+		 * destructor
+		 */
+		virtual ~PubSubWriterGroupModel(void);
+
+
+	  private:
+
+	};
+
+}
+
+#endif
