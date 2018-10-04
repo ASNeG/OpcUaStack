17910463cee9d9bba0ae557c05cf61e3df847b11
Kai Huebl <kai@huebl-sgh.de>
2018-10-04 12:49:10 +0200

    implemented class PubSubState

--- src/OpcUaStackPubSub/PubSubModel/PubSubState.cpp
+++ src/OpcUaStackPubSub/PubSubModel/PubSubState.cpp
@@ -21,11 +21,163 @@ namespace OpcUaStackPubSub
 {
 
 	PubSubState::PubSubState(void)
+	: state_(Disabled)
 	{
 	}
 
 	PubSubState::~PubSubState(void)
 	{
+	}
+
+	PubSubState::State
+	PubSubState::state(void)
+	{
+		return state_;
+	}
+
+	void
+	PubSubState::parent(
+		const PubSubState::SPtr& parent
+	)
+	{
+		// FIXME: todo
+	}
+
+	PubSubState::State
+	PubSubState::enable(void)
+	{
+		switch (state_)
+		{
+			case Disabled:
+				if (parentState() != Operational) {
+					state_ = Paused;
+				}
+				else {
+					state_ = Operational;
+				}
+				break;
+			case Paused:
+				break;
+			case Operational:
+				break;
+			case Error:
+				break;
+			default:
+				break;
+		}
+		return state_;
+	}
+
+	PubSubState::State
+	PubSubState::disable(void)
+	{
+		switch (state_)
+		{
+			case Disabled:
+				break;
+			case Paused:
+				state_ = Disabled;
+				break;
+			case Operational:
+				state_ = Disabled;
+				break;
+			case Error:
+				state_ = Disabled;
+				break;
+			default:
+				break;
+		}
+		return state_;
+	}
+
+	PubSubState::State
+	PubSubState::errorOn(void)
+	{
+		switch (state_)
+		{
+			case Disabled:
+				break;
+			case Paused:
+				break;
+			case Operational:
+				state_ = Error;
+				break;
+			case Error:
+				break;
+			default:
+				break;
+		}
+		return state_;
+	}
+
+	PubSubState::State
+	PubSubState::errorOff(void)
+	{
+		switch (state_)
+		{
+			case Disabled:
+				break;
+			case Paused:
+				break;
+			case Operational:
+				break;
+			case Error:
+				state_ = Operational;
+				break;
+			default:
+				break;
+		}
+		return state_;
+	}
+
+	PubSubState::State
+	PubSubState::parentChangedToDisableOrPaused(void)
+	{
+		switch (state_)
+		{
+			case Disabled:
+				break;
+			case Paused:
+				break;
+			case Operational:
+				state_ = Paused;
+				break;
+			case Error:
+				state_ = Paused;
+				break;
+			default:
+				break;
+		}
+		return state_;
+	}
+
+	PubSubState::State
+	PubSubState::parentChangedToOperational(void)
+	{
+		switch (state_)
+		{
+			case Disabled:
+				break;
+			case Paused:
+				state_ = Operational;
+				break;
+			case Operational:
+				break;
+			case Error:
+				break;
+			default:
+				break;
+		}
+		return state_;
+	}
+
+	PubSubState::State
+	PubSubState::parentState(void)
+	{
+		if (!parent_) {
+			return Operational;
+		}
+		return parent_->state();
 	}
 
 }
--- src/OpcUaStackPubSub/PubSubModel/PubSubState.h
+++ src/OpcUaStackPubSub/PubSubModel/PubSubState.h
@@ -25,12 +25,21 @@ namespace OpcUaStackPubSub
 {
 
 	/**
-	 * This class is used to represent the configuration parameters for PubSubConnections.
+	 * This class is used to expose and control the operation of a PubSub component.
 	 */
 	class DLLEXPORT PubSubState
 	{
 	  public:
 		typedef boost::shared_ptr<PubSubState> SPtr;
+
+		typedef enum
+		{
+			Disabled,		//!< The PubSub component is configured but currently disabled.
+			Paused,			//!< The PubSub component is enabled but currently paused by a parent component. The
+							//!< parent component is either Disabled or Paused.
+			Operational,	//!< The PubSub component is operational.
+			Error			//!< The PubSub component is in an error state.
+		} State;
 
 		/**
 		 * constructor
@@ -41,6 +50,73 @@ namespace OpcUaStackPubSub
 		 * destructor
 		 */
 		virtual ~PubSubState(void);
+
+		/**
+		 * This method returns the state
+		 *
+		 * @return state
+		 */
+		State state(void);
+
+		/**
+		 * This method is used to set the pointer to the parent class
+		 *
+		 * @param[in] parent	 	pointer to parent class
+		 */
+		void parent(
+			const PubSubState::SPtr& parent
+		);
+
+		/**
+		 * This method indicates that the component will be enable
+		 *
+		 * @return status of component
+		 */
+		State enable(void);
+
+		/**
+		 * This method indicates that the component will be disable
+		 *
+		 * @return status of component
+		 */
+		State disable(void);
+
+		/**
+		 * This method indicates that the component will be in error state
+		 *
+		 * @return status of component
+		 */
+		State errorOn(void);
+
+		/**
+		 * This method indicates that the component will be leave the error state
+		 *
+		 * @return status of component
+		 */
+		State errorOff(void);
+
+		/**
+		 * This method indicates that the parent component will be changed to
+		 * disable or paused state
+		 *
+		 * @return status of component
+		 */
+		State parentChangedToDisableOrPaused(void);
+
+		/**
+		 * This method indicates that the parent component will be changed to
+		 * operational or paused state
+		 *
+		 * @return status of component
+		 */
+		State parentChangedToOperational(void);
+
+
+	  private:
+		State parentState(void);
+
+		State state_;				//!< state of the publish subscribe component
+		PubSubState::SPtr parent_;	//!< pointer to parent class
 
 	};
 
