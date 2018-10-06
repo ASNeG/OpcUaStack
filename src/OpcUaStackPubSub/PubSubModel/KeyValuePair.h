d8c472828598118c0c188a421718d81b449027f6
Kai Huebl <kai@huebl-sgh.de>
2018-10-06 09:47:07 +0200

    added new class KeyValuePair

---
+++ src/OpcUaStackPubSub/PubSubModel/KeyValuePair.h
@@ -0,0 +1,52 @@
+/*
+   Copyright 2018 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackPubSub_KeyValuePair_h__
+#define __OpcUaStackPubSub_KeyValuePair_h__
+
+#include <boost/shared_ptr.hpp>
+
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackPubSub
+{
+
+	/**
+	 * This class represents a key value pair
+	 */
+	class DLLEXPORT KeyValuePair
+	{
+	  public:
+		typedef boost::shared_ptr<KeyValuePair> SPtr;
+
+		/**
+		 * constructor
+		 */
+		KeyValuePair(void);
+
+		/**
+		 * destructor
+		 */
+		virtual ~KeyValuePair(void);
+	};
+
+}
+
+#endif
