36250b6674031bd5ffb1e3c728f1a984e5955a42
Kai Huebl <kai@huebl-sgh.de>
2018-10-25 12:45:53 +0200

    added new test case file to check application certificate

---
+++ tst/OpcUaStackCore/Certificate/ApplicationCertificate_t.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "unittest.h"
+#include "OpcUaStackCore/Certificate/ApplicationCertificate.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(ApplicationCertificate_)
+
+BOOST_AUTO_TEST_CASE(ApplicationCertificate_)
+{
+	std::cout << "ApplicationCertificate_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
