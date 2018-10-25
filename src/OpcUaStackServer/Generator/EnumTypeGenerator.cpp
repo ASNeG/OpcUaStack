947af8f6c42cac432d623bf72de5f58e4c999af5
Kai Huebl <kai@huebl-sgh.de>
2018-10-25 09:29:28 +0200

    added copy constructor

--- src/OpcUaStackServer/Generator/EnumTypeGenerator.cpp
+++ src/OpcUaStackServer/Generator/EnumTypeGenerator.cpp
@@ -225,6 +225,7 @@ namespace OpcUaStackServer
 		ss << prefix << std::endl;
 		ss << prefix << "    " << nodeInfo_.className() << "(void);" << std::endl;
 		ss << prefix << "    " << nodeInfo_.className() << "(Enum enumeration);" << std::endl;
+		ss << prefix << "    " << nodeInfo_.className() << "::" << nodeInfo_.className() << "(const " << nodeInfo_.className() << "& value);" << std::endl;
 		ss << prefix << "    virtual ~" << nodeInfo_.className() << "(void);" << std::endl;
 
 		headerContent_ += ss.str();
@@ -462,6 +463,14 @@ namespace OpcUaStackServer
 		ss << prefix << ": Object()" << std::endl;
 		ss << prefix << ", ExtensionObjectBase()" << std::endl;
 		ss << prefix << ", value_(enumeration)" << std::endl;
+		ss << prefix << "{" << std::endl;
+		ss << prefix << "}" << std::endl;
+
+		ss << prefix << std::endl;
+		ss << prefix << nodeInfo_.className() << "::" << nodeInfo_.className() << "(const " << nodeInfo_.className() << "& value)" << std::endl;
+		ss << prefix << ": Object()" << std::endl;
+		ss << prefix << ", ExtensionObjectBase()" << std::endl;
+		ss << prefix << ", value_(value.enumeration())" << std::endl;
 		ss << prefix << "{" << std::endl;
 		ss << prefix << "}" << std::endl;
 
