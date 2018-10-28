8fe8cf57b4d9c975338ed2fb7658ac0175fb8715
Kai Huebl <kai@huebl-sgh.de>
2018-10-28 13:56:41 +0100

    added EnumTypes_t to check generated enumeration types

---
+++ tst/OpcUaStackCore/StandardDataTypes/EnumTypes_t.cpp
@@ -0,0 +1,18 @@
+#include "unittest.h"
+#include "OpcUaStackCore/Core/Core.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaIdentifier.h"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaExtensionObject.h"
+#include "OpcUaStackCore/StandardDataTypes/NodeClass.h"
+
+using namespace OpcUaStackCore;
+
+BOOST_AUTO_TEST_SUITE(EnumTypes_)
+
+BOOST_AUTO_TEST_CASE(EnumTypes_)
+{
+	std::cout << "EnumTypes_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
