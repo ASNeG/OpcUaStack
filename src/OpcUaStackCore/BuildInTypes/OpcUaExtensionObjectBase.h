423f64196de7cec68e4d05f423eede1211e852e3
Kai Huebl <kai@huebl-sgh.de>
2018-10-29 20:45:51 +0100

    rework class ExtensionObjectBase

--- src/OpcUaStackCore/BuildInTypes/OpcUaExtensionObjectBase.h
+++ src/OpcUaStackCore/BuildInTypes/OpcUaExtensionObjectBase.h
@@ -19,52 +19,162 @@
 #define __OpcUaStackCore_ExtensionObjectBase_h__
 
 #include <boost/property_tree/ptree.hpp>
+
+#include <map>
+
 #include "OpcUaStackCore/BuildInTypes/Xmlns.h"
 #include "OpcUaStackCore/BuildInTypes/OpcUaNodeId.h"
 
 namespace OpcUaStackCore
 {
 
+	/**
+	 * This class is the base class for all structure classes
+	 */
     class DLLEXPORT ExtensionObjectBase
 	{
 	  public:
 		typedef boost::shared_ptr<ExtensionObjectBase> SPtr;
+		typedef std::map<OpcUaNodeId, ExtensionObjectBase::SPtr> Map;
 
-		ExtensionObjectBase(void) {}
-		virtual ~ExtensionObjectBase(void) {}
+		/**
+		 * Constructor
+		 */
+		ExtensionObjectBase(void);
 
+		/**
+		 * Destructor
+		 */
+		virtual ~ExtensionObjectBase(void);
+
+		/**
+		 * Creates a structure class and resturns a pointer to this class.
+		 *
+		 * @return pointer to the structure class
+		 */
 		virtual SPtr factory(void) = 0;
-		virtual void opcUaBinaryEncode(std::ostream& os) const = 0;
-		virtual void opcUaBinaryDecode(std::istream& is) = 0;
-		virtual bool encode(boost::property_tree::ptree& pt, Xmlns& xmlns) const { return false; }
-		virtual bool decode(boost::property_tree::ptree& pt, Xmlns& xmlns) { return false; }
 
+		/**
+		 * Function to encode the structure data to the binary format.
+		 *
+		 * @param[out] os					output stream for binary output
+		 */
+		virtual void opcUaBinaryEncode(std::ostream& os) const = 0;
+
+		/**
+		 * Function to decode the binary format to the structure data
+		 *
+		 * @param[in] is					input stream for binary input
+		 */
+		virtual void opcUaBinaryDecode(std::istream& is) = 0;
+
+		/**
+		 * Function to encode the structure data to a xml tree format.
+		 *
+		 * @param[out] pt					tree for xml output
+		 * @param[in] element				element name of the subtree
+		 * @param[in] xmlns					xml namespaces
+		 *
+		 * @return true if successful
+		 */
 		virtual bool xmlEncode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns) = 0;
+
+		/**
+		 * Function to encode the structure data to a xml tree format.
+		 *
+		 * @param[out] pt					tree for xml output
+		 * @param[in] element				element name of the subtree
+		 *
+		 * @return true if successful
+		 */
 		virtual bool xmlEncode(boost::property_tree::ptree& pt, Xmlns& xmlns) = 0;
+
+		/**
+		 * Function to decode the xml tree format to the structure data.
+		 *
+		 * @param[in] pt					tree for xml output
+		 * @param[in] element				element name of the subtree
+		 * @param[in] xmlns					xml namespaces
+		 *
+		 * @return true if successful
+		 */
 		virtual bool xmlDecode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns) = 0;
+
+		/**
+		 * Function to decode the xml tree format to the structure data.
+		 *
+		 * @param[in] pt					tree for xml output
+		 * @param[in] element				element name of the subtree
+		 *
+		 * @return true if successful
+		 */
 		virtual bool xmlDecode(boost::property_tree::ptree& pt, Xmlns& xmlns) = 0;
 
+		/**
+		 * Function to create a copy of the extension object
+		 *
+		 * @param[out] extensionObjectBase	target extension object
+		 *
+		 */
 		virtual void copyTo(ExtensionObjectBase& extensionObjectBase) = 0;
+
+		/**
+		 * Function to compare two extension objects
+		 *
+		 * @param[in] extensionObjectBase	extension object
+		 *
+		 * @return true, if both extension objects are equal
+		 */
 		virtual bool equal(ExtensionObjectBase& extensionObjectBase) const = 0;
+
+		/**
+		 * Funktion to show the parameters of the extension object
+		 *
+		 * @param[out] os					output stream
+		 */
 		virtual void out(std::ostream& os) = 0;
+
+		/**
+		 * Function to result the binary encoding node identifier of the extension object
+		 *
+		 * @return binary encoding node identifier of the extension object
+		 */
 		virtual OpcUaNodeId binaryTypeId(void) { return OpcUaNodeId(0, 0); }
+
+		/**
+		 * Function to result the xml encoding node identifier of the extension object
+		 *
+		 * @return xml encoding node identifier of the extension object
+		 */
 		virtual OpcUaNodeId xmlTypeId(void) { return OpcUaNodeId(0, 0); }
 
-        bool operator==(const ExtensionObjectBase& extensionObjectBase) const
-		{
-        	return this->equal(*const_cast<ExtensionObjectBase*>(&extensionObjectBase));
-		}
+		/**
+		 * Function to compare two extension objects
+		 *
+		 * @param[in] extensionObjectBase	extension object
+		 *
+		 * @return true, if both extension objects are equal
+		 */
+        bool operator==(const ExtensionObjectBase& extensionObjectBase) const;
 
-        bool operator!=(const ExtensionObjectBase& extensionObjectBase) const
-		{
-        	return !this->operator==(extensionObjectBase);
-		}
+		/**
+		 * Function to compare two extension objects
+		 *
+		 * @param[in] extensionObjectBase	extension object
+		 *
+		 * @return true, if both extension objects are not equal
+		 */
+        bool operator!=(const ExtensionObjectBase& extensionObjectBase) const;
 
-        ExtensionObjectBase& operator=(const ExtensionObjectBase& extensionObjectBase)
-        {
-        	const_cast<ExtensionObjectBase*>(&extensionObjectBase)->copyTo(*this);
-        	return *this;
-        }
+		/**
+		 * Function to create a copy of the extension object
+		 *
+		 * @param[in] extensionObjectBase	target extension object
+		 *
+		 * @return copy of the extension object
+		 */
+        ExtensionObjectBase& operator=(const ExtensionObjectBase& extensionObjectBase);
+
 	};
 
 }
