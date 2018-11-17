f9e7448b9b80b4dc9e1488e999f243345418f082
Kai Huebl <kai@huebl-sgh.de>
2018-11-17 01:24:57 +0100

    added generated class DataSetReaderMessageDataType

--- src/OpcUaStackCore/StandardDataTypes/build.sh
+++ src/OpcUaStackCore/StandardDataTypes/build.sh
@@ -31,6 +31,7 @@
 #OpcUaDataTypeGenerator4 --nodeset Opc.Ua.NodeSet2.xml --nodeset Opc.Ua.NodeSet2.Part14.xml --datatype EndpointDescription --buildSubTypes 0
 #OpcUaDataTypeGenerator4 --nodeset Opc.Ua.NodeSet2.xml --nodeset Opc.Ua.NodeSet2.Part14.xml --datatype UserTokenPolicy --buildSubTypes 0
 
+OpcUaDataTypeGenerator4 --nodeset Opc.Ua.NodeSet2.xml --nodeset Opc.Ua.NodeSet2.Part14.xml --datatype DataSetReaderMessageDataType --buildSubTypes 0
 
 
 
