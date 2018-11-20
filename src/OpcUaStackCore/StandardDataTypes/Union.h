573bca8fdbd7b668a52b1bac06f93234bed8cda2
Kai Huebl <kai@huebl-sgh.de>
2018-11-20 17:06:49 +0100

    added generated data type Union

---
+++ src/OpcUaStackCore/StandardDataTypes/Union.h
@@ -0,0 +1,79 @@
+/*
+    DataTypeClass: Union
+
+    Generated Source Code - please do not change this source code
+
+    DataTypeCodeGenerator Version:
+        OpcUaStackCore - 4.1.0
+
+    Autor:     Kai Huebl (kai@huebl-sgh.de)
+*/
+
+#ifndef __OpcUaStackCore_Union_h__
+#define __OpcUaStackCore_Union_h__
+
+#include <boost/shared_ptr.hpp>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/Base/ObjectPool.h"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+
+namespace OpcUaStackCore
+{
+    
+    class DLLEXPORT Union
+    : public Object
+    , public ExtensionObjectBase
+    {
+      public:
+        typedef boost::shared_ptr<Union> SPtr;
+        typedef std::vector<Union::SPtr> Vec;
+    
+        Union(void);
+        virtual ~Union(void);
+        
+        
+        //- ExtensionObjectBase -----------------------------------------------
+        virtual ExtensionObjectBase::SPtr factory(void);
+        virtual std::string namespaceName(void);
+        virtual std::string typeName(void);
+        virtual OpcUaNodeId typeId(void);
+        virtual OpcUaNodeId binaryTypeId(void);
+        virtual OpcUaNodeId xmlTypeId(void);
+        virtual OpcUaNodeId jsonTypeId(void);
+        virtual void opcUaBinaryEncode(std::ostream& os) const;
+        virtual void opcUaBinaryDecode(std::istream& is);
+        virtual bool encode(boost::property_tree::ptree& pt, Xmlns& xmlns) const;
+        virtual bool decode(boost::property_tree::ptree& pt, Xmlns& xmlns);
+        virtual bool xmlEncode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns);
+        virtual bool xmlEncode(boost::property_tree::ptree& pt, Xmlns& xmlns);
+        virtual bool xmlDecode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns);
+        virtual bool xmlDecode(boost::property_tree::ptree& pt, Xmlns& xmlns);
+        virtual bool jsonEncode(boost::property_tree::ptree& pt, const std::string& element);
+        virtual bool jsonEncode(boost::property_tree::ptree& pt);
+        virtual bool jsonDecode(boost::property_tree::ptree& pt, const std::string& element);
+        virtual bool jsonDecode(boost::property_tree::ptree& pt);
+        virtual void copyTo(ExtensionObjectBase& extensionObjectBase);
+        virtual bool equal(ExtensionObjectBase& extensionObjectBase) const;
+        virtual void out(std::ostream& os);
+        //- ExtensionObjectBase -----------------------------------------------
+        
+        void copyTo(Union& value);
+        bool operator==(const Union& value);
+        bool operator!=(const Union& value);
+        Union& operator=(const Union& value);
+    
+      private:
+    
+    };
+    
+    class UnionArray
+    : public OpcUaArray<Union::SPtr, SPtrTypeCoder<Union> >
+    , public Object
+    {
+      public:
+    	   typedef boost::shared_ptr<UnionArray> SPtr;
+    };
+
+}
+
+#endif
