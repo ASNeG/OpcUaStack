ee62b26bbd9b04a01c63526edd32b30471930f7d
Kai Huebl <kai@huebl-sgh.de>
2018-11-24 10:27:52 +0100

    added new generated data types

---
+++ src/OpcUaStackCore/StandardDataTypes/NotificationData.cpp
@@ -0,0 +1,196 @@
+/*
+    DataTypeClass: NotificationData
+
+    Generated Source Code - please do not change this source code
+
+    DataTypeCodeGenerator Version:
+        OpcUaStackCore - 4.1.0
+
+    Autor: Kai Huebl (kai@huebl-sgh.de)
+*/
+
+#include "OpcUaStackCore/StandardDataTypes/NotificationData.h"
+
+namespace OpcUaStackCore
+{
+    
+    NotificationData::NotificationData(void)
+    : Object()
+    , ExtensionObjectBase()
+    {
+    }
+    
+    NotificationData::~NotificationData(void)
+    {
+    }
+    
+    bool
+    NotificationData::operator==(const NotificationData& value)
+    {
+        NotificationData* dst = const_cast<NotificationData*>(&value);
+        return true;
+    }
+    
+    bool
+    NotificationData::operator!=(const NotificationData& value)
+    {
+        return !this->operator==(value);
+    }
+    
+    void
+    NotificationData::copyTo(NotificationData& value)
+    {
+    }
+    
+    NotificationData&
+    NotificationData::operator=(const NotificationData& value)
+    {
+        const_cast<NotificationData*>(&value)->copyTo(*this);
+        return *this;
+    }
+    
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    //
+    // ExtensionObjectBase
+    //
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    
+    ExtensionObjectBase::SPtr
+    NotificationData::factory(void)
+    {
+    	return constructSPtr<NotificationData>();
+    }
+    
+    std::string
+    NotificationData::namespaceName(void)
+    {
+    	return "http://opcfoundation.org/UA/";
+    }
+    
+    std::string
+    NotificationData::typeName(void)
+    {
+    	return "NotificationData";
+    }
+    
+    OpcUaNodeId
+    NotificationData::typeId(void)
+    {
+    	return OpcUaNodeId((OpcUaUInt32)945,0);
+    }
+    
+    OpcUaNodeId
+    NotificationData::binaryTypeId(void)
+    {
+    	return OpcUaNodeId((OpcUaUInt32)947, 0);
+    }
+    
+    OpcUaNodeId
+    NotificationData::xmlTypeId(void)
+    {
+    	return OpcUaNodeId((OpcUaUInt32)946, 0);
+    }
+    
+    OpcUaNodeId
+    NotificationData::jsonTypeId(void)
+    {
+    	return OpcUaNodeId((OpcUaUInt32)15344, 0);
+    }
+    
+    void
+    NotificationData::opcUaBinaryEncode(std::ostream& os) const
+    {
+    }
+    
+    void
+    NotificationData::opcUaBinaryDecode(std::istream& is)
+    {
+    }
+    
+    bool
+    NotificationData::encode(boost::property_tree::ptree& pt, Xmlns& xmlns) const
+    {
+    }
+    
+    bool
+    NotificationData::decode(boost::property_tree::ptree& pt, Xmlns& xmlns)
+    {
+    }
+    
+    bool
+    NotificationData::xmlEncode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns)
+    {
+        boost::property_tree::ptree elementTree;
+        if (!xmlEncode(elementTree, xmlns)) return false;
+        pt.push_back(std::make_pair(element, elementTree));
+        return true;
+    }
+    
+    bool
+    NotificationData::xmlEncode(boost::property_tree::ptree& pt, Xmlns& xmlns)
+    {
+        boost::property_tree::ptree elementTree;
+    
+        return true;
+    }
+    
+    bool
+    NotificationData::xmlDecode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns)
+    {
+        boost::optional<boost::property_tree::ptree&> tree = pt.get_child_optional(element);
+        if (!tree) return false;
+        return xmlDecode(*tree, xmlns);
+    }
+    
+    bool
+    NotificationData::xmlDecode(boost::property_tree::ptree& pt, Xmlns& xmlns)
+    {
+        boost::optional<boost::property_tree::ptree&> tree;
+    
+        return true;
+    }
+    
+    bool
+    NotificationData::jsonEncode(boost::property_tree::ptree& pt, const std::string& element)
+    {
+        return true;
+    }
+    
+    bool
+    NotificationData::jsonEncode(boost::property_tree::ptree& pt)
+    {
+        return true;
+    }
+    
+    bool
+    NotificationData::jsonDecode(boost::property_tree::ptree& pt, const std::string& element)
+    {
+    }
+    
+    bool
+    NotificationData::jsonDecode(boost::property_tree::ptree& pt)
+    {
+    }
+    
+    void
+    NotificationData::copyTo(ExtensionObjectBase& extensionObjectBase)
+    {
+    	NotificationData* dst = dynamic_cast<NotificationData*>(&extensionObjectBase);
+    	copyTo(*dst);
+    }
+    
+    bool
+    NotificationData::equal(ExtensionObjectBase& extensionObjectBase) const
+    {
+    	NotificationData* dst = dynamic_cast<NotificationData*>(&extensionObjectBase);
+    	return *const_cast<NotificationData*>(this) == *dst;
+    }
+    
+    void
+    NotificationData::out(std::ostream& os)
+    {
+    }
+
+}
---
+++ src/OpcUaStackCore/StandardDataTypes/StatusChangeNotification.h
@@ -0,0 +1,83 @@
+/*
+    DataTypeClass: StatusChangeNotification
+
+    Generated Source Code - please do not change this source code
+
+    DataTypeCodeGenerator Version:
+        OpcUaStackCore - 4.1.0
+
+    Autor:     Kai Huebl (kai@huebl-sgh.de)
+*/
+
+#ifndef __OpcUaStackCore_StatusChangeNotification_h__
+#define __OpcUaStackCore_StatusChangeNotification_h__
+
+#include <boost/shared_ptr.hpp>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/Base/ObjectPool.h"
+#include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
+#include "OpcUaStackCore/StandardDataTypes/NotificationData.h"
+
+namespace OpcUaStackCore
+{
+    
+    class DLLEXPORT StatusChangeNotification
+    : public NotificationData
+    {
+      public:
+        typedef boost::shared_ptr<StatusChangeNotification> SPtr;
+        typedef std::vector<StatusChangeNotification::SPtr> Vec;
+    
+        StatusChangeNotification(void);
+        virtual ~StatusChangeNotification(void);
+        
+        OpcUaStatus& status(void);
+        OpcUaDiagnosticInfo& diagnosticInfo(void);
+        
+        //- ExtensionObjectBase -----------------------------------------------
+        virtual ExtensionObjectBase::SPtr factory(void);
+        virtual std::string namespaceName(void);
+        virtual std::string typeName(void);
+        virtual OpcUaNodeId typeId(void);
+        virtual OpcUaNodeId binaryTypeId(void);
+        virtual OpcUaNodeId xmlTypeId(void);
+        virtual OpcUaNodeId jsonTypeId(void);
+        virtual void opcUaBinaryEncode(std::ostream& os) const;
+        virtual void opcUaBinaryDecode(std::istream& is);
+        virtual bool encode(boost::property_tree::ptree& pt, Xmlns& xmlns) const;
+        virtual bool decode(boost::property_tree::ptree& pt, Xmlns& xmlns);
+        virtual bool xmlEncode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns);
+        virtual bool xmlEncode(boost::property_tree::ptree& pt, Xmlns& xmlns);
+        virtual bool xmlDecode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns);
+        virtual bool xmlDecode(boost::property_tree::ptree& pt, Xmlns& xmlns);
+        virtual bool jsonEncode(boost::property_tree::ptree& pt, const std::string& element);
+        virtual bool jsonEncode(boost::property_tree::ptree& pt);
+        virtual bool jsonDecode(boost::property_tree::ptree& pt, const std::string& element);
+        virtual bool jsonDecode(boost::property_tree::ptree& pt);
+        virtual void copyTo(ExtensionObjectBase& extensionObjectBase);
+        virtual bool equal(ExtensionObjectBase& extensionObjectBase) const;
+        virtual void out(std::ostream& os);
+        //- ExtensionObjectBase -----------------------------------------------
+        
+        void copyTo(StatusChangeNotification& value);
+        bool operator==(const StatusChangeNotification& value);
+        bool operator!=(const StatusChangeNotification& value);
+        StatusChangeNotification& operator=(const StatusChangeNotification& value);
+    
+      private:
+        OpcUaStatus status_;
+        OpcUaDiagnosticInfo diagnosticInfo_;
+    
+    };
+    
+    class StatusChangeNotificationArray
+    : public OpcUaArray<StatusChangeNotification::SPtr, SPtrTypeCoder<StatusChangeNotification> >
+    , public Object
+    {
+      public:
+    	   typedef boost::shared_ptr<StatusChangeNotificationArray> SPtr;
+    };
+
+}
+
+#endif
