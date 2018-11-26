7121f5da0850d8e0d3b8c8ccfda42a5962728c2f
Kai Huebl <kai@huebl-sgh.de>
2018-11-26 17:25:10 +0100

    remove unused test files

---
+++ tst/OpcUaStackServer/InformationModel/NodesetXml_t.cpp
@@ -0,0 +1,26 @@
+#include "unittest.h"
+
+#include "OpcUaStackCore/Base/ConfigXml.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaIdentifier.h"
+#include "OpcUaStackServer/AddressSpaceModel/BaseNodeClass.h"
+#include "OpcUaStackServer/AddressSpaceModel/ObjectTypeNodeClass.h"
+#include "OpcUaStackServer/AddressSpaceModel/ObjectNodeClass.h"
+#include "OpcUaStackServer/AddressSpaceModel/VariableTypeNodeClass.h"
+#include "OpcUaStackServer/AddressSpaceModel/VariableNodeClass.h"
+#include "OpcUaStackServer/InformationModel/InformationModelAccess.h"
+#include "OpcUaStackServer/InformationModel/InformationModelNodeSet.h"
+#include "OpcUaStackServer/NodeSet/NodeSetXmlParser.h"
+
+using namespace OpcUaStackCore;
+using namespace OpcUaStackServer;
+
+BOOST_AUTO_TEST_SUITE(NodesetXml_)
+
+BOOST_AUTO_TEST_CASE(NodesetXml_)
+{
+	std::cout << "NodesetXml_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
