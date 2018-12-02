46c2b51c73c3cf4a74a89277d1d3a90dcf9e8d00
Kai Huebl <kai@huebl-sgh.de>
2018-12-02 17:35:25 +0100

    added new class XmlnsGuard

---
+++ src/OpcUaStackCore/BuildInTypes/XmlnsGuard.h
@@ -0,0 +1,46 @@
+/*
+   Copyright 2018 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackCore_XmlnsGuard_h__
+#define __OpcUaStackCore_XmlnsGuard_h__
+
+#include <iostream>
+#include <map>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/BuildInTypes/Xmlns.h"
+
+namespace OpcUaStackCore
+{
+
+	class DLLEXPORT XmlnsGuard
+	{
+	  public:
+		/**
+		 * Encoder
+		 */
+		XmlnsGuard(void);
+
+		/**
+		 * Decoder
+		 */
+		~XmlnsGuard(void);
+
+	  private:
+	};
+}
+
+#endif
