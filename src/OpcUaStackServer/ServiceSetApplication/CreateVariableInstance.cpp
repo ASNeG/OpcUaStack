854500517f0a4005963e266ed07aaf0787ddffa1
Kai Huebl <kai@huebl-sgh.de>
2019-01-03 12:49:49 +0100

    added new class CreateVariableInstance

---
+++ src/OpcUaStackServer/ServiceSetApplication/CreateVariableInstance.cpp
@@ -0,0 +1,57 @@
+/*
+   Copyright 2019 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/ServiceSetApplication/ApplicationServiceTransaction.h"
+#include "OpcUaStackServer/ServiceSetApplication/CreateVariableInstance.h"
+#include "OpcUaStackServer/ServiceSetApplication/NodeReferenceApplication.h"
+
+namespace OpcUaStackServer
+{
+
+	CreateVariableInstance::CreateVariableInstance(void)
+	: resultCode_(Success)
+	{
+	}
+
+	CreateVariableInstance::~CreateVariableInstance(void)
+	{
+	}
+
+	bool
+	CreateVariableInstance::query(ApplicationServiceIf* applicationServiceIf)
+	{
+		resultCode_ = Success;
+
+		// create response
+		auto trx = constructSPtr<ServiceTransactionCreateVariable>();
+
+		// send query to application service
+		applicationServiceIf->sendSync(trx);
+		resultCode_ = trx->statusCode();
+	  	if (resultCode_ != Success) {
+	  		return false;
+	  	}
+		return true;
+	}
+
+	OpcUaStatusCode
+	CreateVariableInstance::resultCode(void)
+	{
+		return resultCode_;
+	}
+
+}
---
+++ src/OpcUaStackServer/ServiceSetApplication/CreateVariableInstance.h
@@ -0,0 +1,48 @@
+/*
+   Copyright 2019 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackServer_CreateVariableInstance_h__
+#define __OpcUaStackServer_CreateVariableInstance_h__
+
+#include <vector>
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaNodeId.h"
+#include "OpcUaStackCore/StandardDataTypes/NodeClass.h"
+#include "OpcUaStackServer/Application/ApplicationIf.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackServer
+{
+
+	class DLLEXPORT CreateVariableInstance
+	{
+	  public:
+		typedef boost::shared_ptr<CreateVariableInstance> SPtr;
+
+		CreateVariableInstance(void);
+		virtual ~CreateVariableInstance(void);
+
+		bool query(ApplicationServiceIf* applicationServiceIf);
+		OpcUaStatusCode resultCode(void);
+
+	  private:
+		OpcUaStatusCode resultCode_;
+	};
+
+}
+#endif
