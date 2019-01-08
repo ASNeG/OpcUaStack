e9e8bb01b3a1cde67cc252ed63674cdd4cd7101c
Kai Huebl <kai@huebl-sgh.de>
2019-01-08 20:40:17 +0100

    rework build script

--- src/OpcUaStackServer/StandardObjectType/build.sh
+++ src/OpcUaStackServer/StandardObjectType/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-OpcUaVariableTypeGenerator4 --nodeset Opc.Ua.NodeSet.xml --variabletype BaseVariableType --buildSubTypes 1
+OpcUaObjectTypeGenerator4 --nodeset Opc.Ua.NodeSet.xml --variabletype BaseObjectType --buildSubTypes 1
