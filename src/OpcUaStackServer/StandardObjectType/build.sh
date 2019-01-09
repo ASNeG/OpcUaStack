d39a782b4b3610f39f2959d334f2e9eb0758cdad
Kai Huebl <kai@huebl-sgh.de>
2019-01-09 08:43:52 +0100

    integrated additional nodesets

--- src/OpcUaStackServer/StandardObjectType/build.sh
+++ src/OpcUaStackServer/StandardObjectType/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-OpcUaObjectTypeGenerator4 --nodeset Opc.Ua.NodeSet.xml --objecttype BaseObjectType --buildSubTypes 1
+NODESETS="--nodeset Opc.Ua.NodeSet2.xml --nodeset Opc.Ua.NodeSet2.Part14.xml --nodeset Opc.Ua.NodeSet2.Services.xml"
+
+OpcUaObjectTypeGenerator4 ${NODESETS} --objecttype BaseObjectType --buildSubTypes 1
