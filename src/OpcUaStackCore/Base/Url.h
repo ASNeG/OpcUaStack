c4f91cff5eaaef41fe95692b4d8e9dec39cdf518
Kai Huebl <kai@huebl-sgh.de>
2019-03-06 22:38:43 +0100

    Fix bug in Url class.

--- src/OpcUaStackCore/Base/Url.h
+++ src/OpcUaStackCore/Base/Url.h
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015-2017 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2019 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -15,7 +15,7 @@
    Autor: Kai Huebl (kai@huebl-sgh.de)
  */
 
-#ifndef __OpcuaStackCore_Url_h__
+#ifndef __OpcUaStackCore_Url_h__
 #define __OpcUaStackCore_Url_h__
 
 #include "OpcUaStackCore/Base/os.h"
