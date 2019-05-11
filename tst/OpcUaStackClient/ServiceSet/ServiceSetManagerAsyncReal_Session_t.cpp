63246ebf4ba18c640cf681165642d98d45760650
Kai Huebl <kai@huebl-sgh.de>
2019-05-11 23:34:48 +0200

    Addeed test to check anonymous user authentication

--- tst/OpcUaStackClient/ServiceSet/ServiceSetManagerAsyncReal_Session_t.cpp
+++ tst/OpcUaStackClient/ServiceSet/ServiceSetManagerAsyncReal_Session_t.cpp
@@ -122,6 +122,53 @@ BOOST_FIXTURE_TEST_CASE(ServiceSetManagerAsyncReal_Session_session_connect_disco
 	BOOST_REQUIRE(sessionState_ == SessionServiceStateId::Disconnected);
 }
 
+BOOST_FIXTURE_TEST_CASE(ServiceSetManagerAsyncReal_Session_user_auth_anonymous, GValueFixture)
+{
+	ServiceSetManager serviceSetManager;
+
+	//
+	// init certificate and crypto manager
+	//
+	auto cryptoManager = CryptoManagerTest::getInstance();
+	BOOST_REQUIRE(cryptoManager.get() != nullptr);
+
+	// set secure channel configuration
+	std::string applicationUri = std::string("urn:") + CryptoManagerTest::getServerHostName() + std::string(":ASNeG:ASNeG-Demo");
+	SessionServiceConfig sessionServiceConfig;
+	sessionServiceConfig.secureChannelClient_->endpointUrl(REAL_SERVER_URI);
+	sessionServiceConfig.secureChannelClient_->applicationUri(applicationUri);
+	sessionServiceConfig.secureChannelClient_->cryptoManager(cryptoManager);
+	sessionServiceConfig.secureChannelClient_->securityMode(MessageSecurityMode::EnumSignAndEncrypt);
+	sessionServiceConfig.secureChannelClient_->securityPolicy(SecurityPolicy::EnumBasic128Rsa15);
+	sessionServiceConfig.session_->sessionName(REAL_SESSION_NAME);
+	sessionServiceConfig.session_->authenticationAnonymous("anonymous");
+	sessionServiceConfig.sessionServiceChangeHandler_ =
+		[this] (SessionBase& session, SessionServiceStateId sessionState) {
+			if (sessionState == SessionServiceStateId::Established ||
+				sessionState == SessionServiceStateId::Disconnected) {
+				sessionState_ = sessionState;
+				cond_.sendEvent();
+			}
+		};
+
+	// create session
+	SessionService::SPtr sessionService;
+	sessionService = serviceSetManager.sessionService(sessionServiceConfig);
+	BOOST_REQUIRE(sessionService.get() != nullptr);
+
+	// connect session
+	cond_.condition(1,0);
+	sessionService->asyncConnect();
+	BOOST_REQUIRE(cond_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(sessionState_ == SessionServiceStateId::Established);
+
+	// disconnect session
+	cond_.condition(1,0);
+	sessionService->asyncDisconnect();
+	BOOST_REQUIRE(cond_.waitForCondition(1000) == true);
+	BOOST_REQUIRE(sessionState_ == SessionServiceStateId::Disconnected);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
