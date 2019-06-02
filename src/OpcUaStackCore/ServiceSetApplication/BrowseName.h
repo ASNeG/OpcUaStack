b9e7060ea4bec053845c08fb3fdbdb510727bb1f
Kai Huebl <kai@huebl-sgh.de>
2019-06-02 17:57:13 +0200

    added json formatter to service set application classes

--- src/OpcUaStackCore/ServiceSetApplication/BrowseName.h
+++ src/OpcUaStackCore/ServiceSetApplication/BrowseName.h
@@ -25,6 +25,7 @@ namespace OpcUaStackCore
 {
 
 	class DLLEXPORT BrowseName
+	: public JsonFormatter
 	{
 	  public:
 		typedef boost::shared_ptr<BrowseName> SPtr;
@@ -122,10 +123,10 @@ namespace OpcUaStackCore
 		bool xmlEncode(boost::property_tree::ptree& pt, Xmlns& xmlns) { return false; }
 		bool xmlDecode(boost::property_tree::ptree& pt, const std::string& element, Xmlns& xmlns) { return false; }
 		bool xmlDecode(boost::property_tree::ptree& pt, Xmlns& xmlns) { return false; }
-		bool jsonEncode(boost::property_tree::ptree& pt, const std::string& element) { return false; }
-		bool jsonEncode(boost::property_tree::ptree& pt) { return false; }
-		bool jsonDecode(boost::property_tree::ptree& pt, const std::string& element) { return false; }
-		bool jsonDecode(boost::property_tree::ptree& pt) { return false; }
+
+	  protected:
+		bool jsonEncodeImpl(boost::property_tree::ptree &pt) const { return false; }
+		bool jsonDecodeImpl(const boost::property_tree::ptree &pt) { return false; }
 
 
 	  private:
