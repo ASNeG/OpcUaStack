d396ae85f0f4348ee195f970004ca2a669709017
Kai Huebl <kai@huebl-sgh.de>
2019-07-20 17:14:13 +0200

    feat: move type generator main function to own file

--- src/OpcUaGenerator/OpcUaDataTypeGenerator.cpp
+++ src/OpcUaGenerator/OpcUaDataTypeGenerator.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2017-2018 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2017-2019 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -372,19 +372,6 @@ namespace OpcUaDataTypeGenerator
 		return 0;
 	}
 
-}
-
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-//
-// main application
-//
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-int main(int argc, char** argv)
-{
-	OpcUaDataTypeGenerator::OpcUaDataTypeGenerator projectBuilder;
-	return projectBuilder.start(argc, argv);
 }
 
 
