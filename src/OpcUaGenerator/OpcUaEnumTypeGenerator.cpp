58aa0476bba804898f3c87456997815b7a5d652b
Kai Huebl <kai@huebl-sgh.de>
2019-07-20 18:47:22 +0200

    feat: move enum type generator main function to own file

--- src/OpcUaGenerator/OpcUaEnumTypeGenerator.cpp
+++ src/OpcUaGenerator/OpcUaEnumTypeGenerator.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2018 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2018-2019 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -413,18 +413,4 @@ namespace OpcUaEnumTypeGenerator
 	}
 
 }
-
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-//
-// main application
-//
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-int main(int argc, char** argv)
-{
-	OpcUaEnumTypeGenerator::OpcUaEnumTypeGenerator projectBuilder;
-	return projectBuilder.start(argc, argv);
-}
-
 
