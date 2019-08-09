b387535ba14c187ac25c49d25901fe2f4bbfe629
Kai Huebl <kai@huebl-sgh.de>
2019-08-09 16:31:03 +0200

    feat: add strand functionallity to connection

--- src/OpcUaStackCore/Network/TCPConnection.h
+++ src/OpcUaStackCore/Network/TCPConnection.h
@@ -33,6 +33,9 @@ namespace OpcUaStackCore
 	  public:
 	    typedef boost::shared_ptr<TCPConnection> SPtr;
 
+	    typedef std::function<void (const boost::system::error_code& error, std::size_t bytes_transfered)> ReadCompleteCallback;
+	    typedef std::function<void (const boost::system::error_code& error, std::size_t bytes_transfered)> WriteCompleteCallback;
+
 		TCPConnection(boost::asio::io_service& io_service);
 		~TCPConnection(void);
 
@@ -40,62 +43,206 @@ namespace OpcUaStackCore
 		void cancel(void);
 		void close(void);
 
-		template<typename BUFFER, typename HANDLER>
-		  void async_read_until(BUFFER& buffer, HANDLER handler, const std::string& str)
+		template<typename BUFFER>
+		  void async_read_until(
+		      BUFFER& buffer,
+			  const ReadCompleteCallback& readCompleteCallback,
+			  const std::string& str
+		  )
 		  {
 			  boost::asio::async_read_until(
 				  socket_,
 				  buffer,
 				  str.c_str(),
-				  handler
+				  readCompleteCallback
 			  );
 		  }
 
-		template<typename BUFFER, typename HANDLER>
-		  void async_read_atLeast(BUFFER& buffer, HANDLER handler, uint32_t atLeast=0)
+		template<typename BUFFER>
+		  void async_read_until(
+			  const boost::shared_ptr<boost::asio::io_service::strand>& strand,
+		      BUFFER& buffer,
+			  const ReadCompleteCallback& readCompleteCallback,
+			  const std::string& str
+		  )
+		  {
+			  strand_ = strand;
+			  readCompleteCallback_  = readCompleteCallback;
+
+			  boost::asio::async_read_until(
+				  socket_,
+				  buffer,
+				  str.c_str(),
+				  [this](const boost::system::error_code& error, std::size_t bytes_transfered) {
+				      strand_->dispatch(
+					      [this, error, bytes_transfered](void) {
+						      readCompleteCallback_(error, bytes_transfered);
+						  }
+				      );
+				  }
+			  );
+		  }
+
+		template<typename BUFFER>
+		  void async_read_atLeast(
+		      BUFFER& buffer,
+			  const ReadCompleteCallback& readCompleteCallback,
+			  uint32_t atLeast=0
+	      )
 		  {
 			  boost::asio::async_read(
 				  socket_,
 				  buffer,
 				  boost::asio::transfer_at_least(atLeast),
-				  handler
+				  readCompleteCallback
 			  );
 		  }
 
-		template<typename BUFFER, typename HANDLER>
-		  void async_read_exactly(BUFFER& buffer, HANDLER handler, uint32_t exactly)
+		template<typename BUFFER>
+		  void async_read_atLeast(
+			  const boost::shared_ptr<boost::asio::io_service::strand>& strand,
+		      BUFFER& buffer,
+			  const ReadCompleteCallback& readCompleteCallback,
+			  uint32_t atLeast=0
+	      )
+		  {
+			  strand_ = strand;
+			  readCompleteCallback_  = readCompleteCallback;
+
+			  boost::asio::async_read(
+				  socket_,
+				  buffer,
+				  boost::asio::transfer_at_least(atLeast),
+				  [this](const boost::system::error_code& error, std::size_t bytes_transfered) {
+				      strand_->dispatch(
+					      [this, error, bytes_transfered](void) {
+						      readCompleteCallback_(error, bytes_transfered);
+						  }
+				      );
+				  }
+			  );
+		  }
+
+		template<typename BUFFER>
+		  void async_read_exactly(
+		      BUFFER& buffer,
+			  const ReadCompleteCallback& readCompleteCallback,
+			  uint32_t exactly
+	      )
 		  {
 			  boost::asio::async_read(
 				  socket_,
 				  buffer,
 				  boost::asio::transfer_exactly(exactly),
-				  handler
+				  readCompleteCallback
 			  );
 		  }
 
-		template<typename BUFFER, typename HANDLER>
-		  void async_read_all(BUFFER& buffer, HANDLER handler)
+		template<typename BUFFER>
+		  void async_read_exactly(
+		      const boost::shared_ptr<boost::asio::io_service::strand>& strand,
+		      BUFFER& buffer,
+			  const ReadCompleteCallback& readCompleteCallback,
+			  uint32_t exactly
+	      )
+		  {
+			  strand_ = strand;
+			  readCompleteCallback_  = readCompleteCallback;
+
+			  boost::asio::async_read(
+				  socket_,
+				  buffer,
+				  boost::asio::transfer_exactly(exactly),
+				  [this](const boost::system::error_code& error, std::size_t bytes_transfered) {
+				      strand_->dispatch(
+					      [this, error, bytes_transfered](void) {
+						      readCompleteCallback_(error, bytes_transfered);
+						  }
+				      );
+				  }
+			  );
+		  }
+
+		template<typename BUFFER>
+		  void async_read_all(
+		      BUFFER& buffer,
+			  const ReadCompleteCallback& readCompleteCallback
+		  )
 		  {
 			  boost::asio::async_read(
 				  socket_,
 				  buffer,
 				  boost::asio::transfer_all(),
-				  handler
+				  readCompleteCallback
 			  );
 		  }
 
-		template<typename BUFFER, typename HANDLER>
-		  void async_write(BUFFER& buffer, HANDLER handler) 
+		template<typename BUFFER>
+		  void async_read_all(
+			  const boost::shared_ptr<boost::asio::io_service::strand>& strand,
+		      BUFFER& buffer,
+			  const ReadCompleteCallback& readCompleteCallback
+		  )
+		  {
+			  strand_ = strand;
+			  readCompleteCallback_  = readCompleteCallback;
+
+			  boost::asio::async_read(
+				  socket_,
+				  buffer,
+				  boost::asio::transfer_all(),
+				  [this](const boost::system::error_code& error, std::size_t bytes_transfered) {
+				      strand_->dispatch(
+					      [this, error, bytes_transfered](void) {
+						      readCompleteCallback_(error, bytes_transfered);
+						  }
+				      );
+				  }
+			  );
+		  }
+
+		template<typename BUFFER>
+		  void async_write(
+		      BUFFER& buffer,
+			  const WriteCompleteCallback& writeCompleteCallback
+	      )
 		  {
 			  boost::asio::async_write(
 				  socket_,
 				  buffer,
-				  handler
+				  writeCompleteCallback
 			  );
 		  }
 
-		template<typename BUFFER, typename HANDLER>
-		  void async_write(BUFFER& buffer1, BUFFER& buffer2, HANDLER handler) 
+		template<typename BUFFER>
+		  void async_write(
+			  const boost::shared_ptr<boost::asio::io_service::strand>& strand,
+		      BUFFER& buffer,
+			  const WriteCompleteCallback& writeCompleteCallback
+	      )
+		  {
+			  strand_ = strand;
+			  writeCompleteCallback_  = writeCompleteCallback;
+
+			  boost::asio::async_write(
+				  socket_,
+				  buffer,
+				  [this](const boost::system::error_code& error, std::size_t bytes_transfered) {
+				      strand_->dispatch(
+					      [this, error, bytes_transfered](void) {
+						      writeCompleteCallback_(error, bytes_transfered);
+						  }
+				      );
+				  }
+			  );
+		  }
+
+		template<typename BUFFER>
+		  void async_write(
+		      BUFFER& buffer1,
+			  BUFFER& buffer2,
+			  const WriteCompleteCallback& writeCompleteCallback
+		  )
 		  {
 			  std::vector<boost::asio::const_buffer> buffer;
 			  buffer.push_back(boost::asio::buffer(buffer1.data()));
@@ -103,12 +250,44 @@ namespace OpcUaStackCore
 			  boost::asio::async_write(
 				  socket_,
 				  buffer,
-				  handler
+				  writeCompleteCallback
 			  );
 		  }
 
-		template<typename BUFFER, typename HANDLER>
-		  void async_write(BUFFER& buffer1, BUFFER& buffer2, BUFFER& buffer3, HANDLER handler) 
+		template<typename BUFFER>
+		  void async_write(
+		      const boost::shared_ptr<boost::asio::io_service::strand>& strand,
+		      BUFFER& buffer1,
+			  BUFFER& buffer2,
+			  const WriteCompleteCallback& writeCompleteCallback
+		  )
+		  {
+			  strand_ = strand;
+			  writeCompleteCallback_  = writeCompleteCallback;
+
+			  std::vector<boost::asio::const_buffer> buffer;
+			  buffer.push_back(boost::asio::buffer(buffer1.data()));
+			  buffer.push_back(boost::asio::buffer(buffer2.data()));
+			  boost::asio::async_write(
+				  socket_,
+				  buffer,
+				  [this](const boost::system::error_code& error, std::size_t bytes_transfered) {
+				      strand_->dispatch(
+					      [this, error, bytes_transfered](void) {
+						      writeCompleteCallback_(error, bytes_transfered);
+						  }
+				      );
+				  }
+			  );
+		  }
+
+		template<typename BUFFER>
+		  void async_write(
+		      BUFFER& buffer1,
+			  BUFFER& buffer2,
+			  BUFFER& buffer3,
+			  const WriteCompleteCallback& writeCompleteCallback
+		  )
 		  {
 			  std::vector<boost::asio::const_buffer> buffer;
 			  buffer.push_back(boost::asio::buffer(buffer1.data()));
@@ -117,21 +296,79 @@ namespace OpcUaStackCore
 			  boost::asio::async_write(
 				  socket_,
 				  buffer,
-				  handler
+				  writeCompleteCallback
+			  );
+		  }
+
+		template<typename BUFFER>
+		  void async_write(
+			  const boost::shared_ptr<boost::asio::io_service::strand>& strand,
+		      BUFFER& buffer1,
+			  BUFFER& buffer2,
+			  BUFFER& buffer3,
+			  const WriteCompleteCallback& writeCompleteCallback
+		  )
+		  {
+			  strand_ = strand;
+			  writeCompleteCallback_  = writeCompleteCallback;
+
+			  std::vector<boost::asio::const_buffer> buffer;
+			  buffer.push_back(boost::asio::buffer(buffer1.data()));
+			  buffer.push_back(boost::asio::buffer(buffer2.data()));
+			  buffer.push_back(boost::asio::buffer(buffer3.data()));
+			  boost::asio::async_write(
+				  socket_,
+				  buffer,
+				  [this](const boost::system::error_code& error, std::size_t bytes_transfered) {
+				      strand_->dispatch(
+					      [this, error, bytes_transfered](void) {
+						      writeCompleteCallback_(error, bytes_transfered);
+						  }
+				      );
+				  }
 			  );
 		  }
 
 		template<typename HANDLER>
-		  void async_write(std::vector<boost::asio::const_buffer>& buffer, HANDLER handler) 
+		  void async_write(
+		      std::vector<boost::asio::const_buffer>& buffer,
+			  const WriteCompleteCallback& writeCompleteCallback
+		  )
 		  {
 			  boost::asio::async_write(
 				  socket_,
 				  buffer,
-				  handler
+				  writeCompleteCallback
+			  );
+		  }
+
+		template<typename HANDLER>
+		  void async_write(
+			  const boost::shared_ptr<boost::asio::io_service::strand>& strand,
+		      std::vector<boost::asio::const_buffer>& buffer,
+			  const WriteCompleteCallback& writeCompleteCallback
+		  )
+		  {
+			  strand_ = strand;
+			  writeCompleteCallback_  = writeCompleteCallback;
+
+			  boost::asio::async_write(
+				  socket_,
+				  buffer,
+				  [this](const boost::system::error_code& error, std::size_t bytes_transfered) {
+				      strand_->dispatch(
+					      [this, error, bytes_transfered](void) {
+						      writeCompleteCallback_(error, bytes_transfered);
+						  }
+				      );
+				  }
 			  );
 		  }
 
 	  private:
+		boost::shared_ptr<boost::asio::io_service::strand> strand_ = nullptr;
+		ReadCompleteCallback readCompleteCallback_ = nullptr;
+		WriteCompleteCallback writeCompleteCallback_ = nullptr;
 		boost::asio::ip::tcp::socket socket_;
 		boost::asio::io_service& io_service_;
 	};
