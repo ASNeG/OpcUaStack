2c6a7d203be37e3072a6b0a1081c4b07555f0b9b
Kai Huebl <kai@huebl-sgh.de>
2019-09-19 20:11:16 +0200

    fix: set opc ua client version to 4

--- tst/OpcUaClient/script/TestConfig.sh
+++ tst/OpcUaClient/script/TestConfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OpcUaClient=OpcUaClient2
+OpcUaClient=OpcUaClient4
 EndpointUrl=opc.tcp://127.0.0.1:8889
 #EndpointUrl=opc.tcp://127.0.0.1:4841
 #EndpointUrl=opc.tcp://192.168.122.99:48010
