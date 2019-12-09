7ccefbefc2902cccd76c8dbae96c98e36bb9954f
Kai Huebl <kai@huebl-sgh.de>
2019-12-09 11:52:19 +0100

    fix: add new context classes

---
+++ src/OpcUaStackCore/Application/ApplicationNodeDeleteContext.h
@@ -0,0 +1,39 @@
+/*
+   Copyright 2019 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackCore_ApplicationNodeDeleteContext_h__
+#define __OpcUaStackCore_ApplicationNodeDeleteContext_h__
+
+#include "OpcUaStackCore/Base/BaseClass.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaDataValue.h"
+
+namespace OpcUaStackCore
+{
+
+	class DLLEXPORT ApplicationNodeDeleteContext
+	{
+	  public:
+		ApplicationNodeDeleteContext(void);
+		~ApplicationNodeDeleteContext(void);
+
+		BaseClass::SPtr applicationContext_;	// IN - application context from register call
+		OpcUaNodeId nodeId_;					// IN - node id to be created
+	};
+
+}
+
+#endif
