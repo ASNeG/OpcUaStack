b1f407fb20dcff160c2cd4d572517ddbd172b346
Kai Huebl <kai@huebl-sgh.de>
2020-05-02 15:07:30 +0200

    fix typo

--- src/OpcUaStackServer/ServiceManager/ServiceManager.cpp
+++ src/OpcUaStackServer/ServiceManager/ServiceManager.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015-2019 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2020 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -258,7 +258,7 @@ namespace OpcUaStackServer
 	}
 
 	bool 
-	ServiceManager::informatinModel(InformationModel::SPtr informationModel)
+	ServiceManager::informationModel(InformationModel::SPtr informationModel)
 	{
 		attributeService_->informationModel(informationModel);
 		methodService_->informationModel(informationModel);
