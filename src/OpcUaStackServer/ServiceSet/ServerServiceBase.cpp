45eff6da68f158ffde85059524a10378926ce2d1
Kai Huebl <kai@huebl-sgh.de>
2020-05-03 08:47:07 +0200

    add new class ServerServiceBase

---
+++ src/OpcUaStackServer/ServiceSet/ServerServiceBase.cpp
@@ -0,0 +1,140 @@
+/*
+   Copyright 2020 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include <future>
+#include <boost/make_shared.hpp>
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaStackServer/ServiceSet/ServerServiceBase.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackServer
+{
+
+	ServerServiceBase::ServerServiceBase(void)
+	: boost::enable_shared_from_this<ServerServiceBase>()
+	{
+	}
+
+	ServerServiceBase::~ServerServiceBase(void)
+	{
+	}
+
+	MessageBusMember::WPtr&
+	ServerServiceBase::messageBusMember(void)
+	{
+		return messageBusMember_;
+	}
+
+	boost::shared_ptr<boost::asio::io_service::strand>&
+	ServerServiceBase::strand(void)
+	{
+		return strand_;
+	}
+
+	void
+	ServerServiceBase::activateReceiver(const ReceiverCallback& receiverCallback)
+	{
+		Log(Info, "activate receiver")
+			.parameter("ServiceName", serviceName_);
+
+		receiverContext_ = boost::make_shared<ReceiverContext>();
+		receiverContext_->receiverCallback_ = receiverCallback;
+
+		// call the receiver for the first time
+		receiveCallback();
+	}
+
+	void
+	ServerServiceBase::deactivateReceiver(void)
+	{
+		if (!receiverContext_->receiverCallback_) {
+			return;
+		}
+
+		if (!strand_->running_in_this_thread()) {
+			// the function was not called by the strand
+
+			std::promise<void> promise;
+			std::future<void> future = promise.get_future();
+
+			strand_->dispatch(
+				[this, &promise]() {
+				    deactivateReceiver();
+				    promise.set_value();
+			    }
+			);
+
+			future.wait();
+			return;
+		}
+
+		Log(Info, "deactivate receiver")
+			.parameter("ServiceName", serviceName_);
+
+		//
+		// the function was called by the strand
+		//
+
+		if (receiverContext_->receiverCallbackRunning_) {
+			// The receiver callback is currently running
+			receiverContext_->shutdown_ = true;
+			receiverContext_.reset();
+			return;
+		}
+
+		// the receiver is currently not running
+		messageBus_->cancelReceiver(messageBusMember_, true);
+		receiverContext_.reset();
+	}
+
+	void
+	ServerServiceBase::receiveCallback(void)
+	{
+		messageBus_->messageReceive(
+			messageBusMember_,
+			strand_,
+			[this](MessageBusError error, const MessageBusMember::WPtr& handleFrom, Message::SPtr& message) {
+
+				// check error
+				if (error != MessageBusError::Ok) {
+					if (error != MessageBusError::Cancel) {
+						Log(Error, "message receiver error")
+							.parameter("ServiceName", serviceName_)
+							.parameter("Error", int(error));
+					}
+					return;
+				}
+
+				// execute callback
+				ReceiverContext::SPtr receiverContext = receiverContext_;
+				if (receiverContext->receiverCallback_) {
+					receiverContext->receiverCallbackRunning_ = true;
+					receiverContext->receiverCallback_(message);
+					receiverContext->receiverCallbackRunning_ = false;
+
+					if (receiverContext->shutdown_) {
+						return;
+					}
+				}
+
+				receiveCallback();
+		    }
+		);
+	}
+
+}
---
+++ src/OpcUaStackServer/ServiceSet/ServerServiceBase.h
@@ -0,0 +1,68 @@
+/*
+   Copyright 2020 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaStackServer_ServerServiceBase_h__
+#define __OpcUaStackServer_ServerServiceBase_h__
+
+#include <functional>
+#include "OpcUaStackCore/Component/MessageBus.h"
+#include "OpcUaStackCore/Utility/IOThread.h"
+
+namespace OpcUaStackServer
+{
+
+	class DLLEXPORT ServerServiceBase
+	: public boost::enable_shared_from_this<ServerServiceBase>
+	{
+	  public:
+		using SPtr = boost::shared_ptr<ServerServiceBase>;
+		using ReceiverCallback = std::function<void (OpcUaStackCore::Message::SPtr&)>;
+
+		ServerServiceBase(void);
+		virtual ~ServerServiceBase(void);
+
+		OpcUaStackCore::MessageBusMember::WPtr& messageBusMember(void);
+		boost::shared_ptr<boost::asio::io_service::strand>& strand(void);
+
+		void activateReceiver(const ReceiverCallback& receiverCallback);
+		void deactivateReceiver(void);
+
+	  protected:
+		void receiveCallback(void);
+
+		// configuration data
+		std::string serviceName_ = "";
+		OpcUaStackCore::IOThread* ioThread_ = nullptr;
+		boost::shared_ptr<boost::asio::io_service::strand> strand_ = nullptr;
+		OpcUaStackCore::MessageBus::SPtr messageBus_ = nullptr;
+		OpcUaStackCore::MessageBusMember::WPtr messageBusMember_;
+
+		// runtime data
+		class ReceiverContext
+		{
+		  public:
+			using SPtr = boost::shared_ptr<ReceiverContext>;
+			ReceiverCallback receiverCallback_ = nullptr;
+			bool receiverCallbackRunning_ = false;
+			bool shutdown_ = false;
+		};
+		ReceiverContext::SPtr receiverContext_;
+	};
+
+}
+
+#endif
