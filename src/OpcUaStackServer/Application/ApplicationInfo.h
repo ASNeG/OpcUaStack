efc1c08503093c617ab3f029ffed161ad34e445e
Kai Huebl <kai@huebl-sgh.de>
2020-05-23 13:09:58 +0200

    read application thread configuration from configuration

--- src/OpcUaStackServer/Application/ApplicationInfo.h
+++ src/OpcUaStackServer/Application/ApplicationInfo.h
@@ -1,5 +1,5 @@
 /*
-   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2016-2020 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -19,6 +19,7 @@
 #define __OpcUaStackServer_ApplicationInfo_h__
 
 #include <string>
+#include <stdint.h>
 #include "OpcUaStackCore/Base/os.h"
 
 namespace OpcUaStackServer
@@ -36,16 +37,22 @@ namespace OpcUaStackServer
 		std::string libraryName(void);
 		void configFileName(const std::string& configFileName);
 		std::string configFileName(void);
+		void threadPoolName(const std::string& threadPoolName);
+		std::string threadPoolName(void);
+		void numberThreads(uint32_t numberThreads);
+		uint32_t numberThreads(void);
 
 		void confDir(const std::string& confDir);
 		std::string confDir(void);
 
 	  private:
-		std::string applicationName_;
-		std::string libraryName_;
-		std::string configFileName_;
+		std::string applicationName_ = "";
+		std::string libraryName_ = "";
+		std::string configFileName_ = "";
+		std::string threadPoolName_ = "ThrAppl";
+		uint32_t numberThreads_ = 2;
+		std::string confDir_ = "";
 
-		std::string confDir_;
 	};
 
 }
