14d79cd17359f34f6fe692abfc45c483ccffacde
Kai Huebl <kai@huebl-sgh.de>
2020-08-23 17:25:21 +0200

    add git version log messages for application libraries

--- src/OpcUaServer/ApplicationLibrary/ApplicationLibrary.cpp
+++ src/OpcUaServer/ApplicationLibrary/ApplicationLibrary.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015-2019 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2015-2020 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
@@ -66,6 +66,18 @@ namespace OpcUaServer
 	ApplicationLibrary::version(void)
 	{
 		return applicationIf_->version();
+	}
+
+	std::string
+	ApplicationLibrary::gitCommit(void)
+	{
+		return applicationIf_->gitCommit();
+	}
+
+	std::string
+	ApplicationLibrary::gitBranch(void)
+	{
+		return applicationIf_->gitBranch();
 	}
 
 	bool
