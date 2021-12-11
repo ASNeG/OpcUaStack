2f95b7bbf493ed3ac4f832d40da0adef159530d5
Kai Huebl <kai@huebl-sgh.de>
2021-12-11 22:14:46 +0100

    add unittest to check class DataTypeDefinition

--- tst/OpcUaStackCore/StandardDataTypes/DataTypes_t.cpp
+++ tst/OpcUaStackCore/StandardDataTypes/DataTypes_t.cpp
@@ -4,7 +4,8 @@
 #include "OpcUaStackCore/BuildInTypes/OpcUaIdentifier.h"
 #include "OpcUaStackCore/BuildInTypes/BuildInTypes.h"
 #include "OpcUaStackCore/BuildInTypes/OpcUaExtensionObject.h"
-#include "OpcUaStackCore/StandardDataTypes/ServerStatusDataType.h"
+#include "OpcUaStackCore/StandardDataTypes/EnumDefinitionExpand.h"
+#include "OpcUaStackCore/StandardDataTypes/StructureDefinitionExpand.h"
 
 using namespace OpcUaStackCore;
 
@@ -13,6 +14,30 @@ BOOST_AUTO_TEST_SUITE(DataTypes_)
 BOOST_AUTO_TEST_CASE(DataTypes_)
 {
 	std::cout << "DataTypes_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(DataTypes_StructureDefinitionExpand)
+{
+	StructureDefinitionExpand::SPtr structureTypeDefinition1 = boost::make_shared<StructureDefinitionExpand>();
+	DataTypeDefinition::SPtr dataTypeDefinition = structureTypeDefinition1;
+
+	BOOST_REQUIRE(dataTypeDefinition->typeId() == OpcUaNodeId((OpcUaUInt32)99,0));
+
+
+	StructureDefinitionExpand::SPtr structureTypeDefinition2 = boost::static_pointer_cast<StructureDefinitionExpand>(dataTypeDefinition);
+	BOOST_REQUIRE(structureTypeDefinition2->typeId() == OpcUaNodeId((OpcUaUInt32)99,0));
+}
+
+BOOST_AUTO_TEST_CASE(DataTypes_EnumDefinitionExpand)
+{
+	EnumDefinitionExpand::SPtr enumTypeDefinition1 = boost::make_shared<EnumDefinitionExpand>();
+	DataTypeDefinition::SPtr dataTypeDefinition = enumTypeDefinition1;
+
+	BOOST_REQUIRE(dataTypeDefinition->typeId() == OpcUaNodeId((OpcUaUInt32)100,0));
+
+
+	EnumDefinitionExpand::SPtr enumTypeDefinition2 = boost::static_pointer_cast<EnumDefinitionExpand>(dataTypeDefinition);
+	BOOST_REQUIRE(enumTypeDefinition2->typeId() == OpcUaNodeId((OpcUaUInt32)100,0));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
