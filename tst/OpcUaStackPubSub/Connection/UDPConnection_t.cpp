bb745b5cd70c53db4eb408b21f884989963d3939
Kai Huebl <kai@huebl-sgh.de>
2022-03-05 10:38:23 +0100

    add udp connection unittest

---
+++ tst/OpcUaStackPubSub/Connection/UDPConnection_t.cpp
@@ -0,0 +1,22 @@
+#include "unittest.h"
+
+BOOST_AUTO_TEST_SUITE(UDPConnection_)
+
+BOOST_AUTO_TEST_CASE(UDPConnection_)
+{
+	std::cout << "UDPConnection_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(UDPConnection_Unicast)
+{
+}
+
+BOOST_AUTO_TEST_CASE(UDPConnection_Broadcast)
+{
+}
+
+BOOST_AUTO_TEST_CASE(UDPConnection_Multicast)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
