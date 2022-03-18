85adf23a866f437324891e5ad6ecfabbc6893547
Kai Huebl <kai@huebl-sgh.de>
2022-03-18 15:10:42 +0100

    add class MsgTransSubscriber

---
+++ src/OpcUaStackPubSub/MessageTransport/MsgTransSubscriber.cpp
@@ -0,0 +1,77 @@
+/*
+   Copyright 2022 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaStackPubSub/MessageTransport/MsgTransSubscriber.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaStackPubSub
+{
+
+	MsgTransSubscriber::MsgTransSubscriber(
+		const std::string& serviceName,
+		OpcUaStackCore::IOThread::SPtr& ioThread,
+		OpcUaStackCore::MessageBus::SPtr& messageBus
+	)
+	: ServerServiceBase()
+	{
+		// set parameter
+		ioThread_ = ioThread;
+
+		// set parameter in server service base
+		serviceName_ = serviceName;
+		ServerServiceBase::ioThread_ = ioThread.get();
+		strand_ = ioThread->createStrand();
+		messageBus_ = messageBus;
+
+		// register message bus receiver
+		MessageBusMemberConfig messageBusMemberConfig;
+		messageBusMemberConfig.strand(strand_);
+		messageBusMember_ = messageBus_->registerMember(serviceName_, messageBusMemberConfig);
+
+		// activate receiver
+		activateReceiver(
+			[this](const MessageBusMember::WPtr& handleFrom, Message::SPtr& message) {
+				// receive message from internal message bus
+
+				// FIXME: todo
+			}
+		);
+	}
+
+	MsgTransSubscriber::~MsgTransSubscriber(void)
+	{
+		// deactivate receiver
+		deactivateReceiver();
+		messageBus_->deregisterMember(messageBusMember_);
+	}
+
+	bool
+	MsgTransSubscriber::startup(void)
+	{
+		// FIXME: todo
+		return true;
+	}
+
+	bool
+	MsgTransSubscriber::shutdown(void)
+	{
+	// FIXME: todo
+		return true;
+	}
+
+}
